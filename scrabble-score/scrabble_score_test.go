cfdf4d4cac8284684ab950d1ff816533d42595f8
Nathan Youngman <git@nathany.com>
2014-04-22 15:38:08 -0600

    scrabble score exercise

---
+++ scrabble-score/scrabble_score_test.go
@@ -0,0 +1,25 @@
+package scrabble_score
+
+import "testing"
+
+var tests = []struct {
+	input    string
+	expected int
+}{
+	{"", 0},
+	{" \t\n", 0},
+	{"a", 1},
+	{"f", 4},
+	{"street", 6},
+	{"quirky", 22},
+	{"MULTIBILLIONAIRE", 20},
+	{"alacrity", 13},
+}
+
+func TestScore(t *testing.T) {
+	for _, test := range tests {
+		if actual := Score(test.input); actual != test.expected {
+			t.Errorf("Score(%q) expected %d, Actual %d", test.input, test.expected, actual)
+		}
+	}
+}
