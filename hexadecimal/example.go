0f23f0da48a945cb171519892febfb790c964b11
John G. Beckett <jgbeckett@gmail.com>
2014-05-22 09:54:19 +0200

    Make hexadecimal problem more idiomatic

--- hexadecimal/example.go
+++ hexadecimal/example.go
@@ -1,46 +1,33 @@
 package hexadecimal
 
 import (
-	"math"
-	"strconv"
-	"strings"
+	"fmt"
 )
 
-//  The easy way:
-//
-//  func ToDecimal(s string) int64 {
-//    v, _ := strconv.ParseInt(s, 16, 64)
-//    return v
-//  }
-
-//  A bit harder
-
-func ToDecimal(s string) (n int64) {
-
-	if invalid(s) {
-		return int64(0)
-	}
-
-	length := len(s)
-
-	for i := length; i > 0; i-- {
-		v := intValue(s[i-1])
-		n = n + (v * int64(math.Pow(16, float64(length-i))))
-	}
-
-	return n
-}
-
-func invalid(s string) bool {
-	for _, char := range strings.Split(s, "") {
-		if char > "f" {
-			return true
+// ToDecimal converts a hexadecimal string to decimal integer.
+// If the string is invalid, it returns 0 and an error.
+func ToDecimal(hex string) (int64, error) {
+	decimal := int64(0)
+	digitVal := int64(0)
+	// for each character in the hex string
+	for _, c := range hex {
+		// get the value of the character
+		switch {
+		// numbers
+		case '0' <= c && c <= '9':
+			digitVal = int64(c - '0')
+			// valid lowercase letters
+		case 'a' <= c && c <= 'f':
+			digitVal = int64(c - 'a' + 10)
+			// valid uppercase letters
+		case 'A' <= c && c <= 'F':
+			digitVal = int64(c - 'A' + 10)
+		default:
+			// if the character is invalid, return 0
+			return 0, fmt.Errorf("%s is an invalid hexadecimal string", hex)
 		}
+		// multiply the current number by 16 (left shift by 4) and add the digit
+		decimal = decimal<<4 + digitVal
 	}
-	return false
-}
-
-func intValue(c uint8) int64 {
-	v, _ := strconv.ParseInt(string(c), 16, 64)
-	return v
+	return decimal, nil
 }
