e19edcd1ad38c8d8371a647323ca3d76e2d7c062
John G. Beckett <jgbeckett@gmail.com>
2014-05-22 16:47:25 +0200

    improve test and example for Grains
    modify grains test to not expect a specific error
    
    clean up stray comment
    
    add test for 0 case
    corrected a missing output error message in a test

--- grains/example.go
+++ grains/example.go
@@ -1,15 +1,23 @@
 package grains
 
-import "math"
+import (
+	"fmt"
+)
 
-func Square(number int) uint64 {
-	return uint64(math.Exp2(float64(number - 1)))
+// Square returns the number of grains on a square on a chess board where the
+// first square has 1 and every subsequent square doubles the number.
+func Square(num int) (uint64, error) {
+	if num < 1 || num > 64 {
+		return 0, fmt.Errorf("Input[%d] invalid. Input should be between 1 and 64 (inclusive)", num)
+	}
+	// a left shift is equivalent to multiplying by 2.  So we need to left
+	// shift by num-1 times to get the number of grains on that square
+	return 1 << (uint16(num) - 1), nil
 }
 
+// Total returns the total number of grains on the chess board
 func Total() uint64 {
-	var total uint64
-	for i := 1; i <= 64; i++ {
-		total += Square(i)
-	}
-	return total
+	// the number of grains added up to any point on the board is simply
+	// two to the power of that number - 1 (because maths)
+	return (1 << 64) - 1
 }
--- grains/grains_test.go
+++ grains/grains_test.go
@@ -1,24 +1,40 @@
 package grains
 
-import "testing"
+import (
+	"testing"
+)
 
 var squareTests = []struct {
-	input    int
-	expected uint64
+	input       int
+	expectedVal uint64
+	expectError bool
 }{
-	{1, 1},
-	{2, 2},
-	{3, 4},
-	{4, 8},
-	{16, 32768},
-	{32, 2147483648},
-	{64, 9223372036854775808},
+	{1, 1, false},
+	{2, 2, false},
+	{3, 4, false},
+	{4, 8, false},
+	{16, 32768, false},
+	{32, 2147483648, false},
+	{64, 9223372036854775808, false},
+	{65, 0, true},
+	{0, 0, true},
+	{-1, 0, true},
 }
 
 func TestSquare(t *testing.T) {
 	for _, test := range squareTests {
-		if actual := Square(test.input); actual != test.expected {
-			t.Errorf("Square(%d) expected %d, Actual %d", test.input, test.expected, actual)
+		actualVal, actualErr := Square(test.input)
+		if actualVal != test.expectedVal {
+			t.Errorf("Square(%d) expected %d, Actual %d", test.input, test.expectedVal, actualVal)
+		}
+
+		// if we expect an error and there isn't one
+		if test.expectError && actualErr == nil {
+			t.Errorf("Square(%d) expected an error, but error is nil", test.input)
+		}
+		// if we don't expect an error and there is one
+		if !test.expectError && actualErr != nil {
+			t.Errorf("Square(%d) expected no error, but error is: %s", test.input, actualErr)
 		}
 	}
 }
@@ -27,5 +43,19 @@ func TestTotal(t *testing.T) {
 	var expected uint64 = 18446744073709551615
 	if actual := Total(); actual != expected {
 		t.Errorf("Total() expected %d, Actual %d", expected, actual)
+	}
+}
+
+func BenchmarkSquare(b *testing.B) {
+	b.StopTimer()
+
+	for _, test := range squareTests {
+		b.StartTimer()
+
+		for i := 0; i < b.N; i++ {
+			Square(test.input)
+		}
+
+		b.StopTimer()
 	}
 }
