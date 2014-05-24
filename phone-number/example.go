2a24c8d93f7fd921c5553c874e303aefa9d2e407
John G. Beckett <jgbeckett@gmail.com>
2014-05-24 15:28:53 +0200

    phone-number: made more idiomatic

--- phone-number/example.go
+++ phone-number/example.go
@@ -2,29 +2,61 @@ package phonenumber
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
+	"unicode"
 )
 
-func Number(in string) (ret string) {
-	re := regexp.MustCompile("[^0-9]*")
-	ret = re.ReplaceAllString(in, "")
-	if len(ret) < 10 || !strings.HasPrefix(ret, "1") {
-		ret = "0000000000"
+// Number takes in a potential phone number string and returns the number
+// without any formatting if it's valid.
+//
+// * If the phone number is less than 10 digits assume that it is bad number
+// * If the phone number is 10 digits assume that it is good
+// * If the phone number is 11 digits and the first number is 1, trim the 1 and use the first 10 digits
+// * If the phone number is 11 digits and the first number is not 1, then it is a bad number
+// * If the phone number is more than 11 digits assume that it is a bad number
+func Number(s string) (string, error) {
+
+	//remove any characters that aren't digits
+	clean := strings.Map(
+		func(r rune) rune {
+			if unicode.IsDigit(r) {
+				return r
+			}
+			return -1
+		}, s)
+
+	numDigits := len(clean)
+	switch {
+	// bad number if less than 10 digits
+	case numDigits < 10:
+		return "", fmt.Errorf("Phone number %q contains too few digits", s)
+	// good number if exactly 10 digits
+	case numDigits == 10:
+		return clean, nil
+	// good number if 11 digits and 1st digit is a 1.  Return last 10 digits.
+	case numDigits == 11 && clean[0] == '1':
+		return clean[1:], nil
 	}
-	return
+	// all other numbers are a bad
+	return "", fmt.Errorf("Phone number %q contains too many digits", s)
 }
 
-func AreaCode(in string) string {
-	return in[0:3]
+// AreaCode takes in a phone number string and returns the area code (first three digits)
+func AreaCode(s string) (string, error) {
+	num, err := Number(s)
+	if err != nil {
+		return "", err
+	}
+	return num[:3], nil
 }
 
-func Format(in string) (ret string) {
-	format := "(%s) %s-%s"
-	if len(in) == 10 {
-		ret = fmt.Sprintf(format, AreaCode(in), in[3:6], in[6:])
-	} else {
-		ret = fmt.Sprintf(format, in[1:4], in[4:7], in[7:])
+// Format takes in a phone number string and returns it as a well formatted
+// human readable string
+func Format(s string) (string, error) {
+	f, err := Number(s)
+	if err != nil {
+		return "", err
 	}
-	return
+	// should follow the sample format of: (123) 456-7890
+	return fmt.Sprintf("(%s) %s-%s", f[:3], f[3:6], f[6:]), nil
 }
--- phone-number/phone_number_test.go
+++ phone-number/phone_number_test.go
@@ -4,75 +4,115 @@ import (
 	"testing"
 )
 
-type test struct {
-	input    string
-	expected string
+type testCase struct {
+	input     string
+	expected  string
+	expectErr bool
 }
 
-func assertAllTestsPass(t *testing.T, tests []test, testFunc func(string) string) {
-	for _, test := range tests {
-		actual := testFunc(test.input)
-		if test.expected != actual {
+var numberTests = []testCase{
+	{"(123) 456-7890", "1234567890", false},
+	{"123.456.7890", "1234567890", false},
+	{"1234567890", "1234567890", false},
+	{"12345678901234567", "", true},
+	{"21234567890", "", true},
+	{"123456789", "", true},
+}
+
+func TestNumber(t *testing.T) {
+	for _, test := range numberTests {
+		actual, actualErr := Number(test.input)
+		if actual != test.expected {
 			t.Errorf("Number(%s): expected [%s], actual: [%s]", test.input, test.expected, actual)
+		}
+		// if we expect an error and there isn't one
+		if test.expectErr && actualErr == nil {
+			t.Errorf("Number(%s): expected an error, but error is nil", test.input)
+		}
+		// if we don't expect an error and there is one
+		if !test.expectErr && actualErr != nil {
+			t.Errorf("Number(%s): expected no error, but error is: %s", test.input, actualErr)
 		}
 	}
 }
 
-func benchAllTests(b *testing.B, tests []test, testFunc func(string) string) {
-	//b.StopTimer()
-	for _, test := range tests {
-		//b.StartTimer()
-
-		testFunc(test.input)
-
-		//b.StopTimer()
-	}
-}
-
-var numberTests = []test{
-	{"(123) 456-7890", "1234567890"},
-	{"123.456.7890", "1234567890"},
-	{"1234567890", "1234567890"},
-	{"21234567890", "0000000000"},
-	{"123456789", "0000000000"},
-}
-
-func TestNumber(t *testing.T) {
-	assertAllTestsPass(t, numberTests, Number)
-}
-
 func BenchmarkNumber(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		benchAllTests(b, numberTests, Number)
+	b.StopTimer()
+	for _, test := range numberTests {
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			Number(test.input)
+		}
+		b.StopTimer()
 	}
 }
 
-var areaCodeTests = []test{
-	{"1234567890", "123"},
-	{"213.456.7890", "213"},
+var areaCodeTests = []testCase{
+	{"1234567890", "123", false},
+	{"213.456.7890", "213", false},
+	{"213.456.7890.2345", "", true},
+	{"213.456", "", true},
 }
 
 func TestAreaCode(t *testing.T) {
-	assertAllTestsPass(t, areaCodeTests, AreaCode)
-}
-
-func BenchmarkAreaCode(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		benchAllTests(b, areaCodeTests, AreaCode)
+	for _, test := range areaCodeTests {
+		actual, actualErr := AreaCode(test.input)
+		if actual != test.expected {
+			t.Errorf("AreaCode(%s): expected [%s], actual: [%s]", test.input, test.expected, actual)
+		}
+		// if we expect an error and there isn't one
+		if test.expectErr && actualErr == nil {
+			t.Errorf("AreaCode(%s): expected an error, but error is nil", test.input)
+		}
+		// if we don't expect an error and there is one
+		if !test.expectErr && actualErr != nil {
+			t.Errorf("AreaCode(%s): expected no error, but error is: %s", test.input, actualErr)
+		}
 	}
 }
 
-var formatTests = []test{
-	{"1234567890", "(123) 456-7890"},
-	{"11234567890", "(123) 456-7890"},
+func BenchmarkAreaCode(b *testing.B) {
+	b.StopTimer()
+	for _, test := range areaCodeTests {
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			AreaCode(test.input)
+		}
+		b.StopTimer()
+	}
+}
+
+var formatTests = []testCase{
+	{"1234567890", "(123) 456-7890", false},
+	{"11234567890", "(123) 456-7890", false},
+	{"112345", "", true},
+	{"11234590870986", "", true},
 }
 
 func TestFormat(t *testing.T) {
-	assertAllTestsPass(t, formatTests, Format)
+	for _, test := range formatTests {
+		actual, actualErr := Format(test.input)
+		if actual != test.expected {
+			t.Errorf("Format(%s): expected [%s], actual: [%s]", test.input, test.expected, actual)
+		}
+		// if we expect an error and there isn't one
+		if test.expectErr && actualErr == nil {
+			t.Errorf("Format(%s): expected an error, but error is nil", test.input)
+		}
+		// if we don't expect an error and there is one
+		if !test.expectErr && actualErr != nil {
+			t.Errorf("Format(%s): expected no error, but error is: %s", test.input, actualErr)
+		}
+	}
 }
 
 func BenchmarkFormat(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		benchAllTests(b, formatTests, Format)
+	b.StopTimer()
+	for _, test := range areaCodeTests {
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			Format(test.input)
+		}
+		b.StopTimer()
 	}
 }
