cbfc6f420c56c2afed6cde18ba0722efa3a11b6c
Sonia Keys <soniakeys@gmail.com>
2014-06-06 12:37:54 -0400

    add palindrome-product

---
+++ palindrome-product/example.go
@@ -0,0 +1,51 @@
+package palindrome
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isPal(x int) bool {
+	s := strconv.Itoa(x)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
+
+type Product struct {
+	Product        int
+	Factorizations [][2]int
+}
+
+func Products(fmin, fmax int) (pmin, pmax Product, err error) {
+	if fmin > fmax {
+		err = fmt.Errorf("fmin > fmax: %d > %d", fmin, fmax)
+		return
+	}
+	for x := fmin; x <= fmax; x++ {
+		for y := x; y <= fmax; y++ {
+			p := x * y
+			if !isPal(p) {
+				continue
+			}
+			compare := func(current *Product, better bool) {
+				switch {
+				case current.Factorizations == nil || better:
+					*current = Product{p, [][2]int{{x, y}}}
+				case p == current.Product:
+					current.Factorizations =
+						append(current.Factorizations, [2]int{x, y})
+				}
+			}
+			compare(&pmin, p < pmin.Product)
+			compare(&pmax, p > pmax.Product)
+		}
+	}
+	if len(pmin.Factorizations) == 0 {
+		err = fmt.Errorf("No palindromes in range [%d, %d].", fmin, fmax)
+	}
+	return
+}
