3b8908146dbb3d2cd855f7da8eeccdc427198edf
Sonia Keys <skeys.cfa.harvard.edu>
2014-07-01 23:00:31 +0000

    pretty close

--- custom-set/custom_set_test.go
+++ custom-set/custom_set_test.go
@@ -1,5 +1,7 @@
 package stringset
 
+// Implement Set as a collection of unique string values.
+//
 // API:
 //
 // New() Set
@@ -26,6 +28,7 @@ package stringset
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -33,42 +36,61 @@ var _ fmt.Stringer = New()
 
 var _empty = New()
 
-// Early tests of New and Add also test String.  No way around the chicken
-// and egg problem.
+// These first tests of New and Add also test String.  No way around the
+// chicken and egg problem.
 func TestNew(t *testing.T) {
-	if res := _empty.String(); res != "{}" {
-		t.Fatalf(`New().String() = %s, want "{}".`, res)
+
+	// New must return an empty set.
+	want := "{}"
+	if res := _empty.String(); res != want {
+		t.Fatalf(`New().String() = %s, want %s.`, res, want)
 	}
 }
 
 var (
 	_a    = New()
 	_b    = New()
+	_c    = New()
 	_ab   = New()
+	_abc  = New()
 	_aS   = `{"a"}`
 	_bS   = `{"b"}`
-	_abS1 = `{"a", "b"}`
-	_abS2 = `{"b", "a"}`
+	_abS  = `{"a", "b"}`
+	_baS  = `{"b", "a"}`
+	_abcS = `{"a", "b", "c"}`
 )
 
 func init() {
 	_a.Add("a")
 	_b.Add("b")
+	_c.Add("c")
 	_ab.Add("a")
 	_ab.Add("b")
+	_abc.Add("a")
+	_abc.Add("b")
+	_abc.Add("c")
 }
 
 func TestAdd(t *testing.T) {
+	// some sets are created and elements added in init() above.
+	// now test results:
+
+	// single element
 	if res := _a.String(); res != _aS {
 		t.Fatalf(`Add("a") = %s, want %s.`, res, _aS)
 	}
+
+	// another set, with a different element element
 	if res := _b.String(); res != _bS {
 		t.Fatalf(`Add("b") = %s, want %s.`, res, _bS)
 	}
-	if res := _ab.String(); res != _abS1 && res != _abS2 {
-		t.Fatalf(`Add("a"); Add("b") = %s, want %s.`, res, _abS1)
+
+	// a third set, with two elements added
+	if res := _ab.String(); res != _abS && res != _baS { // order undefined
+		t.Fatalf(`Add("a"); Add("b") = %s, want %s.`, res, _abS)
 	}
-	// another b should not change b
+
+	// test adding an element that already exists
 	_b.Add("b")
 	if res := _b.String(); res != _bS {
 		t.Fatalf(`Add("b") = %s, want %s.`, res, _bS)
@@ -76,60 +98,77 @@ func TestAdd(t *testing.T) {
 }
 
 func TestNewFromSlice(t *testing.T) {
-	if res := NewFromSlice(nil).String(); res != "{}" {
-		t.Fatalf(`NewFromSlice(nil) = %s, want "{}".`, res)
+	// nil slice should give empty set
+	want := "{}"
+	if res := NewFromSlice(nil).String(); res != want {
+		t.Fatalf(`NewFromSlice(nil) = %s, want %s.`, res, want)
 	}
+
+	// slice with one element:
 	if res := NewFromSlice([]string{"a"}).String(); res != _aS {
-		t.Fatalf(`NewFromSlice([]string{"a"}) = %s, want %s.`,
-			res, _aS)
+		t.Fatalf(`NewFromSlice([]string{"a"}) = %s, want %s.`, res, _aS)
 	}
+
+	// slice with two elements:
 	res := NewFromSlice([]string{"a", "b"}).String()
-	if res != _abS1 && res != _abS2 {
-		t.Fatalf(`NewFromSlice([]string{"a", "b"}) = %s, want %s.`,
-			res, _abS1)
+	if res != _abS && res != _baS { // order undefined
+		t.Fatalf(`NewFromSlice([]string{"a", "b"}) = %s, want %s.`, res, _abS)
+	}
+
+	// slice with repeated element:
+	if res := NewFromSlice([]string{"a", "a"}).String(); res != _aS {
+		t.Fatalf(`NewFromSlice([]string{"a", "a"}) = %s, want %s.`, res, _aS)
 	}
 }
 
 func TestDeleteHasIsEmptyLen(t *testing.T) {
+	// properties of empty set:
 	s := New()
-	t.Log("s := New()")
+	t.Log("s := New()") // log messages provide context for failure messages
 	if l := s.Len(); l != 0 {
 		t.Fatalf(`s.Len() = %d, want 0.`, l)
 	}
-	if !s.IsEmpty() {
-		t.Fatal(`s.IsEmpty() = false, want true.`)
+	// `if !s.IsEmpty() {` is readable, but then literal bools
+	// in the fail message would be prone to being mistyped.
+	// this more tedious form should be safer.
+	if res, want := s.IsEmpty(), true; res != want {
+		t.Fatalf(`s.IsEmpty() = %t, want %t.`, res, want)
 	}
 
+	// properties of set with one element:
 	s.Add("a")
 	t.Log(`s.Add("a")`)
 	if l := s.Len(); l != 1 {
 		t.Fatalf(`s.Len() = %d, want 1.`, l)
 	}
-	if s.IsEmpty() {
-		t.Fatal(`s.IsEmpty() = true, want false.`)
+	if res, want := s.IsEmpty(), false; res != want {
+		t.Fatalf(`s.IsEmpty() = %t, want %t.`, res, want)
 	}
-	if !s.Has("a") {
-		t.Fatal(`s.Has("a") = false, want true.`)
+	if res, want := s.Has("a"), true; res != want {
+		t.Fatalf(`s.Has("a") = %t, want %t.`, res, want)
 	}
 
+	// delete sole element:
 	s.Delete("a")
 	t.Log(`s.Delete("a")`)
-	if !s.IsEmpty() {
-		t.Fatalf(`s.IsEmpty = false, want true.`)
+	if res, want := s.IsEmpty(), true; res != want {
+		t.Fatalf(`s.IsEmpty = %t, want %t.`, res, want)
 	}
 
+	// add a different element
 	s.Add("b")
 	t.Log(`s.Add("b")`)
 	if l := s.Len(); l != 1 {
 		t.Fatalf(`s.Len() = %d, want 1.`, l)
 	}
-	if !s.Has("b") {
-		t.Fatal(`s.Has("b") = false, want true.`)
+	if res, want := s.Has("b"), true; res != want {
+		t.Fatalf(`s.Has("b") = %t, want %t.`, res, want)
 	}
-	if s.Has("a") {
-		t.Fatal(`s.Has("a") = true, want false.`)
+	if res, want := s.Has("a"), false; res != want {
+		t.Fatalf(`s.Has("a") = %t, want %t.`, res, want)
 	}
 
+	// add a second element, then add it a second time.
 	s.Add("a")
 	t.Log(`s.Add("a")`)
 	s.Add("a")
@@ -138,16 +177,17 @@ func TestDeleteHasIsEmptyLen(t *testing.T) {
 		t.Fatalf(`s.Len() = %d, want 2.`, l)
 	}
 
+	// delete the element that was added twice:
 	s.Delete("a")
 	t.Log(`s.Delete("a")`)
-	if s.IsEmpty() {
-		t.Fatal(`s.IsEmpty() = true, want false.`)
+	if res, want := s.IsEmpty(), false; res != want {
+		t.Fatalf(`s.IsEmpty() = %t, want %t.`, res, want)
 	}
 	if l := s.Len(); l != 1 {
 		t.Fatalf(`s.Len() = %d, want 1.`, l)
 	}
-	if !s.Has("b") {
-		t.Fatal(`s.Has("b") = false, want true.`)
+	if res, want := s.Has("b"), true; res != want {
+		t.Fatalf(`s.Has("b") = %t, want %t.`, res, want)
 	}
 	if res := s.String(); res != _bS {
 		t.Fatalf(`s = %s, want %s.`, res, _bS)
@@ -155,61 +195,236 @@ func TestDeleteHasIsEmptyLen(t *testing.T) {
 }
 
 func TestSlice(t *testing.T) {
+	// empty set should produce empty slice
 	s := New()
 	t.Log(`s := New()`)
 	if l := s.Slice(); len(l) != 0 {
 		t.Fatalf(`s.Slice() = %q, want []`, l)
 	}
+
+	// one element:
 	s.Add("a")
 	t.Log(`s.Add("a")`)
 	if l := s.Slice(); len(l) != 1 || l[0] != "a" {
 		t.Fatalf(`s.Slice() = %q, want ["a"]`, l)
 	}
+
+	// add a second element:
 	s.Add("b")
 	t.Log(`s.Add("b")`)
-	if l := s.Slice(); len(l) != 2 || !(l[0] == "a" && l[1] == "b" ||
-		l[0] == "b" && l[1] == "a") {
+	if l := s.Slice(); len(l) != 2 ||
+		!(l[0] == "a" && l[1] == "b" ||
+			l[0] == "b" && l[1] == "a") {
 		t.Fatalf(`s.Slice() = %q, want ["a" "b"]`, l)
 	}
 }
 
-func TestUnionIntersectionDifferenceSymetricDifference(t *testing.T) {
-}
-
 func TestEqualSubsetDisjoint(t *testing.T) {
+	// empty sets should be equal, subsets, disjoint
 	s1 := New()
 	t.Log(`s1 := New()`)
 	s2 := New()
 	t.Log(`s2 := New()`)
-	if !Equal(s1, s2) {
-		t.Fatalf(`Equal(s1, s2) = false, want true.`)
+	if res, want := Equal(s1, s2), true; res != want {
+		t.Fatalf(`Equal(s1, s2) = %t, want %t.`, res, want)
 	}
-	if !Subset(s1, s2) {
-		t.Fatalf(`Subset(s1, s2) = false, want true.`)
+	if res, want := Subset(s1, s2), true; res != want {
+		t.Fatalf(`Subset(s1, s2) = %t, want %t.`, res, want)
 	}
-
-	s2.Add("b")
-	t.Log(`s2.Add("b")`)
-	if Equal(s1, s2) {
-		t.Fatalf(`Equal(s1, s2) = true, want false.`)
-	}
-	if !Subset(s1, s2) {
-		t.Fatalf(`Subset(s1, s2) = false, want true.`)
-	}
-	if Subset(s2, s1) {
-		t.Fatalf(`Subset(s2, s1) = true, want false.`)
+	if res, want := Disjoint(s1, s2), true; res != want {
+		t.Fatalf(`Disjoint(s1, s2) = %t, want %t.`, res, want)
 	}
 
+	// compare set with single element against empty set
 	s1.Add("a")
 	t.Log(`s1.Add("a")`)
-	s1.Add("b")
-	t.Log(`s1.Add("b")`)
+	if res, want := Equal(s1, s2), false; res != want {
+		t.Fatalf(`Equal(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Subset(s1, s2), false; res != want {
+		t.Fatalf(`Subset(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Subset(s2, s1), true; res != want {
+		t.Fatalf(`Subset(s2, s1) = %t, want %t.`, res, want)
+	}
+	if res, want := Disjoint(s1, s2), true; res != want {
+		t.Fatalf(`Disjoint(s1, s2) = %t, want %t.`, res, want)
+	}
+
+	// compare distinct Sets, both {a}
+	s2.Add("a")
+	t.Log(`s2.Add("a")`)
+	if res, want := Equal(s1, s2), true; res != want {
+		t.Fatalf(`Equal(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Subset(s1, s2), true; res != want {
+		t.Fatalf(`Subset(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Disjoint(s1, s2), false; res != want {
+		t.Fatalf(`Disjoint(s1, s2) = %t, want %t.`, res, want)
+	}
+
+	// compare {a} with {a, b}
 	s2.Add("b")
 	t.Log(`s2.Add("b")`)
-	if Subset(s1, s2) {
-		t.Fatalf(`Subset(s1, s2) = false, want true.`)
+	if res, want := Equal(s1, s2), false; res != want {
+		t.Fatalf(`Equal(s1, s2) = %t, want %t.`, res, want)
 	}
-	if !Subset(s2, s1) {
-		t.Fatalf(`Subset(s2, s1) = false, want true.`)
+	if res, want := Subset(s1, s2), true; res != want {
+		t.Fatalf(`Subset(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Subset(s2, s1), false; res != want {
+		t.Fatalf(`Subset(s2, s1) = %t, want %t.`, res, want)
+	}
+	if res, want := Disjoint(s1, s2), false; res != want {
+		t.Fatalf(`Disjoint(s1, s2) = %t, want %t.`, res, want)
+	}
+
+	// compare {a} with {b}
+	s2.Delete("a")
+	t.Log(`s2.Delete("a")`)
+	if res, want := Equal(s1, s2), false; res != want {
+		t.Fatalf(`Equal(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Subset(s1, s2), false; res != want {
+		t.Fatalf(`Subset(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Subset(s2, s1), false; res != want {
+		t.Fatalf(`Subset(s2, s1) = %t, want %t.`, res, want)
+	}
+	if res, want := Disjoint(s1, s2), true; res != want {
+		t.Fatalf(`Disjoint(s1, s2) = %t, want %t.`, res, want)
+	}
+
+	// compare {a, c} with {b, c}
+	s1.Add("c")
+	t.Log(`s1.Add("c")`)
+	s2.Add("c")
+	t.Log(`s2.Add("c")`)
+	if res, want := Equal(s1, s2), false; res != want {
+		t.Fatalf(`Equal(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Subset(s1, s2), false; res != want {
+		t.Fatalf(`Subset(s1, s2) = %t, want %t.`, res, want)
+	}
+	if res, want := Subset(s2, s1), false; res != want {
+		t.Fatalf(`Subset(s2, s1) = %t, want %t.`, res, want)
+	}
+	if res, want := Disjoint(s1, s2), false; res != want {
+		t.Fatalf(`Disjoint(s1, s2) = %t, want %t.`, res, want)
+	}
+}
+
+func TestUnionIntersectionDifferenceSymetricDifference(t *testing.T) {
+	// operations on empty sets
+	s1 := New()
+	t.Log(`s1 := New()`)
+	s2 := New()
+	t.Log(`s2 := New()`)
+	if res := Intersection(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`Intersection(s1, s2) = %v, want {}.`, res)
+	}
+	if res := Union(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`Union(s1, s2) = %v, want {}.`, res)
+	}
+	if res := Difference(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`Difference(s1, s2) = %v, want {}.`, res)
+	}
+	if res := SymetricDifference(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`SymetricDifference(s1, s2) = %v, want {}.`, res)
+	}
+
+	// {a} op empty set:
+	s1.Add("a")
+	t.Log(`s1.Add("a")`)
+	if res := Intersection(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`Intersection(s1, s2) = %v, want {}.`, res)
+	}
+	if res := Union(s1, s2); !reflect.DeepEqual(res, _a) {
+		t.Fatalf(`Union(s1, s2) = %v, want %v.`, res, _a)
+	}
+	if res := Difference(s1, s2); !reflect.DeepEqual(res, _a) {
+		t.Fatalf(`Difference(s1, s2) = %v, want %v.`, res, _a)
+	}
+	if res := Difference(s2, s1); !res.IsEmpty() {
+		t.Fatalf(`Difference(s2, s1) = %v, want {}.`, res)
+	}
+	if res := SymetricDifference(s1, s2); !reflect.DeepEqual(res, _a) {
+		t.Fatalf(`SymetricDifference(s1, s2) = %v, want %v.`, res, _a)
+	}
+
+	// {a} op {a}:
+	s2.Add("a")
+	t.Log(`s2.Add("a")`)
+	if res := Intersection(s1, s2); !reflect.DeepEqual(res, _a) {
+		t.Fatalf(`Intersection(s1, s2) = %v, want %v.`, res, _a)
+	}
+	if res := Union(s1, s2); !reflect.DeepEqual(res, _a) {
+		t.Fatalf(`Union(s1, s2) = %v, want %v.`, res, _a)
+	}
+	if res := Difference(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`Difference(s1, s2) = %v, want {}.`, res)
+	}
+	if res := SymetricDifference(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`SymetricDifference(s1, s2) = %v, want {}.`, res)
+	}
+
+	// {a} op {a, b}
+	s2.Add("b")
+	t.Log(`s2.Add("b")`)
+	if res := Intersection(s1, s2); !reflect.DeepEqual(res, _a) {
+		t.Fatalf(`Intersection(s1, s2) = %v, want %v.`, res, _a)
+	}
+	if res := Union(s1, s2); !reflect.DeepEqual(res, _ab) {
+		t.Fatalf(`Union(s1, s2) = %v, want %s.`, res, _abS)
+	}
+	if res := Difference(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`Difference(s1, s2) = %v, want {}.`, res)
+	}
+	if res := Difference(s2, s1); !reflect.DeepEqual(res, _b) {
+		t.Fatalf(`Difference(s2, s1) = %v, want %v.`, res, _b)
+	}
+	if res := SymetricDifference(s1, s2); !reflect.DeepEqual(res, _b) {
+		t.Fatalf(`SymetricDifference(s1, s2) = %v, want %v.`, res, _b)
+	}
+
+	// {a} op {b}
+	s2.Delete("a")
+	t.Log(`s2.Delete("a")`)
+	if res := Intersection(s1, s2); !res.IsEmpty() {
+		t.Fatalf(`Intersection(s1, s2) = %v, want {}.`, res)
+	}
+	if res := Union(s1, s2); !reflect.DeepEqual(res, _ab) {
+		t.Fatalf(`Union(s1, s2) = %v, want %s.`, res, _abS)
+	}
+	if res := Difference(s1, s2); !reflect.DeepEqual(res, _a) {
+		t.Fatalf(`Difference(s1, s2) = %v, want %v.`, res, _a)
+	}
+	if res := Difference(s2, s1); !reflect.DeepEqual(res, _b) {
+		t.Fatalf(`Difference(s2, s1) = %v, want %v.`, res, _b)
+	}
+	if res := SymetricDifference(s1, s2); !reflect.DeepEqual(res, _ab) {
+		t.Fatalf(`SymetricDifference(s1, s2) = %v, want %s.`, res, _abS)
+	}
+
+	// {a, c} op {b, c}
+	s1.Add("c")
+	t.Log(`s1.Add("c")`)
+	s2.Add("c")
+	t.Log(`s2.Add("c")`)
+	if res := Intersection(s1, s2); !reflect.DeepEqual(res, _c) {
+		t.Fatalf(`Intersection(s1, s2) = %v, want %v.`, res, _c)
+	}
+	if res := Union(s1, s2); !reflect.DeepEqual(res, _abc) {
+		t.Fatalf(`Union(s1, s2) = %v, want %s.`, res, _abcS)
+	}
+	if res := Difference(s1, s2); !reflect.DeepEqual(res, _a) {
+		t.Fatalf(`Difference(s1, s2) = %v, want %v.`, res, _a)
+	}
+	if res := Difference(s2, s1); !reflect.DeepEqual(res, _b) {
+		t.Fatalf(`Difference(s2, s1) = %v, want %v.`, res, _b)
+	}
+	if res := SymetricDifference(s1, s2); !reflect.DeepEqual(res, _ab) {
+		t.Fatalf(`SymetricDifference(s1, s2) = %v, want %s.`, res, _abS)
 	}
 }
--- custom-set/example.go
+++ custom-set/example.go
@@ -5,12 +5,16 @@ import (
 	"reflect"
 )
 
+// Set represents some properties of a mathematical set.
+// Sets are finite and all elements are unique string values.
 type Set map[string]bool
 
+// New constructs a new Set representing an empty set.
 func New() Set {
 	return Set{}
 }
 
+// NewFromSlice constructs a new Set populated from elements of a slice.
 func NewFromSlice(l []string) Set {
 	s := Set{}
 	for _, e := range l {
@@ -19,26 +23,36 @@ func NewFromSlice(l []string) Set {
 	return s
 }
 
+// Add adds element e to Set s.
+//
+// If e is already in s, s is unchanged.
 func (s Set) Add(e string) {
 	s[e] = true
 }
 
+// Delete deletes element e from Set s.
+//
+// If e is not in s, Delete has no effect.
 func (s Set) Delete(e string) {
 	delete(s, e)
 }
 
+// Has returns true if e is an element of s.
 func (s Set) Has(e string) bool {
 	return s[e]
 }
 
+// IsEmpty returns true if s represents the empty set.
 func (s Set) IsEmpty() bool {
 	return len(s) == 0
 }
 
+// Len returns the cardinality, or number of elements in s.
 func (s Set) Len() int {
 	return len(s)
 }
 
+// Slice constructs a slice populated with the elements of s.
 func (s Set) Slice() []string {
 	l := make([]string, len(s))
 	i := 0
@@ -49,6 +63,7 @@ func (s Set) Slice() []string {
 	return l
 }
 
+// String returns a printable representation of s.
 func (s Set) String() string {
 	r := "{"
 	i := 0
@@ -62,14 +77,18 @@ func (s Set) String() string {
 	return r + "}"
 }
 
+// Disjoint returns true if s1 and s2 have no elements in common.
 func Disjoint(s1, s2 Set) bool {
 	return len(Intersection(s1, s2)) == 0
 }
 
+// Equal returns true if s1 and s2 contain the same elements.
 func Equal(s1, s2 Set) bool {
 	return reflect.DeepEqual(s1, s2)
 }
 
+// Intersection constructs a new Set populated with the elements common to
+// both s1 and s2
 func Intersection(s1, s2 Set) Set {
 	r := Set{}
 	for e := range s1 {
@@ -80,6 +99,8 @@ func Intersection(s1, s2 Set) Set {
 	return r
 }
 
+// Union constructs a new Set populated with elements that appear in s1, s2,
+// or both.
 func Union(s1, s2 Set) Set {
 	r := Set{}
 	for e := range s1 {
@@ -91,6 +112,8 @@ func Union(s1, s2 Set) Set {
 	return r
 }
 
+// Difference returns a new Set populated with elements that appear in s1
+// but not in s2.
 func Difference(s1, s2 Set) Set {
 	r := Set{}
 	for e := range s1 {
@@ -101,6 +124,7 @@ func Difference(s1, s2 Set) Set {
 	return r
 }
 
+// Subset returns true if all elements of s1 are also in s2.
 func Subset(s1, s2 Set) bool {
 	for e := range s1 {
 		if !s2[e] {
@@ -110,6 +134,8 @@ func Subset(s1, s2 Set) bool {
 	return true
 }
 
+// SymetricDifference constructs a new set populated with elements that are
+// members of s1 or s2 but not both.
 func SymetricDifference(s1, s2 Set) Set {
 	r := Set{}
 	for e := range s1 {
