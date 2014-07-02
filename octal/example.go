1f771e21fe4ed17126d3919f1fd311209c0ca2db
Sonia Keys <skeys.cfa.harvard.edu>
2014-07-02 20:08:41 +0000

    octal: fix fmt problem spotted with go vet

--- octal/example.go
+++ octal/example.go
@@ -9,7 +9,7 @@ func ParseOctal(octal string) (int64, error) {
 	for _, digit := range octal {
 		// if any digits aren't octal digits (0-7), return 0
 		if digit < '0' || digit > '7' {
-			return 0, fmt.Errorf("unexpected rune '%c'", octal)
+			return 0, fmt.Errorf("unexpected rune '%c'", digit)
 		}
 		// multiply the current number by 8 (left shift) and add the digit
 		num = num<<3 + int64(digit-'0')
