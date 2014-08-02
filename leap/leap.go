d34725410734d565ef54f981d342410ed50547aa
Alan Shen <sunzenshen@gmail.com>
2014-08-02 01:50:40 -0600

    Tutorial comments
    Explains how to create implementation files for exercises following the
    first one.

--- leap/leap.go
+++ leap/leap.go
@@ -1,4 +1,9 @@
+// For each exercise, create an implementation file with the same package name as the provided tests.
 package leap
 
 func IsLeapYear(i int) bool {
+	// To run the unit tests, run "go test" in the exercise directory, without quotes.
+	// Many exercises come with benchmarks, which can be run with "go test -bench ."
+	// Use the unit test results and the tests themselves to determine which function API's to implement.
+	// Please run "go fmt" on your code before submitting it.
 }
