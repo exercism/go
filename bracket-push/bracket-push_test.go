758eab614f1448f1c2c54a2a91249956d34b619e
Peter Minten <peter@pminten.nl>
2015-03-05 17:16:34 +0100

    Add bracket-push exercise

---
+++ bracket-push/bracket-push_test.go
@@ -0,0 +1,64 @@
+package bracket_push
+
+import (
+	"testing"
+)
+
+var testCases = []struct {
+	input    string
+	expected bool
+}{
+	{
+		input:    "",
+		expected: true,
+	},
+	{
+		input:    "{}",
+		expected: true,
+	},
+	{
+		input:    "{{",
+		expected: false,
+	},
+	{
+		input:    "{}[]",
+		expected: true,
+	},
+	{
+		input:    "{[]}",
+		expected: true,
+	},
+	{
+		input:    "{[)][]}",
+		expected: false,
+	},
+	{
+		input:    "{[]([()])}",
+		expected: true,
+	},
+}
+
+func TestBracket(t *testing.T) {
+	for _, tt := range testCases {
+		actual, err := Bracket(tt.input)
+		// We don't expect errors for any of the test cases
+		if err != nil {
+			t.Fatalf("Bracket(%q) returned error %q.  Error not expected.", tt.input, err)
+		}
+		if actual != tt.expected {
+			t.Fatalf("Bracket(%q) was expected to return %v but returned %v.",
+				tt.input, tt.expected, actual)
+		}
+	}
+}
+
+func BenchmarkBracket(b *testing.B) {
+	b.StopTimer()
+	for _, tt := range testCases {
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			Bracket(tt.input)
+		}
+		b.StopTimer()
+	}
+}
