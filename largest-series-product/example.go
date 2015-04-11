5e76936da22af26b64752f24d8e7555cc9311748
Sonia Keys <soniakeys@gmail.com>
2015-04-11 19:12:47 -0400

    largest series product: test against int64 values

--- largest-series-product/example.go
+++ largest-series-product/example.go
@@ -2,20 +2,22 @@ package lsproduct
 
 import "fmt"
 
-func LargestSeriesProduct(digits string, span int) (int, error) {
+const TestVersion = 1
+
+func LargestSeriesProduct(digits string, span int) (int64, error) {
 	if len(digits) < span {
 		return 0, fmt.Errorf("len(%s) < span: %d < %d", digits, len(digits), span)
 	}
-	v := make([]int, len(digits))
+	v := make([]int64, len(digits))
 	for i, r := range digits {
 		if r < '0' || r > '9' {
 			return 0, fmt.Errorf("input %q contains non-digits", digits)
 		}
-		v[i] = int(r - '0')
+		v[i] = int64(r - '0')
 	}
-	maxsp := 1
+	maxsp := int64(1)
 	for i, last := 0, len(v)-span+1; i < last; i++ {
-		sp := 1
+		sp := int64(1)
 		for _, d := range v[i : i+span] {
 			sp *= d
 		}
