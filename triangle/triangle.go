9667e34b9eaa3f13e412a7eef9b525901445fbd0
Sonia Keys <soniakeys@gmail.com>
2015-10-24 11:24:56 -0400

    triangle stub

---
+++ triangle/triangle.go
@@ -0,0 +1,15 @@
+//+build !example
+
+// Code this function.
+func KindFromSides(a, b, c float64) Kind
+
+// Notice it returns this type.  Pick something suitable.
+type Kind
+
+// Pick values for the following identifiers used by the test program.
+Equ // equilateral
+Iso // isosceles
+Sca // scalene
+NaT // not a triangle
+
+// Organize your code for readability.
