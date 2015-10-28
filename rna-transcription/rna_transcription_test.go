565238cf154af1c1fdcc48b0f12dc984af146bf1
Sonia Keys <soniakeys@gmail.com>
2015-10-28 17:52:23 -0400

    rna-transcription update:  common test cases, TestVersion, error message arg order consistency, simplify benchmark.

--- rna-transcription/rna_transcription_test.go
+++ rna-transcription/rna_transcription_test.go
@@ -1,39 +1,28 @@
 package strand
 
-import (
-	"testing"
-)
+import "testing"
 
-type rnaTest struct {
-	input    string
-	expected string
-}
+const testVersion = 1
 
-var rnaTests = []rnaTest{
-	{"C", "G"},
-	{"G", "C"},
-	{"A", "U"},
-	{"T", "A"},
-	{"ACGTGGTCTTAA", "UGCACCAGAAUU"},
-}
+// Retired testVersions
+// (none) 3e164bf0858b6bf7dbed8ff8a8d487105a41ada6
 
 func TestRnaTranscription(t *testing.T) {
+	if TestVersion != testVersion {
+		t.Fatalf("Found TestVersion = %v, want %v", TestVersion, testVersion)
+	}
 	for _, test := range rnaTests {
-		actual := ToRna(test.input)
-		if actual != test.expected {
-			t.Errorf("ToRna(%s): expected %s, actual %s", test.input, test.expected, actual)
+		if actual := ToRna(test.input); actual != test.expected {
+			t.Errorf("ToRna(%s): %s, expected %s",
+				test.input, actual, test.expected)
 		}
 	}
 }
 
 func BenchmarkRnaTranscription(b *testing.B) {
-	b.StopTimer()
-	for _, test := range rnaTests {
-		b.StartTimer()
-
-		for i := 0; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {
+		for _, test := range rnaTests {
 			ToRna(test.input)
 		}
-		b.StopTimer()
 	}
 }
