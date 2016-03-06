baaa605f3b10952e011f3c70c58062bdf441e756
Peter Tseng <pht24@cornell.edu>
2016-03-06 10:36:52 -0800

    poker: deduplicate cards from hands
    I'm still undecided on whether duplicate cards in the same hand should
    be an error, but it's at least reasonable to me that tests should only
    test one thing at a time. So regardless of whether they are an error, we
    can have a single test that tests that behavior instead of a bunch of
    them.
    
    Fixes #250

--- exercises/poker/poker_test.go
+++ exercises/poker/poker_test.go
@@ -88,17 +88,17 @@ var validTestCases = []struct {
 		hands: []string{
 			"4♢ 3♤ 4♤ J♤ K♤",
 			"A♡ K♡ J♢ 10♧ 9♡",
-			"3♢ 8♡ 3♢ 3♧ 9♧",
+			"3♢ 8♡ 3♡ 3♧ 9♧",
 			"2♢ 8♡ 5♢ 2♡ 8♧",
 		},
-		best: []string{"3♢ 8♡ 3♢ 3♧ 9♧"},
+		best: []string{"3♢ 8♡ 3♡ 3♧ 9♧"},
 	},
 	{
 		name: "best three of a kind",
 		hands: []string{
 			"4♢ 3♤ 4♤ J♤ 4♡",
 			"A♡ K♡ J♢ 10♧ 9♡",
-			"3♢ 8♡ 3♢ 3♧ 9♧",
+			"3♢ 8♡ 3♡ 3♧ 9♧",
 			"2♢ 8♡ 5♢ 2♡ 8♧",
 		},
 		best: []string{"4♢ 3♤ 4♤ J♤ 4♡"},
@@ -108,7 +108,7 @@ var validTestCases = []struct {
 		hands: []string{
 			"4♢ 3♤ 4♤ J♤ K♤",
 			"Q♡ K♡ J♢ 10♧ 9♡",
-			"3♢ 8♡ 3♢ 3♧ 9♧",
+			"3♢ 8♡ 3♡ 3♧ 9♧",
 			"2♢ 8♡ 5♢ 2♡ 8♧",
 		},
 		best: []string{"Q♡ K♡ J♢ 10♧ 9♡"},
@@ -118,7 +118,7 @@ var validTestCases = []struct {
 		hands: []string{
 			"4♢ 3♤ 4♤ J♤ K♤",
 			"2♤ 3♡ A♤ 5♤ 4♤",
-			"3♢ 8♡ 3♢ 3♧ 9♧",
+			"3♢ 8♡ 3♡ 3♧ 9♧",
 			"2♢ 8♡ 5♢ 2♡ 8♧",
 		},
 		best: []string{"2♤ 3♡ A♤ 5♤ 4♤"},
@@ -138,7 +138,7 @@ var validTestCases = []struct {
 		hands: []string{
 			"4♤ 3♤ 8♤ J♤ K♤",
 			"Q♡ K♡ J♢ 10♧ 9♡",
-			"3♢ 8♡ 3♢ 3♧ 9♧",
+			"3♢ 8♡ 3♡ 3♧ 9♧",
 			"2♢ 8♡ 5♢ 2♡ 8♧",
 		},
 		best: []string{"4♤ 3♤ 8♤ J♤ K♤"},
@@ -148,10 +148,10 @@ var validTestCases = []struct {
 		hands: []string{
 			"4♤ 3♤ 8♤ J♤ K♤",
 			"Q♡ K♡ J♢ 10♧ 9♡",
-			"3♢ 8♢ A♢ 3♢ 7♢",
+			"3♢ 8♢ A♢ 4♢ 7♢",
 			"2♢ 8♡ 5♢ 2♡ 8♧",
 		},
-		best: []string{"3♢ 8♢ A♢ 3♢ 7♢"},
+		best: []string{"3♢ 8♢ A♢ 4♢ 7♢"},
 	},
 	{
 		name: "full house beats lower",
@@ -159,7 +159,7 @@ var validTestCases = []struct {
 			"4♤ 3♤ 8♤ J♤ K♤",
 			"2♢ 8♡ 8♢ 2♡ 8♧",
 			"Q♡ K♡ J♢ 10♧ 9♡",
-			"3♢ A♡ 3♢ 3♧ A♧",
+			"3♢ A♡ 3♡ 3♧ A♧",
 		},
 		best: []string{"2♢ 8♡ 8♢ 2♡ 8♧"},
 	},
@@ -168,8 +168,8 @@ var validTestCases = []struct {
 		hands: []string{
 			"4♤ 3♤ 8♤ J♤ K♤",
 			"2♢ 8♡ 8♢ 2♡ 8♧",
-			"5♡ 5♢ A♢ 5♧ A♢",
-			"3♢ A♡ 3♢ 3♧ A♧",
+			"5♡ 5♢ A♧ 5♧ A♢",
+			"3♢ A♡ 3♡ 3♧ A♧",
 		},
 		best: []string{"2♢ 8♡ 8♢ 2♡ 8♧"},
 	},
@@ -216,8 +216,8 @@ var validTestCases = []struct {
 	},
 	{
 		name:  "tie for best pair",
-		hands: []string{"4♡ 2♡ 5♧ 4♢ 10♡", "4♧ 10♢ 5♤ 2♤ 4♧"},
-		best:  []string{"4♡ 2♡ 5♧ 4♢ 10♡", "4♧ 10♢ 5♤ 2♤ 4♧"},
+		hands: []string{"4♡ 2♡ 5♧ 4♢ 10♡", "4♧ 10♢ 5♤ 2♤ 4♤"},
+		best:  []string{"4♡ 2♡ 5♧ 4♢ 10♡", "4♧ 10♢ 5♤ 2♤ 4♤"},
 	},
 	{
 		name: "tie of three",
