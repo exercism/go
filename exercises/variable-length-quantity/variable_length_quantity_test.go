914be72ca909e5b7580e2c1fc6d6459efd709250
Matt Aimonetti <mattaimonetti@gmail.com>
2016-04-27 20:19:00 -0700

    remove the test/exercise documentation from the exercise since it will be moved to the problem

--- exercises/variable-length-quantity/variable_length_quantity_test.go
+++ exercises/variable-length-quantity/variable_length_quantity_test.go
@@ -5,22 +5,6 @@ import (
 	"testing"
 )
 
-/*
-The goal of this exercise is to implement
-VLQ encoding/decoding as described here: https://en.wikipedia.org/wiki/Variable-length_quantity
-
-In short, the goal of this encoding is to save encode integer values in a way that would save bytes.
-Only the first 7 bits of each byte is significant (right-justified; sort of like an ASCII byte).
-So, if you have a 32-bit value, you have to unpack it into a series of 7-bit bytes.
-Of course, you will have a variable number of bytes depending upon your integer.
-To indicate which is the last byte of the series, you leave bit #7 clear.
-In all of the preceding bytes, you set bit #7.
-
-So, if an integer is between `0-127`, it can be represented as one byte.
-The largest integer allowed is `0FFFFFFF`, which translates to 4 bytes variable length.
-Here are examples of delta-times as 32-bit values, and the variable length quantities that they translate to:
-*/
-
 func TestEncodeDecodeVarint(t *testing.T) {
 	testCases := []struct {
 		input  []byte
