12ae9e2361e784e80aa1b8e8d296903d8e1f349c
Abhijeet Kasurde <akasurde@redhat.com>
2016-10-02 11:52:02 +0530

    Acronym: Add acronym excercise
    This example adds excercise for converting a long name into
    three letter acronyms
    
    Fixes https://github.com/exercism/xgo/issues/382
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

---
+++ exercises/acronym/example.go
@@ -0,0 +1,21 @@
+package acronym
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+const testVersion = 1
+
+func abbreviate(s string) string {
+	regex := regexp.MustCompile("[A-Z]+[a-z]*|[a-z]+")
+	words := regex.FindAllString(s, -1)
+	abbr := []string{}
+
+	for _, word := range words {
+		abbr = append(abbr, string(word[0]))
+	}
+
+	return fmt.Sprintf("%s", strings.ToUpper(strings.Join(abbr, "")))
+}
