a5d91a064960cd2495d1ed47e39f07242c657923
Ngo The Trung <ngo.the.trung.aczne@gmail.com>
2016-10-02 21:50:41 +0800

    Variable length encoding: Test encoding size

--- exercises/variable-length-quantity/variable_length_quantity_test.go
+++ exercises/variable-length-quantity/variable_length_quantity_test.go
@@ -9,25 +9,30 @@ func TestEncodeDecodeVarint(t *testing.T) {
 	testCases := []struct {
 		input  []byte
 		output uint32
+		size   int
 	}{
-		0: {[]byte{0x7F}, 127},
-		1: {[]byte{0x81, 0x00}, 128},
-		2: {[]byte{0xC0, 0x00}, 8192},
-		3: {[]byte{0xFF, 0x7F}, 16383},
-		4: {[]byte{0x81, 0x80, 0x00}, 16384},
-		5: {[]byte{0xFF, 0xFF, 0x7F}, 2097151},
-		6: {[]byte{0x81, 0x80, 0x80, 0x00}, 2097152},
-		7: {[]byte{0xC0, 0x80, 0x80, 0x00}, 134217728},
-		8: {[]byte{0xFF, 0xFF, 0xFF, 0x7F}, 268435455},
+		0: {[]byte{0x7F}, 127, 1},
+		1: {[]byte{0x81, 0x00}, 128, 1},
+		2: {[]byte{0xC0, 0x00}, 8192, 1},
+		3: {[]byte{0xFF, 0x7F}, 16383, 1},
+		4: {[]byte{0x81, 0x80, 0x00}, 16384, 2},
+		5: {[]byte{0xFF, 0xFF, 0x7F}, 2097151, 2},
+		6: {[]byte{0x81, 0x80, 0x80, 0x00}, 2097152, 3},
+		7: {[]byte{0xC0, 0x80, 0x80, 0x00}, 134217728, 3},
+		8: {[]byte{0xFF, 0xFF, 0xFF, 0x7F}, 268435455, 3},
 
-		9:  {[]byte{0x82, 0x00}, 256},
-		10: {[]byte{0x81, 0x10}, 144},
+		9:  {[]byte{0x82, 0x00}, 256, 1},
+		10: {[]byte{0x81, 0x10}, 144, 1},
 	}
 
 	for i, tc := range testCases {
 		t.Logf("test case %d - %#v\n", i, tc.input)
-		if o, _ := DecodeVarint(tc.input); o != tc.output {
+		o, size := DecodeVarint(tc.input)
+		if o != tc.output {
 			t.Fatalf("expected %d\ngot\n%d\n", tc.output, o)
+		}
+		if size != tc.size {
+			t.Fatalf("expected encoding size of %d bytes\ngot %d bytes\n", tc.size, size)
 		}
 		if encoded := EncodeVarint(tc.output); bytes.Compare(encoded, tc.input) != 0 {
 			t.Fatalf("%d - expected %#v\ngot\n%#v\n", tc.output, tc.input, encoded)
