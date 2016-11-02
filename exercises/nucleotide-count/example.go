07563646e02132931669b5753e6e9dec61b9c480
ferhat elmas <elmas.ferhat@gmail.com>
2016-11-03 00:31:07 +0100

    nucleotide-count: Add comments to example

--- exercises/nucleotide-count/example.go
+++ exercises/nucleotide-count/example.go
@@ -7,10 +7,13 @@ import (
 
 const testVersion = 1
 
+// Histogram is a mapping from nucleotide to its count in given DNA
 type Histogram map[byte]int
 
+// DNA is a list of nucleotides
 type DNA string
 
+// Count counts number of occurrences of given nucleotide in given DNA
 func (dna DNA) Count(nucleotide byte) (count int, err error) {
 	validNucleotides := "ACGT"
 	if !strings.Contains(validNucleotides, string(nucleotide)) {
@@ -20,6 +23,8 @@ func (dna DNA) Count(nucleotide byte) (count int, err error) {
 	return strings.Count(string(dna), string(nucleotide)), nil
 }
 
+// Counts generates a histogram of valid nucleotides in given DNA.
+// Returns error if DNA contains invalid nucleotide.
 func (dna DNA) Counts() Histogram {
 	a, _ := dna.Count('A')
 	c, _ := dna.Count('C')
