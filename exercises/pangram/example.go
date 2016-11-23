1e09e833d880eca52662afd39a4b3f1643313222
Abhijeet <akasurde@redhat.com>
2016-11-23 21:13:25 +0530

    Pangram: Add Pangram exercise (#396)
    * Pangram: Add Pangram exercise
    
    This PR adds exercise for determining if a sentence
    is a pangram or not.
    
    Fixes https://github.com/exercism/xgo/issues/395
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

---
+++ exercises/pangram/example.go
@@ -0,0 +1,32 @@
+package pangram
+
+import (
+	"strings"
+)
+
+const testVersion = 1
+
+func IsPangram(s string) bool {
+	lowerString := strings.ToLower(s)
+	var check [26]bool
+	var count int
+	for _, c := range lowerString {
+		if c >= 'a' {
+			if c > 'z' {
+				continue
+			}
+			c -= 97
+		} else {
+			continue
+		}
+
+		if !check[c] {
+			if count == 25 {
+				return true
+			}
+			check[c] = true
+			count++
+		}
+	}
+	return false
+}
---
+++ exercises/pangram/pangram_test.go
@@ -0,0 +1,43 @@
+package pangram
+
+import (
+	"testing"
+)
+
+const targetTestVersion = 1
+
+type testCase struct {
+	input         string
+	expected      bool
+	failureReason string
+}
+
+var testCases = []testCase{
+	{"", false, "sentence empty"},
+	{"The quick brown fox jumps over the lazy dog", true, ""},
+	{"a quick movement of the enemy will jeopardize five gunboats", false, "missing character 'x'"},
+	{"the quick brown fish jumps over the lazy dog", false, "another missing character 'x'"},
+	{"the 1 quick brown fox jumps over the 2 lazy dogs", true, ""},
+	{"7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog", false, "missing letters replaced by numbers"},
+	{"\"Five quacking Zephyrs jolt my wax bed.\"", true, ""},
+	{"Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.", true, ""},
+	{"Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства.", false, "Panagram in alphabet other than ASCII"},
+}
+
+func TestTestVersion(t *testing.T) {
+	if testVersion != targetTestVersion {
+		t.Errorf("Found testVersion = %v, want %v.", testVersion, targetTestVersion)
+	}
+}
+
+func TestPangram(t *testing.T) {
+	for _, test := range testCases {
+		actual := IsPangram(test.input)
+		if actual != test.expected {
+			t.Errorf("Pangram test [%s], expected [%t], actual [%t]", test.input, test.expected, actual)
+			if !test.expected {
+				t.Logf("[%s] should not be a pangram because : %s\n", test.input, test.failureReason)
+			}
+		}
+	}
+}
