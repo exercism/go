fe955bc4d30182c19111a113cd697cc1d9e8313b
Rob Phoenix <robphoenix@protonmail.com>
2017-01-17 13:40:48 +0000

    custom-set: update, remove & re-order tests
    updated tests for custom-set #328
    
    for reference:
    https://github.com/exercism/x-common/pull/232
    https://github.com/exercism/x-common/pull/257

--- exercises/custom-set/example.go
+++ exercises/custom-set/example.go
@@ -2,12 +2,9 @@
 
 package stringset
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
-const testVersion = 3
+const testVersion = 4
 
 // Set represents some properties of a mathematical set.
 // Sets are finite and all elements are unique string values.
@@ -27,46 +24,6 @@ func NewFromSlice(l []string) Set {
 	return s
 }
 
-// Add adds element e to Set s.
-//
-// If e is already in s, s is unchanged.
-func (s Set) Add(e string) {
-	s[e] = true
-}
-
-// Delete deletes element e from Set s.
-//
-// If e is not in s, Delete has no effect.
-func (s Set) Delete(e string) {
-	delete(s, e)
-}
-
-// Has returns true if e is an element of s.
-func (s Set) Has(e string) bool {
-	return s[e]
-}
-
-// IsEmpty returns true if s represents the empty set.
-func (s Set) IsEmpty() bool {
-	return len(s) == 0
-}
-
-// Len returns the cardinality, or number of elements in s.
-func (s Set) Len() int {
-	return len(s)
-}
-
-// Slice constructs a slice populated with the elements of s.
-func (s Set) Slice() []string {
-	l := make([]string, len(s))
-	i := 0
-	for e := range s {
-		l[i] = e
-		i++
-	}
-	return l
-}
-
 // String returns a printable representation of s.
 func (s Set) String() string {
 	r := "{"
@@ -81,14 +38,45 @@ func (s Set) String() string {
 	return r + "}"
 }
 
+// IsEmpty returns true if s represents the empty set.
+func (s Set) IsEmpty() bool {
+	return len(s) == 0
+}
+
+// Has returns true if e is an element of s.
+func (s Set) Has(e string) bool {
+	return s[e]
+}
+
+// Add adds element e to Set s.
+// If e is already in s, s is unchanged.
+func (s Set) Add(e string) {
+	s[e] = true
+}
+
+// Subset returns true if all elements of s1 are also in s2.
+func Subset(s1, s2 Set) bool {
+	for e := range s1 {
+		if !s2[e] {
+			return false
+		}
+	}
+	return true
+}
+
 // Disjoint returns true if s1 and s2 have no elements in common.
 func Disjoint(s1, s2 Set) bool {
-	return len(Intersection(s1, s2)) == 0
+	for e := range s1 {
+		if s2[e] {
+			return false
+		}
+	}
+	return true
 }
 
 // Equal returns true if s1 and s2 contain the same elements.
 func Equal(s1, s2 Set) bool {
-	return reflect.DeepEqual(s1, s2)
+	return Subset(s1, s2) && Subset(s2, s1)
 }
 
 // Intersection constructs a new Set populated with the elements common to
@@ -99,19 +87,6 @@ func Intersection(s1, s2 Set) Set {
 		if s2[e] {
 			r[e] = true
 		}
-	}
-	return r
-}
-
-// Union constructs a new Set populated with elements that appear in s1, s2,
-// or both.
-func Union(s1, s2 Set) Set {
-	r := Set{}
-	for e := range s1 {
-		r[e] = true
-	}
-	for e := range s2 {
-		r[e] = true
 	}
 	return r
 }
@@ -128,29 +103,15 @@ func Difference(s1, s2 Set) Set {
 	return r
 }
 
-// Subset returns true if all elements of s1 are also in s2.
-func Subset(s1, s2 Set) bool {
-	for e := range s1 {
-		if !s2[e] {
-			return false
-		}
-	}
-	return true
-}
-
-// SymmetricDifference constructs a new set populated with elements that are
-// members of s1 or s2 but not both.
-func SymmetricDifference(s1, s2 Set) Set {
+// Union constructs a new Set populated with elements that appear in s1, s2,
+// or both.
+func Union(s1, s2 Set) Set {
 	r := Set{}
 	for e := range s1 {
-		if !s2[e] {
-			r[e] = true
-		}
+		r[e] = true
 	}
 	for e := range s2 {
-		if !s1[e] {
-			r[e] = true
-		}
+		r[e] = true
 	}
 	return r
 }
--- exercises/custom-set/example_slice.go
+++ exercises/custom-set/example_slice.go
@@ -5,19 +5,25 @@ package stringset
 // Example based on slices rather than maps, to validate that such a solution
 // can pass the tests.
 //
-// The API is a little akward for this because it has value receivers but
+// The API is a little awkward for this because it has value receivers but
 // a pointer in a struct works okay.
 
 import "fmt"
 
-const testVersion = 3
+const testVersion = 4
 
-type Set struct{ s *[]string }
+// Set represents some properties of a mathematical set.
+// Sets are finite and all elements are unique string values.
+type Set struct {
+	s *[]string
+}
 
+// New constructs a new Set representing an empty set.
 func New() Set {
 	return Set{new([]string)}
 }
 
+// NewFromSlice constructs a new Set populated from elements of a slice.
 func NewFromSlice(l []string) Set {
 	s := New()
 	for _, e := range l {
@@ -26,48 +32,7 @@ func NewFromSlice(l []string) Set {
 	return s
 }
 
-func (s Set) Add(v string) {
-	for _, e := range *s.s {
-		if e == v {
-			return
-		}
-	}
-	*s.s = append(*s.s, v)
-}
-
-func (s Set) Delete(v string) {
-	ss := *s.s
-	for i, e := range ss {
-		if e == v {
-			last := len(ss) - 1
-			ss[i] = ss[last]
-			*s.s = ss[:last]
-			return
-		}
-	}
-}
-
-func (s Set) Has(v string) bool {
-	for _, e := range *s.s {
-		if e == v {
-			return true
-		}
-	}
-	return false
-}
-
-func (s Set) IsEmpty() bool {
-	return len(*s.s) == 0
-}
-
-func (s Set) Len() int {
-	return len(*s.s)
-}
-
-func (s Set) Slice() []string {
-	return append([]string{}, *s.s...)
-}
-
+// String returns a printable representation of s.
 func (s Set) String() string {
 	r := "{"
 	i := 0
@@ -81,46 +46,30 @@ func (s Set) String() string {
 	return r + "}"
 }
 
-func Disjoint(s1, s2 Set) bool {
-	for _, v := range *s1.s {
-		if s2.Has(v) {
-			return false
+// IsEmpty returns true if s represents the empty set.
+func (s Set) IsEmpty() bool {
+	return len(*s.s) == 0
+}
+
+// Has returns true if e is an element of s.
+func (s Set) Has(v string) bool {
+	for _, e := range *s.s {
+		if e == v {
+			return true
 		}
 	}
-	return true
+	return false
 }
 
-func Equal(s1, s2 Set) bool {
-	return s1.Len() == s2.Len() && Subset(s1, s2)
-}
-
-func Intersection(s1, s2 Set) Set {
-	var i []string
-	for _, v := range *s1.s {
-		if s2.Has(v) {
-			i = append(i, v)
+// Add adds element e to Set s.
+// If e is already in s, s is unchanged.
+func (s Set) Add(v string) {
+	for _, e := range *s.s {
+		if e == v {
+			return
 		}
 	}
-	return Set{&i}
-}
-
-func Union(s1, s2 Set) Set {
-	s := s1.Slice()
-	u := Set{&s}
-	for _, v := range *s2.s {
-		u.Add(v)
-	}
-	return u
-}
-
-func Difference(s1, s2 Set) Set {
-	var d []string
-	for _, v := range *s1.s {
-		if !s2.Has(v) {
-			d = append(d, v)
-		}
-	}
-	return Set{&d}
+	*s.s = append(*s.s, v)
 }
 
 // Subset returns true if all elements of s1 are also in s2.
@@ -133,19 +82,52 @@ func Subset(s1, s2 Set) bool {
 	return true
 }
 
-// SymmetricDifference constructs a new set populated with elements that are
-// members of s1 or s2 but not both.
-func SymmetricDifference(s1, s2 Set) Set {
-	var s []string
+// Disjoint returns true if s1 and s2 have no elements in common.
+func Disjoint(s1, s2 Set) bool {
+	for _, v := range *s1.s {
+		if s2.Has(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal returns true if s1 and s2 contain the same elements.
+func Equal(s1, s2 Set) bool {
+	return Subset(s1, s2) && Subset(s2, s1)
+}
+
+// Intersection constructs a new Set populated with the elements common to
+// both s1 and s2
+func Intersection(s1, s2 Set) Set {
+	var i []string
+	for _, v := range *s1.s {
+		if s2.Has(v) {
+			i = append(i, v)
+		}
+	}
+	return Set{&i}
+}
+
+// Difference returns a new Set populated with elements that appear in s1
+// but not in s2.
+func Difference(s1, s2 Set) Set {
+	var d []string
 	for _, v := range *s1.s {
 		if !s2.Has(v) {
-			s = append(s, v)
+			d = append(d, v)
 		}
 	}
+	return Set{&d}
+}
+
+// Union constructs a new Set populated with elements that appear in s1, s2,
+// or both.
+func Union(s1, s2 Set) Set {
+	s := append([]string{}, *s1.s...)
+	u := Set{&s}
 	for _, v := range *s2.s {
-		if !s1.Has(v) {
-			s = append(s, v)
-		}
+		u.Add(v)
 	}
-	return Set{&s}
+	return u
 }
