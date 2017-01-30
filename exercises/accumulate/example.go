061f8575d0d4709f4d0d42f723b2f5a6f41b7aac
Tom Leen <tml@leentop.thomasleen.com>
2017-01-29 20:22:09 -0500

    moved target test versioning test before problem specific test

--- exercises/accumulate/example.go
+++ exercises/accumulate/example.go
@@ -1,5 +1,7 @@
 package accumulate
 
+const testVersion = 1
+
 func Accumulate(s []string, f func(st string) string) (result []string) {
 	for _, v := range s {
 		result = append(result, []string{f(v)}...)
