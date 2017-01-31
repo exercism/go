6f6a391a7de9689535bff53a8dcc9c6ca952170a
Tom Leen <tml@leentop.thomasleen.com>
2017-01-31 14:24:10 -0500

    added test versioning consistency to accumulate, see #470

--- exercises/accumulate/example.go
+++ exercises/accumulate/example.go
@@ -1,5 +1,8 @@
 package accumulate
 
+// TestVersion constant declaration - part of the boilerplate for exercism.
+const testVersion = 1
+
 func Accumulate(s []string, f func(st string) string) (result []string) {
 	for _, v := range s {
 		result = append(result, []string{f(v)}...)
