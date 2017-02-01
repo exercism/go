ace6fbb6f6f79c38a4935f1a7e15b73526679424
Peter Tseng <pht24@cornell.edu>
2017-01-31 19:19:58 -0800

    hello-world: simplify to unconditional "Hello, World!"
    The introductory exercise should require as little Go knowledge as
    necessary. We'd like to introduce `go test` and `testVersion` here. We
    skip introducing the empty string as no string, as this is more specific
    knowledge.
    
    https://github.com/exercism/x-common/issues/520

--- exercises/hello-world/example.go
+++ exercises/hello-world/example.go
@@ -1,12 +1,8 @@
 package greeting
 
-const testVersion = 3
+const testVersion = 4
 
-// HelloWorld says hello to given name.
-// If no name is passed, it greets the world.
-func HelloWorld(name string) string {
-	if name == "" {
-		name = "World"
-	}
-	return "Hello, " + name + "!"
+// HelloWorld greets the world.
+func HelloWorld() string {
+	return "Hello, World!"
 }
--- exercises/hello-world/hello_test.go
+++ exercises/hello-world/hello_test.go
@@ -2,25 +2,20 @@ package greeting
 
 import "testing"
 
-// Define a function HelloWorld(string) string.
+// Define a function named HelloWorld that takes no arguments,
+// and returns a string.
+// In other words, define a function with the following signature:
+// HelloWorld() string
 //
 // Also define a testVersion with a value that matches
 // the targetTestVersion here.
 
-const targetTestVersion = 3
+const targetTestVersion = 4
 
 func TestHelloWorld(t *testing.T) {
-	tests := []struct {
-		name, expected string
-	}{
-		{"", "Hello, World!"},
-		{"Gopher", "Hello, Gopher!"},
-		{"ゴーファー", "Hello, ゴーファー!"},
-	}
-	for _, test := range tests {
-		if observed := HelloWorld(test.name); observed != test.expected {
-			t.Fatalf("HelloWorld(%s) = %v, want %v", test.name, observed, test.expected)
-		}
+	expected := "Hello, World!"
+	if observed := HelloWorld(); observed != expected {
+		t.Fatalf("HelloWorld() = %v, want %v", observed, expected)
 	}
 
 	if testVersion != targetTestVersion {
