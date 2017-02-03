e032417b2a5ca998d840a52b5d4d535119740018
Rob Phoenix <robphoenix@protonmail.com>
2017-02-03 11:02:02 +0000

    pangram: add benchmark test

--- exercises/pangram/pangram_test.go
+++ exercises/pangram/pangram_test.go
@@ -41,3 +41,11 @@ func TestPangram(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkPangram(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, test := range testCases {
+			IsPangram(test.input)
+		}
+	}
+}
