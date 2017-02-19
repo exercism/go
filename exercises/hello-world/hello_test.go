843db1a065b4e412e81fbbe2d363920ae703f06c
Tom Leen <tml@leentop.thomasleen.com>
2017-02-19 12:38:15 -0500

    hello-world: add more detailed explanation concerning the test consistency setup
    Closes #515

--- exercises/hello-world/hello_test.go
+++ exercises/hello-world/hello_test.go
@@ -3,12 +3,16 @@ package greeting
 import "testing"
 
 // Define a function HelloWorld(string) string.
-//
-// Also define a testVersion with a value that matches
-// the targetTestVersion here.
 
+// In any exercise solution using these tests you must also define a
+// testVersion constant with a value that matches the targetTestVersion
+// here.
 const targetTestVersion = 3
 
+// TestTestVersion is used to ensure the exercise solution is considered
+// compatable with the rest of the tests. It should be the initial testing
+// function in any test suite. If there is an incompatibility the tests can
+// be considered unreliable therefore this function will abort the testing.
 func TestTestVersion(t *testing.T) {
 	if testVersion != targetTestVersion {
 		t.Fatalf("Found testVersion = %v, want %v", testVersion, targetTestVersion)
--- exercises/hello-world/hello_world.go
+++ exercises/hello-world/hello_world.go
@@ -10,6 +10,15 @@ package greeting
 // after you have posted this code to the Exercism site -- reviewers
 // will see that your code can't necessarily be expected to pass the
 // current test suite because it was written to an earlier test version.
+//
+// This is a convention done for Exercism exercises, it is not a requirement
+// of the Go programming language.
+//
+// This test versioning setup will be common to all the exercises in the
+// Go language track. When crafting your own solution file from scratch you
+// will be expected to add this constant or the initial test will fail.
+// The version number you should use will be found in the constant
+// targetTestVersion in the test file.
 const testVersion = 3
 
 // HelloWorld needs a comment documenting it as package does.
