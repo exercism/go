fe4183693cc8553904cc956eea90142d895c5d76
Tom Leen <tl@thomasleen.com>
2017-02-20 17:24:55 -0500

    hello-world: add more detailed explanation concerning the test consistency setup (#532)
    Closes #515

--- exercises/hello-world/hello_world.go
+++ exercises/hello-world/hello_world.go
@@ -10,6 +10,16 @@ package greeting
 // after you have posted this code to the Exercism site -- reviewers
 // will see that your code can't necessarily be expected to pass the
 // current test suite because it was written to an earlier test version.
+//
+// This is a convention done for Exercism exercises in the Go language track,
+// it is not a requirement of the Go programming language.
+//
+// This test versioning setup will be common to all the exercises in the
+// Go language track. When crafting your own solution file from scratch you
+// will be expected to add this constant or the initial test will fail.
+// The version number you should use will be found in the constant
+// "targetTestVersion" in the test file, see ./hello_test.go for more
+// information.
 const testVersion = 4
 
 // HelloWorld needs a comment documenting it as package does.
