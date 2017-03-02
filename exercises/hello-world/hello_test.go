a8ac63cafdbed045000bb37c314b47d226d979c3
Rob Phoenix <rob@robphoenix.com>
2017-03-02 10:26:58 +0000

    hello-world: add example benchmarking function

--- exercises/hello-world/hello_test.go
+++ exercises/hello-world/hello_test.go
@@ -33,3 +33,25 @@ func TestHelloWorld(t *testing.T) {
 		t.Fatalf("HelloWorld() = %v, want %v", observed, expected)
 	}
 }
+
+// BenchmarkHelloWorld() is a benchmarking function. These functions follow the
+// form `func BenchmarkXxx(*testing.B)` and can be used to test the performance
+// of your implementation. They may not be present in every exercise, but when
+// they are you can run them by including the `-bench` flag with the `go test`
+// command, like so: `go test -bench .`
+//
+// You will see output similar to the following:
+//
+// BenchmarkHelloWorld   	2000000000	         0.46 ns/op
+//
+// This means that the loop ran 2000000000 times at a speed of 0.46 ns per loop.
+//
+// While benchmarking can be useful to compare different iterations of the same
+// exercise, keep in mind that others will run the same benchmarks on different
+// machines, with different specs, so the results from these benchmark tests may
+// vary.
+func BenchmarkHelloWorld(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HelloWorld()
+	}
+}
