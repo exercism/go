15790c2ee789c6bfef688bd2b76784f13fecc809
Lee Nipper <lee.nipper@gmail.com>
2017-03-29 08:05:56 -0500

    change: Add exercise (#598)

---
+++ exercises/change/cases_test.go
@@ -0,0 +1,83 @@
+package change
+
+// Source: exercism/x-common
+// Commit: 3d8b5b3 change: Fix canonical-data.json formatting
+
+var testCases = []struct {
+	description    string
+	coins          []int
+	target         int
+	valid          bool  // true => no error, false => error expected
+	expectedChange []int // when .valid == true, the expected change coins
+}{
+	{
+		"single coin change",
+		[]int{1, 5, 10, 25, 100},
+		25,
+		true,
+		[]int{25},
+	},
+	{
+		"multiple coin change",
+		[]int{1, 5, 10, 25, 100},
+		15,
+		true,
+		[]int{5, 10},
+	},
+	{
+		"change with Lilliputian Coins",
+		[]int{1, 4, 15, 20, 50},
+		23,
+		true,
+		[]int{4, 4, 15},
+	},
+	{
+		"change with Lower Elbonia Coins",
+		[]int{1, 5, 10, 21, 25},
+		63,
+		true,
+		[]int{21, 21, 21},
+	},
+	{
+		"large target values",
+		[]int{1, 2, 5, 10, 20, 50, 100},
+		999,
+		true,
+		[]int{2, 2, 5, 20, 20, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100},
+	},
+	{
+		"possible change without unit coins available",
+		[]int{2, 5, 10, 20, 50},
+		21,
+		true,
+		[]int{2, 2, 2, 5, 10},
+	},
+	{
+		"no coins make 0 change",
+		[]int{1, 5, 10, 21, 25},
+		0,
+		true,
+		[]int{},
+	},
+	{
+		"error testing for change smaller than the smallest of coins",
+		[]int{5, 10},
+		3,
+		false,
+		[]int(nil),
+	},
+	{
+		"error if no combination can add up to target",
+		[]int{5, 10},
+		94,
+		false,
+		[]int(nil),
+	},
+	{
+		"cannot find negative change values",
+		[]int{1, 2, 5},
+		-5,
+		false,
+		[]int(nil),
+	},
+}
---
+++ exercises/change/change_test.go
@@ -0,0 +1,48 @@
+package change
+
+import (
+	"reflect"
+	"testing"
+)
+
+const targetTestVersion = 1
+
+func TestTestVersion(t *testing.T) {
+	if testVersion != targetTestVersion {
+		t.Fatalf("Found testVersion = %v, want %v", testVersion, targetTestVersion)
+	}
+}
+
+func TestChange(t *testing.T) {
+	for _, tc := range testCases {
+		actual, err := Change(tc.coins, tc.target)
+		if tc.valid {
+			if err != nil {
+				t.Fatalf("%s : Change(%v, %d): expected %v, got error %s",
+					tc.description, tc.coins, tc.target, tc.expectedChange, err)
+			} else {
+				if !reflect.DeepEqual(actual, tc.expectedChange) {
+					t.Fatalf("%s : Change(%v, %d): expected %v, actual %v",
+						tc.description, tc.coins, tc.target, tc.expectedChange, actual)
+				} else {
+					t.Logf("PASS: %s", tc.description)
+				}
+			}
+		} else {
+			if err == nil {
+				t.Fatalf("%s : Change(%v, %d): expected error, got %v",
+					tc.description, tc.coins, tc.target, actual)
+			} else {
+				t.Logf("PASS: %s", tc.description)
+			}
+		}
+	}
+}
+
+func BenchmarkChange(b *testing.B) {
+	for _, tc := range testCases {
+		for i := 0; i < b.N; i++ {
+			Change(tc.coins, tc.target)
+		}
+	}
+}
