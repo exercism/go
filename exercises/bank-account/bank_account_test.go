858522207a0ef2f2895b5490730790e56ef09787
parroty <parroty@users.noreply.github.com>
2017-04-07 23:16:04 +0900

    bank-account: Fix typo in the function signature (#616)

--- exercises/bank-account/bank_account_test.go
+++ exercises/bank-account/bank_account_test.go
@@ -1,6 +1,6 @@
 // API:
 //
-// Open(initalDeposit int64) *Account
+// Open(initialDeposit int64) *Account
 // (Account) Close() (payout int64, ok bool)
 // (Account) Balance() (balance int64, ok bool)
 // (Account) Deposit(amount int64) (newBalance int64, ok bool)
