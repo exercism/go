5f3dd6cf2b54b463ee2c8dbdd71dcf11cd675b50
Lee Nipper <lee.nipper@gmail.com>
2017-04-07 20:23:02 -0500

    forth: update generator
    For issue #604.
    
    - Update generator:
      Use .Header in template.
      Align template with latest canonical-data.json.
    
    - Update test program:
      Reference testGroups containing the generated test cases.
      Annotate the FAIL output with test group name & test case description.
      Add PASS output for successful cases.

--- exercises/forth/forth_test.go
+++ exercises/forth/forth_test.go
@@ -11,53 +11,38 @@ import (
 
 const targetTestVersion = 1
 
-type testCase struct {
-	description string
-	input       []string
-	expected    []int // nil slice indicates error expected.
-}
-
-type testcaseSection struct {
-	name  string
-	tests []testCase
-}
-
 func TestTestVersion(t *testing.T) {
 	if testVersion != targetTestVersion {
 		t.Fatalf("Found testVersion = %v, want %v", testVersion, targetTestVersion)
 	}
 }
 
-func runTestCases(t *testing.T, sectionName string, testCases []testCase) {
-	for _, tc := range testCases {
-		description := sectionName + " - " + tc.description
-		if v, err := Forth(tc.input); err == nil {
-			var _ error = err
-			if tc.expected == nil {
-				t.Fatalf("%s\n\tForth(%#v) expected an error, got %v",
-					description, tc.input, v)
-			} else if !reflect.DeepEqual(v, tc.expected) {
-				t.Fatalf("%s\n\tForth(%#v) expected %v, got %v",
-					description, tc.input, tc.expected, v)
-			}
-		} else if tc.expected != nil {
-			t.Fatalf("%s\n\tForth(%#v) expected %v, got an error: %q",
-				description, tc.input, tc.expected, err)
-		}
-	}
-}
-
 func TestForth(t *testing.T) {
-	for _, testSection := range testSections {
-		runTestCases(t, testSection.name, testSection.tests)
+	for _, tg := range testGroups {
+		for _, tc := range tg.tests {
+			if v, err := Forth(tc.input); err == nil {
+				var _ error = err
+				if tc.expected == nil {
+					t.Fatalf("FAIL: %s | %s\n\tForth(%#v) expected an error, got %v",
+						tg.group, tc.description, tc.input, v)
+				} else if !reflect.DeepEqual(v, tc.expected) {
+					t.Fatalf("FAIL: %s | %s\n\tForth(%#v) expected %v, got %v",
+						tg.group, tc.description, tc.input, tc.expected, v)
+				}
+			} else if tc.expected != nil {
+				t.Fatalf("FAIL: %s | %s\n\tForth(%#v) expected %v, got an error: %q",
+					tg.group, tc.description, tc.input, tc.expected, err)
+			}
+			t.Logf("PASS: %s | %s", tg.group, tc.description)
+		}
 	}
 }
 
 func BenchmarkForth(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		for _, testSection := range testSections {
-			for _, test := range testSection.tests {
-				Forth(test.input)
+		for _, tg := range testGroups {
+			for _, tc := range tg.tests {
+				Forth(tc.input)
 			}
 		}
 	}
