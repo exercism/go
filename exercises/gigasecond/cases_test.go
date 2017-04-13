c630e9a82f49d688934fe57c36ee5fe379c8b07f
Lee Nipper <lee.nipper@gmail.com>
2017-04-12 22:28:47 -0500

    gigasecond: update generator
    For issue #604.
    
    Use .Header in generator template.
    Align template with latest canonical-data.json.
    Add test description to generated cases.

--- exercises/gigasecond/cases_test.go
+++ exercises/gigasecond/cases_test.go
@@ -1,30 +1,37 @@
 package gigasecond
 
 // Source: exercism/x-common
-// Commit: 1e9e232 Merge pull request #45 from soniakeys/gigasecond-tests
+// Commit: 61e7d70 Fix "Gigasecond: Schema Compliance"
+// x-common version: 1.0.0
 
 // Add one gigasecond to the input.
 var addCases = []struct {
-	in   string
-	want string
+	description string
+	in          string
+	want        string
 }{
 	{
+		"date only specification of time",
 		"2011-04-25",
 		"2043-01-01T01:46:40",
 	},
 	{
+		"second test for date only specification of time",
 		"1977-06-13",
 		"2009-02-19T01:46:40",
 	},
 	{
+		"third test for date only specification of time",
 		"1959-07-19",
 		"1991-03-27T01:46:40",
 	},
 	{
+		"full time specified",
 		"2015-01-24T22:00:00",
 		"2046-10-02T23:46:40",
 	},
 	{
+		"full time with day roll-over",
 		"2015-01-24T23:59:59",
 		"2046-10-03T01:46:39",
 	},
