068b4d9c4ea5f4d068bc06a709e0cd7698ead114
Tapan Karecha <tapan@karecha.com>
2017-04-25 16:38:40 +0530

    Test for exercise transpose now correctly handles the tests where one of actual and expected results is zero length
    This issue has been reported in:
    https://github.com/exercism/xgo/issues/651

--- exercises/transpose/transpose_test.go
+++ exercises/transpose/transpose_test.go
@@ -17,6 +17,11 @@ func TestTranspose(t *testing.T) {
 	for _, test := range testCases {
 		actual := Transpose(test.input)
 		if !reflect.DeepEqual(actual, test.expected) {
+			// check for zero length slices
+			if len(actual) == 0 || len(test.expected) == 0 {
+				t.Fatalf("\n\tTranspose(%q): %s\n\n\tExpected: %q\n\tGot: %q",
+					test.input, test.description, test.expected, actual)
+			}
 			// let's make the error more specific and find the row it's on
 			min := min(len(test.expected), len(actual))
 			for i := 0; i < min; i++ {
