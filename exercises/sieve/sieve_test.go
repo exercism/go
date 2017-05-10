18bb69464e5a0c683096775498fa01210c046f81
Peter Tseng <pht24@cornell.edu>
2017-05-09 21:15:42 -0700

    sieve: declare compliance with x-common 0.7.0

--- exercises/sieve/sieve_test.go
+++ exercises/sieve/sieve_test.go
@@ -7,6 +7,9 @@ import (
 
 const targetTestVersion = 1
 
+// converted from x-common by hand
+// missing: 1, 2, 13
+// x-common version: 0.7.0
 var p10 = []int{2, 3, 5, 7}
 var p1000 = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
 	59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
