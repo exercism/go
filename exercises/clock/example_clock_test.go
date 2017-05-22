88c65b16367d359b9fc5c32706e214cc665ec862
Rob Phoenix <rob@robphoenix.com>
2017-05-22 10:57:15 +0100

    clock: separate example tests
    The output of a single example test was quite overwhelming. Breaking the
    example test into smaller tests makes any failure output easier to grok.
    
    See https://github.com/exercism/xgo/pull/568#discussion_r117623204

--- exercises/clock/example_clock_test.go
+++ exercises/clock/example_clock_test.go
@@ -2,39 +2,53 @@ package clock
 
 import "fmt"
 
-func ExampleClock() {
-	// Create a clock
+func ExampleClock_new() {
+	// a new clock
 	clock1 := New(10, 30)
 	fmt.Println(clock1.String())
 
-	// Add 30 minutes to it
-	clock1 = clock1.Add(30)
-	fmt.Println(clock1.String())
+	// Output: 10:30
+}
 
-	// Subtract an hour and a half from it
-	clock1 = clock1.Add(-90)
-	fmt.Println(clock1.String())
+func ExampleClock_Add_add() {
+	// create a clock
+	clock := New(10, 30)
 
-	// Create a second clock
-	clock2 := New(9, 30)
-	fmt.Println(clock2.String())
+	// add 30 minutes to it
+	clock = clock.Add(30)
+	fmt.Println(clock.String())
 
-	// Are the clocks equal?
+	// Output: 11:00
+}
+
+func ExampleClock_Add_subtract() {
+	// create a clock
+	clock := New(10, 30)
+
+	// subtract an hour and a half from it
+	clock = clock.Add(-90)
+	fmt.Println(clock.String())
+
+	// Output: 09:00
+}
+
+func ExampleClock_compare() {
+	// a new clock
+	clock1 := New(10, 30)
+
+	// a second clock, same as the first
+	clock2 := New(10, 30)
+
+	// are the clocks equal?
 	fmt.Println(clock2 == clock1)
 
-	// Change the second clock
+	// change the second clock
 	clock2 = clock2.Add(30)
-	fmt.Println(clock2.String())
 
-	// Are the clocks equal now?
+	// are the clocks equal now?
 	fmt.Println(clock2 == clock1)
 
 	// Output:
-	// 10:30
-	// 11:00
-	// 09:30
-	// 09:30
 	// true
-	// 10:00
 	// false
 }
