ebcc8e6608a23d1d8118e4e020a18e2c94be9c65
Tom Leen <tml@leentop.thomasleen.com>
2017-07-04 12:38:15 -0400

    error-handling: Add lengthy description of expected solution
    This is sort of a tricky problem if you are new to Go based on where it
    currently falls in the exercises list. Seemed like people are getting lost in
    the "what" of the problem instead of focusing on the "how". The README is
    necessarly generic because error handling differs greatly between
    languages. This updates the solution description in the tests file with more
    detail of the expected behavour of the Use function in the solution. It also
    provides links to the language concepts which will be used in the solution and
    some comments in the common file.
    
    Resolves #703

--- exercises/error-handling/common.go
+++ exercises/error-handling/common.go
@@ -2,6 +2,16 @@ package erratum
 
 import "io"
 
+// These are the support types and interface definitions used in the
+// implementation if your Use function. See the test suite file at
+// ./error_handling_test.go for information on the expected implementation.
+//
+// Because this are part of the package "erratum" if your solution file
+// is also declared in the package you will automatically have access to
+// these definitions (you do not have to re-declare them).
+
+// TransientError is an error that may occur while opening a resource via
+// ResourceOpener.
 type TransientError struct {
 	err error
 }
@@ -10,6 +20,10 @@ func (e TransientError) Error() string {
 	return e.err.Error()
 }
 
+// FrobError is a possible error from doing some frobbing, your implementation
+// will require calling your Resource's Defrob(string) method.
+// When this error occurs, the FrobError's defrobTag string will contain the
+// string you must pass into Defrob.
 type FrobError struct {
 	defrobTag string
 	inner     error
@@ -20,6 +34,9 @@ func (e FrobError) Error() string {
 }
 
 type Resource interface {
+
+	// Resource is using composition to inherit the requirements of the io.Closer
+	// interface. What this means is that a Resource will have a .Close() method.
 	io.Closer
 
 	// Frob does something with the input string.
