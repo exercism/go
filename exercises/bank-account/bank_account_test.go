4d8927a59b7d86ce89f4ffe4b8d29818c8124e8f
Tom Leen <tml@leentop.thomasleen.com>
2017-07-10 11:26:35 -0400

    bank-account: more info about expected solution + mutex links
    Added a little more detail on the expected solution, concurrency, and not
    allowing the balance to go below 0 on withdrawals.
    
    Looking at the submitted solutions some users were not discovering Mutexes
    on their own. Thought it may be helpful to add links to the godocs/tutorials
    concering Mutexes if people have no idea where to start on the solution.

--- exercises/bank-account/bank_account_test.go
+++ exercises/bank-account/bank_account_test.go
@@ -6,9 +6,23 @@
 // (Account) Deposit(amount int64) (newBalance int64, ok bool)
 //
 // If Open is given a negative initial deposit, it must return nil.
-// Deposit must handle a negative amount as a withdrawal.
+// Deposit must handle a negative amount as a withdrawal. Withdrawals must
+// not succeed if they result in a negative balance.
 // If any Account method is called on an closed account, it must not modify
 // the account and must return ok = false.
+
+// The tests will execute some operations concurrently. You should strive
+// to ensure that operations on the Account leave it in a consistent state.
+// For example: multiple goroutines may be depositing and withdrawing money
+// simultaneously, two withdraws occurring concurrently should not be able
+// to bring the balance into the negative.
+
+// If you are new to concurrent operations in Go it will be worth looking
+// at the sync package, specifically Mutexes:
+//
+// https://golang.org/pkg/sync/
+// https://tour.golang.org/concurrency/9
+// https://gobyexample.com/mutexes
 
 package account
 
