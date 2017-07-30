efd9ae68068c004fe7bd50345b3f781726966b5f
Lee Nipper <lee.nipper@gmail.com>
2017-07-30 17:58:13 -0500

    rna-transcription: update descriptions in cases_test.go
    Re-generate cases_test.go with canonical-data.json
    which has updated descriptions("rna" => "RNA").
    
    No test data changed, so no testVersion change.

--- exercises/rna-transcription/cases_test.go
+++ exercises/rna-transcription/cases_test.go
@@ -1,25 +1,25 @@
 package strand
 
 // Source: exercism/x-common
-// Commit: 0b20fff rna-transcription: Fix canonical-data.json formatting
-// x-common version: 1.0.0
+// Commit: cb1fd3a rna-transcription: rephrase negative test descriptions
+// x-common version: 1.0.1
 
 var rnaTests = []struct {
 	input    string
 	expected string
 }{
-	// rna complement of cytosine is guanine
+	// RNA complement of cytosine is guanine
 	{"C", "G"},
 
-	// rna complement of guanine is cytosine
+	// RNA complement of guanine is cytosine
 	{"G", "C"},
 
-	// rna complement of thymine is adenine
+	// RNA complement of thymine is adenine
 	{"T", "A"},
 
-	// rna complement of adenine is uracil
+	// RNA complement of adenine is uracil
 	{"A", "U"},
 
-	// rna complement
+	// RNA complement
 	{"ACGTGGTCTTAA", "UGCACCAGAAUU"},
 }
