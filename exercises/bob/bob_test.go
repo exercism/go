0e58402be22781b2794e5179d6576f5f891d53c3
Alessandro Baffa <alessandro.baffa@gmail.com>
2017-09-12 23:14:28 +0900

    fix conflicts

--- exercises/bob/bob_test.go
+++ exercises/bob/bob_test.go
@@ -2,21 +2,20 @@ package bob
 
 import "testing"
 
-func TestHeyBob(t *testing.T) {
+func TestHey(t *testing.T) {
 	for _, tt := range testCases {
 		actual := Hey(tt.input)
 		if actual != tt.expected {
 			msg := `
 	ALICE (%s): %q
 	BOB: %s
-
 	Expected Bob to respond: %s`
 			t.Fatalf(msg, tt.description, tt.input, actual, tt.expected)
 		}
 	}
 }
 
-func BenchmarkBob(b *testing.B) {
+func BenchmarkHey(b *testing.B) {
 	for _, tt := range testCases {
 		for i := 0; i < b.N; i++ {
 			Hey(tt.input)
