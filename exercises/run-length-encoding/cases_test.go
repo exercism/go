a85ba4191c1a4fee0393bf796591adbd8f15fc9c
Mat Forsberg <mforsberg@pinnergy.com>
2017-09-19 06:05:13 -0500

    Formatted and added UUID
    Formatted go files and added UUID to config.json

--- exercises/run-length-encoding/cases_test.go
+++ exercises/run-length-encoding/cases_test.go
@@ -4,29 +4,29 @@ var encodeTests = []struct {
 	input    string
 	expected string
 }{
-	{"",""},
-	{"XYZ","XYZ"},
-	{"AABBBCCCC","2A3B4C"},
-	{"WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB","12WB12W3B24WB"},
-	{"  hsqq qww  ","2 hs2q q2w2 "},
-	{"aabbbcccc","2a3b4c"},
+	{"", ""},
+	{"XYZ", "XYZ"},
+	{"AABBBCCCC", "2A3B4C"},
+	{"WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB", "12WB12W3B24WB"},
+	{"  hsqq qww  ", "2 hs2q q2w2 "},
+	{"aabbbcccc", "2a3b4c"},
 }
 
 var decodeTests = []struct {
 	input    string
 	expected string
 }{
-	{"",""},
-	{"XYZ","XYZ"},
-	{"2A3B4C","AABBBCCCC"},
-	{"12WB12W3B24WB","WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"},
-	{"2 hs2q q2w2 ","  hsqq qww  "},
-	{"2a3b4c","aabbbcccc"},
+	{"", ""},
+	{"XYZ", "XYZ"},
+	{"2A3B4C", "AABBBCCCC"},
+	{"12WB12W3B24WB", "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"},
+	{"2 hs2q q2w2 ", "  hsqq qww  "},
+	{"2a3b4c", "aabbbcccc"},
 }
 
 var encodeDecodeTests = []struct {
 	input    string
 	expected string
 }{
-	{"zzz ZZ  zZ","zzz ZZ  zZ"},
+	{"zzz ZZ  zZ", "zzz ZZ  zZ"},
 }
