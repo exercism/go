89eeeab4841cd2585879751eff33bce0705b144a
Minjie Zha <minjiezha@gmail.com>
2017-10-20 21:00:38 -0700

    address comments

--- exercises/rotational-cipher/example.go
+++ exercises/rotational-cipher/example.go
@@ -4,19 +4,16 @@ import (
 	"unicode"
 )
 
-// RotationalCipher is an implementation for the rotaional cipher exercise.
+// RotationalCipher is an implementation for the rotational cipher exercise.
 func RotationalCipher(plain string, shiftKey int) string {
 	var cipher []rune
 	for _, ru := range plain {
 		if unicode.IsUpper(ru) {
 			ru = rune('A' + (int(ru-'A')+shiftKey)%26)
-			cipher = append(cipher, ru)
 		} else if unicode.IsLower(ru) {
 			ru = rune('a' + (int(ru-'a')+shiftKey)%26)
-			cipher = append(cipher, ru)
-		} else {
-			cipher = append(cipher, ru)
 		}
+		cipher = append(cipher, ru)
 	}
 
 	return string(cipher)
--- exercises/rotational-cipher/rotational_cipher_test.go
+++ exercises/rotational-cipher/rotational_cipher_test.go
@@ -70,14 +70,14 @@ func TestRotationalCipher(t *testing.T) {
 	for _, testCase := range testCases {
 		cipher := RotationalCipher(testCase.inputPlain, testCase.inputShiftKey)
 		if cipher != testCase.expected {
-			t.Fatalf("FAIL: %s\n\tRotationalCipher(%s, %v) expected %s, got %s",
+			t.Fatalf("FAIL: %s\n\tRotationalCipher(%s, %d)\nexpected: %s, \ngot:      %s",
 				testCase.description, testCase.inputPlain, testCase.inputShiftKey, testCase.expected, cipher)
 		}
 		t.Logf("PASS: %s", testCase.description)
 	}
 }
 
-func BenchmarkCollatzConjecture(b *testing.B) {
+func BenchmarkRotationalCipher(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, testCase := range testCases {
 			RotationalCipher(testCase.inputPlain, testCase.inputShiftKey)
