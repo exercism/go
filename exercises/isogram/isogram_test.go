e5c8a3542d1f590859d631f8fc29119727ae2339
ferhat elmas <elmas.ferhat@gmail.com>
2017-10-31 16:56:12 +0100

    isogram: add test generator

--- exercises/isogram/isogram_test.go
+++ exercises/isogram/isogram_test.go
@@ -2,29 +2,13 @@ package isogram
 
 import "testing"
 
-var testCases = []struct {
-	word     string
-	expected bool
-}{
-	{"duplicates", true},
-	{"eleven", false},
-	{"subdermatoglyphic", true},
-	{"Alphabet", false},
-	{"thumbscrew-japingly", true},
-	{"Hjelmqvist-Gryb-Zock-Pfund-Wax", true},
-	{"Heizölrückstoßabdämpfung", true},
-	{"the quick brown fox", false},
-	{"Emily Jung Schwartzkopf", true},
-	{"éléphant", false},
-}
-
 func TestIsIsogram(t *testing.T) {
 	for _, c := range testCases {
-		if IsIsogram(c.word) != c.expected {
-			t.Fatalf("FAIL: Word %q, expected %v, got %v", c.word, c.expected, !c.expected)
+		if IsIsogram(c.input) != c.expected {
+			t.Fatalf("FAIL: %s\nWord %q, expected %t, got %t", c.description, c.input, c.expected, !c.expected)
 		}
 
-		t.Logf("PASS: Word %q", c.word)
+		t.Logf("PASS: Word %q", c.input)
 	}
 }
 
@@ -34,7 +18,7 @@ func BenchmarkIsIsogram(b *testing.B) {
 		b.StartTimer()
 
 		for i := 0; i < b.N; i++ {
-			IsIsogram(c.word)
+			IsIsogram(c.input)
 		}
 
 		b.StopTimer()
