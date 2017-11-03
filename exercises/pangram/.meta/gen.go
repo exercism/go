2604e26384625dae142d94ca346b574e2c996532
ferhat elmas <elmas.ferhat@gmail.com>
2017-11-03 19:47:27 +0100

    pangram: add test generator

---
+++ exercises/pangram/.meta/gen.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"text/template"
+
+	"../../../gen"
+)
+
+func main() {
+	t, err := template.New("").Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var j js
+	if err := gen.Gen("pangram", &j, t); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// The JSON structure we expect to be able to unmarshal into
+type js struct {
+	Exercise string
+	Version  string
+	Cases    []struct {
+		Cases []oneCase
+	}
+}
+
+// Test cases
+type oneCase struct {
+	Description string
+	Property    string
+	Input       string
+	Expected    bool
+}
+
+// Template to generate test cases.
+var tmpl = `package pangram
+
+{{.Header}}
+
+var testCases = []struct {
+	description	string
+	input		string
+	expected	bool
+}{ {{range .J.Cases}}{{range .Cases}}
+{
+	description:	{{printf "%q"  .Description}},
+	input:		{{printf "%q"  .Input}},
+	expected:	{{printf "%t"  .Expected}},
+},{{end}}{{end}}
+}
+`
--- exercises/pangram/pangram_test.go
+++ exercises/pangram/pangram_test.go
@@ -4,33 +4,12 @@ import (
 	"testing"
 )
 
-type testCase struct {
-	input         string
-	expected      bool
-	failureReason string
-}
-
-var testCases = []testCase{
-	{"", false, "sentence empty"},
-	{"The quick brown fox jumps over the lazy dog", true, ""},
-	{"a quick movement of the enemy will jeopardize five gunboats", false, "missing character 'x'"},
-	{"the quick brown fish jumps over the lazy dog", false, "another missing character 'x'"},
-	{"the 1 quick brown fox jumps over the 2 lazy dogs", true, ""},
-	{"7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog", false, "missing letters replaced by numbers"},
-	{"\"Five quacking Zephyrs jolt my wax bed.\"", true, ""},
-	{"abcdefghijklmnopqrstuvwxy", false, "missing character 'z'"},
-	{"bcdefghijklmnopqrstuvwxyz", false, "missing character 'a'"},
-}
-
 func TestPangram(t *testing.T) {
 	for _, test := range testCases {
-		actual := IsPangram(test.input)
-		if actual != test.expected {
-			t.Errorf("Pangram test [%s], expected [%t], actual [%t]", test.input, test.expected, actual)
-			if !test.expected {
-				t.Logf("[%s] should not be a pangram because : %s\n", test.input, test.failureReason)
-			}
+		if actual := IsPangram(test.input); actual != test.expected {
+			t.Fatalf("FAIL: %s\nInput %q expected [%t], actual [%t]", test.description, test.input, test.expected, actual)
 		}
+		t.Logf("PASS: %s", test.description)
 	}
 }
 
