b08d519bcff5e6c8cc967bdf680487225b9e164e
ferhat elmas <elmas.ferhat@gmail.com>
2017-11-07 22:50:01 +0100

    allergies: add test generator (#938)

--- exercises/allergies/allergies_test.go
+++ exercises/allergies/allergies_test.go
@@ -1,35 +1,16 @@
 package allergies
 
 import (
-	"fmt"
-	"sort"
+	"reflect"
 	"testing"
 )
 
-var allergiesTests = []struct {
-	expected []string
-	input    uint
-}{
-	{[]string{}, 0},
-	{[]string{"eggs"}, 1},
-	{[]string{"peanuts"}, 2},
-	{[]string{"strawberries"}, 8},
-	{[]string{"eggs", "peanuts"}, 3},
-	{[]string{"eggs", "shellfish"}, 5},
-	{[]string{"strawberries", "tomatoes", "chocolate", "pollen", "cats"}, 248},
-	{[]string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}, 255},
-	{[]string{"eggs", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}, 509},
-}
-
 func TestAllergies(t *testing.T) {
-	for _, test := range allergiesTests {
-		actual := Allergies(test.input)
-		sort.Strings(actual)
-		sort.Strings(test.expected)
-		if fmt.Sprintf("%s", actual) != fmt.Sprintf("%s", test.expected) {
-			t.Fatalf("FAIL: Allergies(%d): expected %q, actual %q", test.input, test.expected, actual)
+	for _, test := range listTests {
+		if actual := Allergies(test.score); !reflect.DeepEqual(actual, test.expected) && (len(actual) > 0 || len(test.expected) > 0) {
+			t.Fatalf("FAIL: Allergies(%d): expected %#v, actual %#v", test.score, test.expected, actual)
 		} else {
-			t.Logf("PASS: Allergic to %q", test.expected)
+			t.Logf("PASS: Allergic to %#v", test.expected)
 		}
 	}
 }
@@ -40,31 +21,21 @@ func BenchmarkAllergies(b *testing.B) {
 		b.StartTimer()
 
 		for i := 0; i < b.N; i++ {
-			Allergies(test.i)
+			Allergies(test.score)
 		}
 		b.StopTimer()
 	}
 }
 
-var allergicToTests = []struct {
-	expected bool
-	i        uint
-	allergen string
-}{
-	{false, 0, "peanuts"},
-	{false, 0, "cats"},
-	{false, 0, "strawberries"},
-	{true, 1, "eggs"},
-	{true, 5, "eggs"},
-}
-
 func TestAllergicTo(t *testing.T) {
 	for _, test := range allergicToTests {
-		actual := AllergicTo(test.i, test.allergen)
-		if actual != test.expected {
-			t.Fatalf("FAIL: AllergicTo(%d, %s): expected %t, actual %t", test.i, test.allergen, test.expected, actual)
-		} else {
-			t.Logf("PASS: AllergicTo(%d, %s) %t", test.i, test.allergen, actual)
+		for _, response := range test.expected {
+			actual := AllergicTo(test.score, response.substance)
+			if actual != response.result {
+				t.Fatalf("FAIL: AllergicTo(%d, %s): expected %t, actual %t", test.score, response.substance, response.result, actual)
+			} else {
+				t.Logf("PASS: AllergicTo(%d, %s) %t", test.score, response.substance, actual)
+			}
 		}
 	}
 }
@@ -72,11 +43,13 @@ func TestAllergicTo(t *testing.T) {
 func BenchmarkAllergicTo(b *testing.B) {
 	b.StopTimer()
 	for _, test := range allergicToTests {
-		b.StartTimer()
+		for _, response := range test.expected {
+			b.StartTimer()
 
-		for i := 0; i < b.N; i++ {
-			AllergicTo(test.i, test.allergen)
+			for i := 0; i < b.N; i++ {
+				AllergicTo(test.score, response.substance)
+			}
+			b.StopTimer()
 		}
-		b.StopTimer()
 	}
 }
---
+++ exercises/allergies/cases_test.go
@@ -0,0 +1,60 @@
+package allergies
+
+// Source: exercism/problem-specifications
+// Commit: 879bc89 allergies: Fix canonical-data.json formatting
+// Problem Specifications Version: 1.0.0
+
+// allergicTo
+type allergicResult struct {
+	substance string
+	result    bool
+}
+
+var allergicToTests = []struct {
+	description string
+	score       uint
+	expected    []allergicResult
+}{
+	{
+		description: "no allergies means not allergic",
+		score:       0,
+		expected: []allergicResult{
+			{"peanuts", false},
+			{"cats", false},
+			{"strawberries", false},
+		},
+	},
+	{
+		description: "is allergic to eggs",
+		score:       1,
+		expected: []allergicResult{
+			{"eggs", true},
+		},
+	},
+	{
+		description: "allergic to eggs in addition to other stuff",
+		score:       5,
+		expected: []allergicResult{
+			{"eggs", true},
+			{"shellfish", true},
+			{"strawberries", false},
+		},
+	},
+}
+
+// list
+var listTests = []struct {
+	description string
+	score       uint
+	expected    []string
+}{
+	{"no allergies at all", 0, []string{}},
+	{"allergic to just eggs", 1, []string{"eggs"}},
+	{"allergic to just peanuts", 2, []string{"peanuts"}},
+	{"allergic to just strawberries", 8, []string{"strawberries"}},
+	{"allergic to eggs and peanuts", 3, []string{"eggs", "peanuts"}},
+	{"allergic to more than eggs but not peanuts", 5, []string{"eggs", "shellfish"}},
+	{"allergic to lots of stuff", 248, []string{"strawberries", "tomatoes", "chocolate", "pollen", "cats"}},
+	{"allergic to everything", 255, []string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}},
+	{"ignore non allergen score parts", 509, []string{"eggs", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}},
+}
