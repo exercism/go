eca09687082b68e1f5fa3ff1d1454a6a8996c409
ferhat elmas <elmas.ferhat@gmail.com>
2017-11-10 10:30:47 +0100

    accumulate: make it more standard (#946)
    - deprecated, #823 will replace it but until that moment

--- exercises/accumulate/example.go
+++ exercises/accumulate/example.go
@@ -1,8 +1,9 @@
 package accumulate
 
+// Accumulate transform given strings by using given function
 func Accumulate(s []string, f func(st string) string) (result []string) {
 	for _, v := range s {
-		result = append(result, []string{f(v)}...)
+		result = append(result, f(v))
 	}
 	return result
 }
