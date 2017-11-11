331074e8538619af9502b4dffc168eb445071e47
Lee Nipper <lee.nipper@gmail.com>
2017-11-11 16:24:31 -0600

    wordy: create test case generator
    Add .meta/gen.go to generate cases_test.go.
    
    Update test program to use generated test case array.
    Put FAIL and PASS in test result output.
    
    For #605.

---
+++ exercises/wordy/.meta/gen.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+	"text/template"
+
+	"../../../gen"
+)
+
+func main() {
+	t, err := template.New("").Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var j js
+	if err := gen.Gen("wordy", &j, t); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type OneCase struct {
+	Description string
+	Input       string
+	Expected    interface{}
+}
+
+// The JSON structure we expect to be able to unmarshal into
+type js struct {
+	Cases []OneCase
+}
+
+func (c OneCase) Valid() bool {
+	valid, _ := determineExpected(c.Expected)
+	return valid
+}
+
+func (c OneCase) Answer() int {
+	_, answer := determineExpected(c.Expected)
+	return answer
+}
+
+// determineExpected examines an .Expected interface{} object and determines
+// whether a test case is valid(bool) and has an answer or expects an error.
+// returning valid and answer.
+func determineExpected(expected interface{}) (bool, int) {
+	ans, ok := expected.(float64)
+	if ok {
+		return ok, int(ans)
+	}
+	_, ok = expected.(bool)
+	if ok {
+		return false, 0
+	}
+	return false, 0
+}
+
+// template applied to above data structure generates the Go test cases
+var tmpl = `package wordy
+
+{{.Header}}
+
+type wordyTest struct {
+	description string
+	question   string
+	ok bool
+	answer    int
+}
+
+var tests = []wordyTest {
+{{range .J.Cases}}{
+	"{{.Description}}",
+	"{{.Input}}",
+{{if .Valid}} true,
+	{{.Answer}},
+{{- else}} false,
+	0,
+{{- end}}
+},
+{{end}}}
+`
