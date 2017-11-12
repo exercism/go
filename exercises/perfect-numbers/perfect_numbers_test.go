5ccf8ba75792f41d0a4c37175ee2708ba66db9e0
Lee Nipper <lee.nipper@gmail.com>
2017-11-12 00:34:14 -0600

    perfect-numbers: create test case generator
    Add .meta/gen.go to generate cases_test.go.
    
    Update test program to use generated test case array.
    Put FAIL and PASS in test result output.
    Keep TestGivesPositiveRequiredError redundant test on zero
    since it verifies ErrOnlyPositive.
    
    Update example solution to use int64 for input type
    since -1 test case in canonical-data.json.
    
    For #605.

--- exercises/perfect-numbers/perfect_numbers_test.go
+++ exercises/perfect-numbers/perfect_numbers_test.go
@@ -4,32 +4,27 @@ import "testing"
 
 var _ error = ErrOnlyPositive
 
-var classificationTestCases = []struct {
-	input    uint64
-	expected Classification
-}{
-	{1, ClassificationDeficient},
-	{13, ClassificationDeficient},
-	{12, ClassificationAbundant},
-	{6, ClassificationPerfect},
-	{28, ClassificationPerfect},
-	{496, ClassificationPerfect},
-	{8128, ClassificationPerfect},
-}
-
 func TestGivesPositiveRequiredError(t *testing.T) {
 	if _, err := Classify(0); err != ErrOnlyPositive {
-		t.Errorf("Expected error %q but got %q", ErrOnlyPositive, err)
+		t.Fatalf("FAIL GivesPositiveRequiredError Expected error %q but got %q", ErrOnlyPositive, err)
 	}
+	t.Logf("PASS GivesPositiveRequiredError")
 }
 
 func TestClassifiesCorrectly(t *testing.T) {
 	for _, c := range classificationTestCases {
-		if cat, err := Classify(c.input); err != nil {
-			t.Errorf("%d: Expected no error but got %s", c.input, err)
-		} else if cat != c.expected {
-			t.Errorf("%d: Expected %q, got %q", c.input, c.expected, cat)
+		cat, err := Classify(c.input)
+		switch {
+		case err != nil:
+			if c.ok {
+				t.Fatalf("FAIL %s\nClassify(%d)\nExpected no error but got error %q", c.description, c.input, err)
+			}
+		case !c.ok:
+			t.Fatalf("FAIL %s\nClassify(%d)\nExpected error but got %q", c.description, c.input, cat)
+		case cat != c.expected:
+			t.Fatalf("FAIL %s\nClassify(%d)\nExpected %q, got %q", c.description, c.input, c.expected, cat)
 		}
+		t.Logf("PASS %s", c.description)
 	}
 }
 
