83d54e6a9e546efca6bb28cd1fd91f0315943a10
Lee Nipper <lee.nipper@gmail.com>
2017-11-12 19:27:35 -0600

    book-store: create test case generator (#954)
    Add .meta/gen.go to generate cases_test.go.
    
    Update test program to use generated test case array.
    
    For #605.

---
+++ exercises/book-store/cases_test.go
@@ -0,0 +1,77 @@
+package bookstore
+
+// Source: exercism/problem-specifications
+// Commit: 087ad69 book-store: Add missing trailing zero
+// Problem Specifications Version: 1.0.1
+
+var testCases = []struct {
+	description string
+	basket      []int
+	expected    float64
+}{
+	{
+		description: "Only a single book",
+		basket:      []int{1},
+		expected:    8.00,
+	},
+	{
+		description: "Two of the same book",
+		basket:      []int{2, 2},
+		expected:    16.00,
+	},
+	{
+		description: "Empty basket",
+		basket:      []int{},
+		expected:    0.00,
+	},
+	{
+		description: "Two different books",
+		basket:      []int{1, 2},
+		expected:    15.20,
+	},
+	{
+		description: "Three different books",
+		basket:      []int{1, 2, 3},
+		expected:    21.60,
+	},
+	{
+		description: "Four different books",
+		basket:      []int{1, 2, 3, 4},
+		expected:    25.60,
+	},
+	{
+		description: "Five different books",
+		basket:      []int{1, 2, 3, 4, 5},
+		expected:    30.00,
+	},
+	{
+		description: "Two groups of four is cheaper than group of five plus group of three",
+		basket:      []int{1, 1, 2, 2, 3, 3, 4, 5},
+		expected:    51.20,
+	},
+	{
+		description: "Group of four plus group of two is cheaper than two groups of three",
+		basket:      []int{1, 1, 2, 2, 3, 4},
+		expected:    40.80,
+	},
+	{
+		description: "Two each of first 4 books and 1 copy each of rest",
+		basket:      []int{1, 1, 2, 2, 3, 3, 4, 4, 5},
+		expected:    55.60,
+	},
+	{
+		description: "Two copies of each book",
+		basket:      []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5},
+		expected:    60.00,
+	},
+	{
+		description: "Three copies of first book and 2 each of remaining",
+		basket:      []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1},
+		expected:    68.00,
+	},
+	{
+		description: "Three each of first 2 books and 2 each of remaining books",
+		basket:      []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2},
+		expected:    75.20,
+	},
+}
