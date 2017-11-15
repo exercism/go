f6c62316ad3d9fabd8fe197c79707433200d6809
Alex Glikson <alex.glikson@gmail.com>
2017-11-15 14:02:32 -0500

    space-age: fix formatting

--- exercises/space-age/space_age_test.go
+++ exercises/space-age/space_age_test.go
@@ -8,7 +8,7 @@ import (
 func TestAge(t *testing.T) {
 	const precision = 0.01
 	for _, tc := range testCases {
-		if actual := Age(tc.seconds, tc.planet); math.Abs(actual-tc.expected)>precision {
+		if actual := Age(tc.seconds, tc.planet); math.Abs(actual-tc.expected) > precision {
 			t.Fatalf("FAIL: %s\nExpected: %#v\nActual: %#v", tc.description, tc.expected, actual)
 		}
 		t.Logf("PASS: %s", tc.description)
