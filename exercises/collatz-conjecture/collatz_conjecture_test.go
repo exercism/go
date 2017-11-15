999244e8f08c64b64e6484990751bb35bd95fc3f
Lee Nipper <lee.nipper@gmail.com>
2017-11-14 21:43:49 -0600

    collatz-conjecture: add generator (#958)
    Add .meta/gen.go to generate cases_test.go.
    Make use of zero-value of expectError bool.
    
    Update test program to use generated test case array.
    
    For #605.

--- exercises/collatz-conjecture/collatz_conjecture_test.go
+++ exercises/collatz-conjecture/collatz_conjecture_test.go
@@ -4,48 +4,6 @@ import (
 	"testing"
 )
 
-var testCases = []struct {
-	description string
-	input       int
-	expectError bool
-	expected    int
-}{
-	{
-		description: "zero steps for one",
-		input:       1,
-		expectError: false,
-		expected:    0,
-	},
-	{
-		description: "divide if even",
-		input:       16,
-		expectError: false,
-		expected:    4,
-	},
-	{
-		description: "even and old steps",
-		input:       12,
-		expectError: false,
-		expected:    9,
-	},
-	{
-		description: "large number of even and odd steps",
-		input:       1000000,
-		expectError: false,
-		expected:    152,
-	},
-	{
-		description: "zero is an error",
-		input:       0,
-		expectError: true,
-	},
-	{
-		description: "negative value is an error",
-		input:       -15,
-		expectError: true,
-	},
-}
-
 func TestCollatzConjecture(t *testing.T) {
 	for _, testCase := range testCases {
 		steps, err := CollatzConjecture(testCase.input)
