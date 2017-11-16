d27dc9f6bb749608bd00160a868d4dcd37232896
Lee Nipper <lee.nipper@gmail.com>
2017-11-16 07:20:29 -0600

    grains: add generator (#959)
    Add .meta/gen.go to generate cases_test.go.
    Make use of zero-value of expectError bool.
    
    Update test program to use generated test case array.
    The test case value for the 'Total()' is retained in TestTotal()
    since there wasn't an array of cases in the canonical-data.json
    for the 'total' property, and there is only one valid return value.
    
    For #605.

---
+++ exercises/grains/cases_test.go
@@ -0,0 +1,64 @@
+package grains
+
+// Source: exercism/problem-specifications
+// Commit: d4554e6 grains: fix a typo in comments of canonical data
+// Problem Specifications Version: 1.0.0
+
+// returns the number of grains on the square
+var squareTests = []struct {
+	description string
+	input       int
+	expectedVal uint64
+	expectError bool
+}{
+	{
+		description: "1",
+		input:       1,
+		expectedVal: 1,
+	},
+	{
+		description: "2",
+		input:       2,
+		expectedVal: 2,
+	},
+	{
+		description: "3",
+		input:       3,
+		expectedVal: 4,
+	},
+	{
+		description: "4",
+		input:       4,
+		expectedVal: 8,
+	},
+	{
+		description: "16",
+		input:       16,
+		expectedVal: 32768,
+	},
+	{
+		description: "32",
+		input:       32,
+		expectedVal: 2147483648,
+	},
+	{
+		description: "64",
+		input:       64,
+		expectedVal: 9223372036854775808,
+	},
+	{
+		description: "square 0 returns an error",
+		input:       0,
+		expectError: true,
+	},
+	{
+		description: "negative square returns an error",
+		input:       -1,
+		expectError: true,
+	},
+	{
+		description: "square greater than 64 returns an error",
+		input:       65,
+		expectError: true,
+	},
+}
--- exercises/grains/grains_test.go
+++ exercises/grains/grains_test.go
@@ -4,41 +4,25 @@ import (
 	"testing"
 )
 
-var squareTests = []struct {
-	input       int
-	expectedVal uint64
-	expectError bool
-}{
-	{1, 1, false},
-	{2, 2, false},
-	{3, 4, false},
-	{4, 8, false},
-	{16, 32768, false},
-	{32, 2147483648, false},
-	{64, 9223372036854775808, false},
-	{65, 0, true},
-	{0, 0, true},
-	{-1, 0, true},
-}
-
 func TestSquare(t *testing.T) {
 	for _, test := range squareTests {
 		actualVal, actualErr := Square(test.input)
 
 		// check actualVal only if no error expected
 		if !test.expectError && actualVal != test.expectedVal {
-			t.Errorf("Square(%d) expected %d, Actual %d", test.input, test.expectedVal, actualVal)
+			t.Fatalf("FAIL: %s\nSquare(%d) expected %d, Actual %d", test.description, test.input, test.expectedVal, actualVal)
 		}
 
 		// if we expect an error and there isn't one
 		if test.expectError && actualErr == nil {
-			t.Errorf("Square(%d) expected an error, but error is nil", test.input)
+			t.Fatalf("FAIL: %s\nSquare(%d) expected an error, but error is nil", test.description, test.input)
 		}
 		// if we don't expect an error and there is one
 		if !test.expectError && actualErr != nil {
 			var _ error = actualErr
-			t.Errorf("Square(%d) expected no error, but error is: %s", test.input, actualErr)
+			t.Fatalf("FAIL: %s\nSquare(%d) expected no error, but error is: %s", test.description, test.input, actualErr)
 		}
+		t.Logf("PASS: %s", test.description)
 	}
 }
 
