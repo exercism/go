69bf90e0bdc97cf56352bed9c57c97cca5224c6a
Lee Nipper <lee.nipper@gmail.com>
2017-11-17 09:03:40 -0600

    sieve: add generator (#962)
    Add .meta/gen.go to generate cases_test.go.
    
    Update test program to use generated test case array.
    
    Update example solution to pass test which includes limit as prime.
    
    For #605.

--- exercises/sieve/example.go
+++ exercises/sieve/example.go
@@ -1,15 +1,15 @@
 package sieve
 
 func Sieve(limit int) (primes []int) {
-	c := make([]bool, limit)
-	for p := 2; p < limit; {
-		for i := p + p; i < limit; i += p {
+	c := make([]bool, limit+1)
+	for p := 2; p <= limit; {
+		for i := p + p; i <= limit; i += p {
 			c[i] = true
 		}
-		for p++; p < limit && c[p]; p++ {
+		for p++; p <= limit && c[p]; p++ {
 		}
 	}
-	for i := 2; i < limit; i++ {
+	for i := 2; i <= limit; i++ {
 		if !c[i] {
 			primes = append(primes, i)
 		}
