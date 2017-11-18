cc8783b2cfb36e667c2551da04df425d88d6238e
Lee Nipper <lee.nipper@gmail.com>
2017-11-18 10:28:10 -0600

    prime-factors: add generator
    Add .meta/gen.go to generate cases_test.go.
    
    Update test program to use generated test case array.
    Output FAIL and PASS in test output.
    
    For #605.

---
+++ exercises/prime-factors/cases_test.go
@@ -0,0 +1,48 @@
+package prime
+
+// Source: exercism/problem-specifications
+// Commit: bf5bb2b prime-factors: Fix canonical-data.json formatting
+// Problem Specifications Version: 1.0.0
+
+var tests = []struct {
+	description string
+	input       int64
+	expected    []int64
+}{
+
+	{
+		"no factors",
+		1,
+		[]int64{},
+	},
+	{
+		"prime number",
+		2,
+		[]int64{2},
+	},
+	{
+		"square of a prime",
+		9,
+		[]int64{3, 3},
+	},
+	{
+		"cube of a prime",
+		8,
+		[]int64{2, 2, 2},
+	},
+	{
+		"product of primes and non-primes",
+		12,
+		[]int64{2, 2, 3},
+	},
+	{
+		"product of primes",
+		901255,
+		[]int64{5, 17, 23, 461},
+	},
+	{
+		"factors include a large prime",
+		93819012551,
+		[]int64{11, 9539, 894119},
+	},
+}
