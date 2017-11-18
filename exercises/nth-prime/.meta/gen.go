0df6d5d18f5c33d6f45678de26eb0d7c9deb290c
Lee Nipper <lee.nipper@gmail.com>
2017-11-18 16:20:09 -0600

    nth-prime: add generator
    Add .meta/gen.go to generate cases_test.go.
    
    Update test program to use generated test case array.
    Output FAIL and PASS in test output.
    
    For #605.

---
+++ exercises/nth-prime/.meta/gen.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+	"text/template"
+
+	"../../../gen"
+)
+
+func main() {
+	t, err := template.New("").Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var j js
+	if err := gen.Gen("nth-prime", &j, t); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type OneCase struct {
+	Description string
+	Input       int
+	Expected    interface{}
+}
+
+// The JSON structure we expect to be able to unmarshal into
+type js struct {
+	Cases []OneCase
+}
+
+func (c OneCase) HasPrimeAnswer() bool {
+	hasPrimeAnswer, _ := determineExpected(c.Expected)
+	return hasPrimeAnswer
+}
+
+func (c OneCase) PrimeAnswer() int {
+	_, answer := determineExpected(c.Expected)
+	return answer
+}
+
+// determineExpected examines an .Expected interface{} object and determines
+// whether a test case has a Prime answer or expects an error,
+// returning true and the answer, or false and zero.
+func determineExpected(expected interface{}) (bool, int) {
+	value, ok := expected.(float64)
+	if ok {
+		return true, int(value)
+	}
+	return false, 0
+}
+
+// template applied to above data structure generates the Go test cases
+var tmpl = `package prime
+
+{{.Header}}
+
+var tests = []struct {
+	description string
+	n   int
+	p   int
+	ok  bool
+}{
+{{range .J.Cases}}{
+	"{{.Description}}",
+	{{.Input}},
+{{- if .HasPrimeAnswer}}
+	{{.PrimeAnswer}},
+	true,
+{{- else}}
+	0,
+	false,
+{{- end}}
+},
+{{end}}}
+`
