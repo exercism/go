31e6fe0cf7ee474181ea8207f3d527974ed80ffa
Lee Nipper <lee.nipper@gmail.com>
2017-11-26 21:14:44 -0600

    prime-factors: add generator (#963)
    Add .meta/gen.go to generate cases_test.go.
    
    Update test program to use generated test case array.
    Output FAIL and PASS in test output.
    
    For #605.

--- exercises/prime-factors/prime_factors_test.go
+++ exercises/prime-factors/prime_factors_test.go
@@ -7,30 +7,15 @@ import (
 	"testing"
 )
 
-var tests = []struct {
-	input    int64
-	expected []int64
-}{
-	{1, []int64{}},
-	{2, []int64{2}},
-	{3, []int64{3}},
-	{4, []int64{2, 2}},
-	{6, []int64{2, 3}},
-	{8, []int64{2, 2, 2}},
-	{9, []int64{3, 3}},
-	{27, []int64{3, 3, 3}},
-	{625, []int64{5, 5, 5, 5}},
-	{901255, []int64{5, 17, 23, 461}},
-	{93819012551, []int64{11, 9539, 894119}},
-}
-
 func TestPrimeFactors(t *testing.T) {
 	for _, test := range tests {
 		actual := Factors(test.input)
 		if !reflect.DeepEqual(actual, test.expected) {
-			t.Errorf("prime.Factors(%d) = %#v; expected %#v",
-				test.input, actual, test.expected)
+			t.Fatalf("FAIL %s\nFactors(%d) = %#v;\nexpected %#v",
+				test.description, test.input,
+				actual, test.expected)
 		}
+		t.Logf("PASS %s", test.description)
 	}
 }
 
