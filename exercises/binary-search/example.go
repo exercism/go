6f16504afb2c9d56340131f20def70fa89f6b787
ferhat elmas <elmas.ferhat@gmail.com>
2017-11-28 10:49:45 +0100

    binary-search: add test generator (#957)

--- exercises/binary-search/example.go
+++ exercises/binary-search/example.go
@@ -1,38 +1,16 @@
 package binarysearch
 
-import "fmt"
-
-func SearchInts(s []int, k int) (i int) {
-	for j := len(s); i < j; {
+// SearchInts search given key in the given slice
+// upon finding returns its index, otherwise -1
+func SearchInts(s []int, k int) int {
+	for i, j := 0, len(s)-1; i <= j; {
 		if h := (i + j) / 2; s[h] < k {
 			i = h + 1
+		} else if s[h] > k {
+			j = h - 1
 		} else {
-			j = h
+			return h
 		}
 	}
-	return
-}
-
-func Message(s []int, k int) string {
-	x := SearchInts(s, k)
-	switch {
-	case x == len(s):
-		if x == 0 {
-			return "slice has no values"
-		}
-		return fmt.Sprintf("%d > all %d values", k, x)
-	case s[x] != k:
-		if x == 0 {
-			return fmt.Sprintf("%d < all values", k)
-		}
-		return fmt.Sprintf("%d > %d at index %d, < %d at index %d",
-			k, s[x-1], x-1, s[x], x)
-	}
-	switch x {
-	case 0:
-		return fmt.Sprintf("%d found at beginning of slice", k)
-	case len(s) - 1:
-		return fmt.Sprintf("%d found at end of slice", k)
-	}
-	return fmt.Sprintf("%d found at index %d", k, x)
+	return -1
 }
