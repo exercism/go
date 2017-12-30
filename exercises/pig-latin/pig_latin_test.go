96709303f45cd9b9efd80cd506424deafff8ab0b
Andrea Manzini <ilmanzo@gmail.com>
2017-12-30 22:18:30 +0100

    go fmt

--- exercises/pig-latin/pig_latin_test.go
+++ exercises/pig-latin/pig_latin_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestPigLatin(t *testing.T) {
 	for _, test := range testCases {
 		if pl := PigLatin(test.input); pl != test.expected {
-      t.Fatalf("FAIL: %s\nPigLatin(%q) = %q, want %q.", test.description, test.input, pl, test.expected)
+			t.Fatalf("FAIL: %s\nPigLatin(%q) = %q, want %q.", test.description, test.input, pl, test.expected)
 		}
 	}
 }
