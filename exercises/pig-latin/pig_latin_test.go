27a70ae449b935ca92d5d9f672e3415aac5add8c
Andrea Manzini <ilmanzo@gmail.com>
2018-01-01 18:00:26 +0100

    each test gets a description printed and PASS or FAIL

--- exercises/pig-latin/pig_latin_test.go
+++ exercises/pig-latin/pig_latin_test.go
@@ -7,9 +7,9 @@ import (
 
 func TestPigLatin(t *testing.T) {
 	for _, test := range testCases {
-		fmt.Printf("Test: %s\n", test.description)
 		if pl := Sentence(test.input); pl != test.expected {
 			t.Fatalf("FAIL: Sentence(%q) = %q, want %q.", test.input, pl, test.expected)
 		}
+		fmt.Printf("PASS: %s\n", test.description)
 	}
 }
