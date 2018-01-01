f7b138355d1c0b2311ce3fbe3267354fdcce65a9
Ngoc Hai <ngochaidinh94@gmail.com>
2018-01-01 10:50:22 +0700

    Fix error formatting for simple-cipher

--- exercises/simple-cipher/simple_cipher_test.go
+++ exercises/simple-cipher/simple_cipher_test.go
@@ -96,7 +96,7 @@ func TestShift(t *testing.T) {
 	// invalid shifts
 	for _, s := range []int{-27, -26, 0, 26, 27} {
 		if NewShift(s) != nil {
-			t.Fatal("NewShift(%d) returned non-nil, "+
+			t.Fatalf("NewShift(%d) returned non-nil, "+
 				"Want nil return for invalid argument.", s)
 		}
 	}
