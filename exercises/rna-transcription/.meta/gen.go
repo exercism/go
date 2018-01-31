b7c2d24d684117c756786cebbe242188b3a477ea
Lee Nipper <lee.nipper@gmail.com>
2018-01-30 22:33:36 -0600

    rna-transcription: fix generator for input change
    Add Input to match canonical-json.data.
    Remove obsolete commentary about test cases not used.
    Record test description in the test case data.

--- exercises/rna-transcription/.meta/gen.go
+++ exercises/rna-transcription/.meta/gen.go
@@ -22,33 +22,25 @@ func main() {
 type js struct {
 	Cases []struct {
 		Description string
-		Dna         string
-		Expected    string
+		Input       struct {
+			Dna string
+		}
+		Expected string
 	}
 }
 
 // Template applied to above data structure generates the Go test cases.
-//
-// Note x-common test cases have some test cases that are not used here.
-//
-// It has some with invalid DNA inputs indicated with null expected values.
-// The current API does not have an error or ok return.  It could be changed
-// but it's simple for now, leaving the result undefined for invalid input.
-// So these test cases are skipped here.
-//
-// Also the x-common test cases include some test cases in the reverse
-// direction, from rna to dna.  These are not called for by the exercise
-// readme and have no biological basis and so are not converted here.
 var tmpl = `package strand
 
 {{.Header}}
 
 var rnaTests = []struct {
-	input    string
-	expected string
+	description string
+	input       string
+	expected    string
 }{
-{{range .J.Cases}} {{if and .Dna .Expected}} // {{.Description}}
-	{"{{.Dna}}", "{{.Expected}}"},
-
-{{end}}{{end}}}
+{{range .J.Cases}}{ "{{.Description}}",
+	"{{.Input.Dna}}", "{{.Expected}}",
+},
+{{end}}}
 `
