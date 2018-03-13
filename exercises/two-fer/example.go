1bf5dfbfa1acde550c6d58e9e4502ec7fef198a4
Wayne Warren <wayne.warren.s@gmail.com>
2018-03-13 18:29:17 -0500

    two-fer: Remove extraneous go files.

--- exercises/two-fer/example.go
+++
@@ -1,9 +0,0 @@
-package twofer
-
-// ShareWith apportions the goods between two people.
-func ShareWith(name string) string {
-	if name == "" {
-		name = "you"
-	}
-	return "One for " + name + ", one for me."
-}
