2ff823b2ec91a214cb1b5afad888125b16614d26
zeimusu <james.kilfiger@gmail.com>
2018-03-15 20:49:11 +0000

    Update cases_test.go
    Spelling Dominoe ->Domino

--- exercises/dominoes/cases_test.go
+++ exercises/dominoes/cases_test.go
@@ -6,37 +6,37 @@ package dominoes
 
 var testCases = []struct {
 	description string
-	dominoes    []Dominoe
+	dominoes    []Domino
 	valid       bool // true => can chain, false => cannot chain
 }{
 	{
 		"empty input = empty output",
-		[]Dominoe{},
+		[]Domino{},
 		true,
 	},
 	{
 		"singleton input = singleton output",
-		[]Dominoe{Dominoe{1, 1}},
+		[]Domino{Domino{1, 1}},
 		true,
 	},
 	{
 		"singleton that can't be chained",
-		[]Dominoe{Dominoe{1, 2}},
+		[]Domino{Domino{1, 2}},
 		false,
 	},
 	{
 		"three elements",
-		[]Dominoe{Dominoe{1, 2}, Dominoe{3, 1}, Dominoe{2, 3}},
+		[]Domino{Domino{1, 2}, Domino{3, 1}, Domino{2, 3}},
 		true,
 	},
 	{
 		"can reverse dominoes",
-		[]Dominoe{Dominoe{1, 2}, Dominoe{1, 3}, Dominoe{2, 3}},
+		[]Domino{Domino{1, 2}, Domino{1, 3}, Domino{2, 3}},
 		true,
 	},
 	{
 		"can't be chained",
-		[]Dominoe{Dominoe{1, 2}, Dominoe{4, 1}, Dominoe{2, 3}},
+		[]Domino{Domino{1, 2}, Domino{4, 1}, Domino{2, 3}},
 		false,
 	},
 	{
@@ -46,17 +46,17 @@ var testCases = []struct {
 		//Nevertheless, there is no chain here, as there's no way to get from 1 to 2.
 		//This test (and the two following) prevent solutions from using the even-degree test as the sole criterion,
 		//as that is not a sufficient condition.
-		[]Dominoe{Dominoe{1, 1}, Dominoe{2, 2}},
+		[]Domino{Domino{1, 1}, Domino{2, 2}},
 		false,
 	},
 	{
 		"disconnected - double loop",
-		[]Dominoe{Dominoe{1, 2}, Dominoe{2, 1}, Dominoe{3, 4}, Dominoe{4, 3}},
+		[]Domino{Domino{1, 2}, Domino{2, 1}, Domino{3, 4}, Domino{4, 3}},
 		false,
 	},
 	{
 		"disconnected - single isolated",
-		[]Dominoe{Dominoe{1, 2}, Dominoe{2, 3}, Dominoe{3, 1}, Dominoe{4, 4}},
+		[]Domino{Domino{1, 2}, Domino{2, 3}, Domino{3, 1}, Domino{4, 4}},
 		false,
 	},
 	{
@@ -66,17 +66,17 @@ var testCases = []struct {
 		//there is no chain possible.
 		//There is indeed a chain here, so this test checks for this line of reasoning.
 		//You need to place the (2, 4) after the (1, 2) rather than the (2, 3).
-		[]Dominoe{Dominoe{1, 2}, Dominoe{2, 3}, Dominoe{3, 1}, Dominoe{2, 4}, Dominoe{2, 4}},
+		[]Domino{Domino{1, 2}, Domino{2, 3}, Domino{3, 1}, Domino{2, 4}, Domino{2, 4}},
 		true,
 	},
 	{
 		"separate loops",
-		[]Dominoe{Dominoe{1, 2}, Dominoe{2, 3}, Dominoe{3, 1}, Dominoe{1, 1}, Dominoe{2, 2}, Dominoe{3, 3}},
+		[]Domino{Domino{1, 2}, Domino{2, 3}, Domino{3, 1}, Domino{1, 1}, Domino{2, 2}, Domino{3, 3}},
 		true,
 	},
 	{
 		"nine elements",
-		[]Dominoe{Dominoe{1, 2}, Dominoe{5, 3}, Dominoe{3, 1}, Dominoe{1, 2}, Dominoe{2, 4}, Dominoe{1, 6}, Dominoe{2, 3}, Dominoe{3, 4}, Dominoe{5, 6}},
+		[]Domino{Domino{1, 2}, Domino{5, 3}, Domino{3, 1}, Domino{1, 2}, Domino{2, 4}, Domino{1, 6}, Domino{2, 3}, Domino{3, 4}, Domino{5, 6}},
 		true,
 	},
 }
