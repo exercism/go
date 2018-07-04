8c7b5bd4dc5cca55ca82c49aec84b96b236221d5
Alex Garrett <ljagged@gmail.com>
2018-07-04 11:43:06 -0500

    list-ops:  add exercise (#1126)
    Resolves #823

---
+++ exercises/list-ops/example.go
@@ -0,0 +1,99 @@
+package listops
+
+// IntList is an abstraction of a list of integers
+type IntList []int
+type predFunc func(int) bool
+type binFunc func(int, int) int
+type unaryFunc func(int) int
+
+// Foldl folds (reduces) the given list from the left with a function
+func (s IntList) Foldl(fn binFunc, initial int) int {
+	if len(s) == 0 {
+		return initial
+	}
+	x, xs := s[0], s[1:]
+	return xs.Foldl(fn, fn(initial, x))
+}
+
+// Foldr folds (reduces) the given list from the right with a function
+func (s IntList) Foldr(fn binFunc, initial int) int {
+	flippedFunc := func(x, y int) int { return fn(y, x) }
+	// Note: This relies on s being finite
+	return s.Reverse().Foldl(flippedFunc, initial)
+}
+
+// Filter list returning only values that satisfy the filter function
+func (s IntList) Filter(fn predFunc) IntList {
+	filtered := make([]int, 0, len(s))
+	var filterAcc func(predFunc, IntList, IntList) IntList
+	filterAcc = func(fn predFunc, acc IntList, lst IntList) IntList {
+		if len(lst) == 0 {
+			return acc
+		}
+		x, xs := lst[0], lst[1:]
+		if fn(x) {
+			acc = append(acc, x)
+		}
+		return filterAcc(fn, acc, xs)
+	}
+	return filterAcc(fn, filtered, s)
+}
+
+// Length returns the length of a list
+func (s IntList) Length() int {
+	// for fun, let's do this with recursion
+	var lengthAcc func(IntList, int) int
+	var isEmpty func(IntList) bool
+	// yeah, this is kind of cheaty, but it's hard to determine if a slice is
+	// empty without using len, so walling it off
+	isEmpty = func(l IntList) bool {
+		return len(l) == 0
+	}
+	lengthAcc = func(lst IntList, acc int) int {
+		if isEmpty(lst) {
+			return acc
+		}
+		return lengthAcc(lst[1:], acc+1)
+	}
+	return lengthAcc(s, 0)
+}
+
+// Map returns a list of elements whose values equal the list value transformed by the mapping function
+func (s IntList) Map(fn unaryFunc) IntList {
+	newSlice := make([]int, len(s))
+	for idx, elt := range s {
+		newSlice[idx] = fn(elt)
+	}
+	return newSlice
+}
+
+// Reverse reverses the list
+func (s IntList) Reverse() IntList {
+	last := len(s) - 1
+	newSlice := make([]int, len(s))
+	for idx, elt := range s {
+		newSlice[last-idx] = elt
+	}
+	return newSlice
+}
+
+// Append adds the elements of the argument list to the receiver
+func (s IntList) Append(lst IntList) IntList {
+	offset := len(s)
+	newSlice := make([]int, offset+len(lst))
+	copy(newSlice, s)
+	for idx, elt := range lst {
+		newSlice[offset+idx] = elt
+	}
+	return newSlice
+}
+
+// Concat concatenates a list of lists
+func (s IntList) Concat(lists []IntList) IntList {
+	newSlice := make([]int, len(s))
+	copy(newSlice, s)
+	for _, l := range lists {
+		newSlice = append(newSlice, l...)
+	}
+	return newSlice
+}
