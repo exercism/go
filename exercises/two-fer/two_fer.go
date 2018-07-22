550cf512f6f02d1cb69a68afbb08a94e3d19bb51
r4wm <raymondmintz11@gmail.com>
2018-07-22 12:22:01 -0700

    two-fer: add param name to stub function (#1136)

--- exercises/two-fer/two_fer.go
+++ exercises/two-fer/two_fer.go
@@ -6,7 +6,7 @@
 package twofer
 
 // ShareWith should have a comment documenting it.
-func ShareWith(string) string {
+func ShareWith(name string) string {
 	// Write some code here to pass the test suite.
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
