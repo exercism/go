e023a1adb3f73ed5175827d71c5bcd2402538053
Hilary Holz <hilary@hholz.com>
2018-07-30 13:49:06 -0700

    twofer: add benchmark

--- exercises/two-fer/two_fer_test.go
+++ exercises/two-fer/two_fer_test.go
@@ -4,17 +4,28 @@ import "testing"
 
 // Define a function ShareWith(string) string.
 
+var tests = []struct {
+	name, expected string
+}{
+	{"", "One for you, one for me."},
+	{"Alice", "One for Alice, one for me."},
+	{"Bob", "One for Bob, one for me."},
+}
+
 func TestShareWith(t *testing.T) {
-	tests := []struct {
-		name, expected string
-	}{
-		{"", "One for you, one for me."},
-		{"Alice", "One for Alice, one for me."},
-		{"Bob", "One for Bob, one for me."},
-	}
 	for _, test := range tests {
 		if observed := ShareWith(test.name); observed != test.expected {
 			t.Fatalf("ShareWith(%s) = %v, want %v", test.name, observed, test.expected)
 		}
+	}
+}
+
+func BenchmarkShareWith(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+
+		for _, test := range tests {
+			ShareWith(test.name)
+		}
+
 	}
 }
