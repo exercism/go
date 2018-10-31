08952ee04ed5daaea06775c2f583198d507d90d7
Harald Nordgren <haraldnordgren@gmail.com>
2018-10-31 19:18:49 +0100

    travis: bump go versions (#1172)

--- exercises/scrabble-score/cases_test.go
+++ exercises/scrabble-score/cases_test.go
@@ -10,15 +10,15 @@ type scrabbleTest struct {
 }
 
 var scrabbleScoreTests = []scrabbleTest{
-	{"a", 1},                // lowercase letter
-	{"A", 1},                // uppercase letter
-	{"f", 4},                // valuable letter
-	{"at", 2},               // short word
-	{"zoo", 12},             // short, valuable word
-	{"street", 6},           // medium word
-	{"quirky", 22},          // medium, valuable word
-	{"OxyphenButazone", 41}, // long, mixed-case word
-	{"pinata", 8},           // english-like word
-	{"", 0},                 // empty input
+	{"a", 1},                           // lowercase letter
+	{"A", 1},                           // uppercase letter
+	{"f", 4},                           // valuable letter
+	{"at", 2},                          // short word
+	{"zoo", 12},                        // short, valuable word
+	{"street", 6},                      // medium word
+	{"quirky", 22},                     // medium, valuable word
+	{"OxyphenButazone", 41},            // long, mixed-case word
+	{"pinata", 8},                      // english-like word
+	{"", 0},                            // empty input
 	{"abcdefghijklmnopqrstuvwxyz", 87}, // entire alphabet available
 }
