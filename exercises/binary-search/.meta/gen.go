7246816539509b883a98e6477f967887d4ed5d2b
Andrea Manzini <ilmanzo@gmail.com>
2018-11-04 04:23:02 +0100

    binary-search: update generator (#1169)

--- exercises/binary-search/.meta/gen.go
+++ exercises/binary-search/.meta/gen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"text/template"
 
@@ -33,7 +34,30 @@ type oneCase struct {
 		Array []int
 		Value int
 	}
-	Expected int
+	Expected ExpectedType
+}
+
+type ExpectedType struct {
+	ValueInt    int
+	ValueString string
+}
+
+func (e *ExpectedType) UnmarshalJSON(b []byte) error {
+	if b[0] != '{' {
+		var i int
+		if err := json.Unmarshal(b, &i); err != nil {
+			return err
+		}
+		e.ValueInt = i
+		return nil
+	}
+	var s map[string]string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+	e.ValueInt = -1
+	e.ValueString = s["error"]
+	return nil
 }
 
 // Template to generate test cases.
@@ -41,17 +65,19 @@ var tmpl = `package binarysearch
 
 {{.Header}}
 
-var testCases = []struct {
+var testCases =	[]struct {
 	description	string
 	slice		[]int
-	key		int
-	x	int
+	key			int
+	x			int
+	err			string
 }{ {{range .J.Cases}}
 {
 	description:	{{printf "%q"  .Description}},
 	slice:		{{printf "%#v" .Input.Array}},
-	key:		{{printf "%d"  .Input.Value}},
-	x:	{{printf "%d"  .Expected}},
+	key:			{{printf "%d"  .Input.Value}},
+	x:			{{printf "%d"  .Expected.ValueInt}},
+	err:			{{printf "%q"  .Expected.ValueString}},
 },{{end}}
 }
 `
