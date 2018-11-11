ec3abdb07ff6ccce40fd75938eed0021f1dc7fb8
Lee Nipper <lee.nipper@gmail.com>
2018-11-10 22:46:31 -0600

    leap: add new test case
    Update generated cases_test.go with new test case,
    "year divisible by 200, not divisible by 400: common year",
    from problem-specifications.

--- exercises/leap/cases_test.go
+++ exercises/leap/cases_test.go
@@ -1,8 +1,8 @@
 package leap
 
 // Source: exercism/problem-specifications
-// Commit: e348053 leap: Apply new "input" policy
-// Problem Specifications Version: 1.3.0
+// Commit: 3134d31 leap 1.4.0: negative test: year divisible by 200, not by 400
+// Problem Specifications Version: 1.4.0
 
 var testCases = []struct {
 	year        int
@@ -13,4 +13,5 @@ var testCases = []struct {
 	{1996, true, "year divisible by 4, not divisible by 100: leap year"},
 	{2100, false, "year divisible by 100, not divisible by 400: common year"},
 	{2000, true, "year divisible by 400: leap year"},
+	{1800, false, "year divisible by 200, not divisible by 400: common year"},
 }
