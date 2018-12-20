6f214fc02bb8cbf946e5c59ff0b403beaacdd835
Tina Schoenborn <tina.schoenborn@innoq.com>
2018-12-20 09:51:09 +0100

    remove double negation in test

--- exercises/isbn-verifier/isbn_verifier_test.go
+++ exercises/isbn-verifier/isbn_verifier_test.go
@@ -7,11 +7,11 @@ import (
 func TestIsValidISBN(t *testing.T) {
 	for _, test := range testCases {
 		observed := IsValidISBN(test.isbn)
-		if observed != test.expected {
+		if observed == test.expected {
+			t.Logf("PASS: %s", test.description)
+		} else {
 			t.Errorf("FAIL: %s\nIsValidISBN(%q)\nExpected: %t, Actual: %t",
 				test.description, test.isbn, test.expected, observed)
-		} else {
-			t.Logf("PASS: %s", test.description)
 		}
 	}
 }
