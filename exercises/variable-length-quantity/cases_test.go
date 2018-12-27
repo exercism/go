2745b447a074c4a7e7f5ff6d210d58c4cffc6287
Andrea Manzini <ilmanzo@gmail.com>
2018-12-27 23:17:28 +0100

    variable-length-quantity: update generator to upstream problem specifications
    fix naming for error flag

--- exercises/variable-length-quantity/cases_test.go
+++ exercises/variable-length-quantity/cases_test.go
@@ -1,8 +1,8 @@
 package variablelengthquantity
 
 // Source: exercism/problem-specifications
-// Commit: 48dc5e6 variable-length-quantity: Apply new "input" policy
-// Problem Specifications Version: 1.1.0
+// Commit: 191f8086 variable-length-quantity: add mention of 'error' to comment
+// Problem Specifications Version: 1.2.0
 
 // Encode a series of integers, producing a series of bytes.
 var encodeTestCases = []struct {
@@ -107,46 +107,55 @@ var decodeTestCases = []struct {
 	description string
 	input       []byte
 	output      []uint32 // nil slice indicates error expected.
+	shouldFail  bool
 }{
 
 	{
 		"one byte",
 		[]byte{0x7f},
 		[]uint32{0x7f},
+		false,
 	},
 	{
 		"two bytes",
 		[]byte{0xc0, 0x0},
 		[]uint32{0x2000},
+		false,
 	},
 	{
 		"three bytes",
 		[]byte{0xff, 0xff, 0x7f},
 		[]uint32{0x1fffff},
+		false,
 	},
 	{
 		"four bytes",
 		[]byte{0x81, 0x80, 0x80, 0x0},
 		[]uint32{0x200000},
+		false,
 	},
 	{
 		"maximum 32-bit integer",
 		[]byte{0x8f, 0xff, 0xff, 0xff, 0x7f},
 		[]uint32{0xffffffff},
+		false,
 	},
 	{
 		"incomplete sequence causes error",
 		[]byte{0xff},
-		[]uint32(nil),
+		[]uint32{},
+		true,
 	},
 	{
 		"incomplete sequence causes error, even if value is zero",
 		[]byte{0x80},
-		[]uint32(nil),
+		[]uint32{},
+		true,
 	},
 	{
 		"multiple values",
 		[]byte{0xc0, 0x0, 0xc8, 0xe8, 0x56, 0xff, 0xff, 0xff, 0x7f, 0x0, 0xff, 0x7f, 0x81, 0x80, 0x0},
 		[]uint32{0x2000, 0x123456, 0xfffffff, 0x0, 0x3fff, 0x4000},
+		false,
 	},
 }
