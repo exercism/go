ef55e6b0bf6822d97ad3f8af8aee18d0062a33b2
Seth <github@bromberger.com>
2019-02-04 08:26:40 -0800

    implements #1230

--- exercises/reverse-string/example.go
+++ exercises/reverse-string/example.go
@@ -1,7 +1,7 @@
 package reverse
 
-// String reverses given string
-func String(s string) string {
+// Reverse reverses given string
+func Reverse(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
