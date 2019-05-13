ac93fdf7881f8c57bc02a3bfde895d713302a535
Weidong Feng <weidong.feng@shanbay.com>
2019-05-13 10:29:37 +0800

    remove all example files

--- exercises/accumulate/example.go
+++
@@ -1,9 +0,0 @@
-package accumulate
-
-// Accumulate transform given strings by using given function
-func Accumulate(s []string, f func(st string) string) (result []string) {
-	for _, v := range s {
-		result = append(result, f(v))
-	}
-	return result
-}
--- exercises/gigasecond/example.go
+++
@@ -1,8 +0,0 @@
-package gigasecond
-
-import "time"
-
-// AddGigasecond returns time t plus one gigasecond.
-func AddGigasecond(t time.Time) time.Time {
-	return t.Add(1e9 * time.Second)
-}
