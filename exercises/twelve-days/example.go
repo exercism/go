baed602bed6e81cda56f5bc0598392fd1c07d310
Isaac Good <nuc-git@isaacgood.com>
2019-07-22 08:05:09 -0700

    Create the array at full size ahead of time

--- exercises/twelve-days/example.go
+++ exercises/twelve-days/example.go
@@ -50,9 +50,9 @@ func Verse(i int) string {
 }
 
 func Song() string {
-	var song []string
+	var song = make([]string, 12)
 	for i := 1; i <= 12; i++ {
-		song = append(song, Verse(i))
+		song[i-1] = Verse(i)
 	}
 	return strings.Join(song, "\n")
 }
