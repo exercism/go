773c985acc5541efe9949caa56384a8525647d2d
luca paterlini <paterlini.luca@gmail.com>
2019-08-25 14:16:19 +0100

    suggestion to add one or more test cases
    this will force the reader to an optimal solution

--- exercises/two-bucket/bonus_test.go
+++ exercises/two-bucket/bonus_test.go
@@ -13,6 +13,9 @@ var noSolutionCases = []bucketTestCase{
 		"No solution case 2",
 		3, 6, 1, "one", "", 0, 0, true,
 	},
+	{	"Adding Complexity",
+		3333333, 6666667, 1, "one", "one", 19999992, 6666667, false,
+	},
 }
 
 func TestSolve_bonus(t *testing.T) {
