ffb73b68ba9b56cc4f18655feedee13993d96dcf
Lee Nipper <lee.nipper@gmail.com>
2019-09-01 17:51:23 -0500

    parallel-letter-frequency: improved stub and README.md
    For parallel-letter-frequency exercise,
    students have had some confusion over the
    file named frequency.go and its content, and
    whether or not to delete it.
    
    Move the necessary declarations from frequency.go
    into both the stub file and the example.go.
    
    Remove the frequency.go file.
    
    Update README.md to give instruction about
    coding the solution in the stub file.

--- exercises/parallel-letter-frequency/parallel_letter_frequency.go
+++ exercises/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,1 +1,14 @@
 package letter
+
+// FreqMap records the frequency of each rune in a given text.
+type FreqMap map[rune]int
+
+// Frequency counts the frequency of each rune in a given text and returns this
+// data as a FreqMap.
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
