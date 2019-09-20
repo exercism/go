b254e8e6eda51067f7fd3aaa5c4d597ed79bde09
udhos <udhos>
2019-09-20 02:53:34 -0300

    Define constants for rods names in order to make the problem description a bit cleaner.

--- exercises/tower-of-hanoi/example.go
+++ exercises/tower-of-hanoi/example.go
@@ -12,8 +12,18 @@ package towerofhanoi
 // A move is a [2]int value specifying source and destination rods.
 //
 // Example
-// Solve(1, 'A', 'B', 'C') could return a list with a single move:
-//  [][2]int{{'A', 'B'}} // move disk from rod 'A' to rod 'B'
+//
+// In order to illustrate the problem, consider the constants below
+// naming the rods. Your implementation is free to define similar
+// constants, but it is not required to do so.
+// const (
+//      A = 'A'
+//      B = 'B'
+//      C = 'C'
+// )
+//
+// Solve(1, A, B, C) could return a list with a single move:
+//  [][2]int{{A, B}} // move disk from rod A to rod B
 func Solve(disks, from, to, via int) [][2]int {
 
 	if disks < 1 {
