fe8c5ec45c640e32682387dfa34e660834c2cc79
Jack Mordaunt <jackmordaunt@gmail.com>
2020-01-17 09:50:14 +1300

    prime-factors: accept order-independent results by sorting in the test harness.

--- exercises/prime-factors/prime_factors_test.go
+++ exercises/prime-factors/prime_factors_test.go
@@ -1,15 +1,15 @@
 package prime
 
-// Return prime factors in increasing order
-
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
 func TestPrimeFactors(t *testing.T) {
 	for _, test := range tests {
 		actual := Factors(test.input)
+		sort.Sort(ascending(actual))
 		if !reflect.DeepEqual(actual, test.expected) {
 			t.Fatalf("FAIL %s\nFactors(%d) = %#v;\nexpected %#v",
 				test.description, test.input,
@@ -26,3 +26,9 @@ func BenchmarkPrimeFactors(b *testing.B) {
 		}
 	}
 }
+
+type ascending []int64
+
+func (l ascending) Len() int             { return len(l) }
+func (l ascending) Swap(ii, jj int)      { l[ii], l[jj] = l[jj], l[ii] }
+func (l ascending) Less(ii, jj int) bool { return l[ii] < l[jj] }
