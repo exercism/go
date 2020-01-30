16f4e8660af295ca5faba214fc1d144dffa6ab96
Gabriel Nelle <tehsphinx@web.de>
2020-01-30 05:12:46 +0100

    Concept Exercise: strings
    * Concept Exercise: strings
    
    * refactor the strings exercise
    
    * rename to `basic-strings` and cleanup
    
    * update concept/Readme.md
    
    * better formatted error messages in the tests
    
    * remove config.json
    
    * more indepth introduction on runes
    
    * similarities to slices
    
    * requested changes

---
+++ exercises/concept/basic-strings/basic_strings.go
@@ -0,0 +1,21 @@
+package basic_strings
+
+// Message extracts the message from the provided log line.
+func Message(line string) string {
+	panic("not implemented")
+}
+
+// MessageLen counts the amount of characters (runes) in the message of the log line.
+func MessageLen(line string) int {
+	panic("not implemented")
+}
+
+// LogLevel extracts the log level string from the provided log line.
+func LogLevel(line string) string {
+	panic("not implemented")
+}
+
+// Reformat reformats the log line in the format `message (logLevel)`.
+func Reformat(line string) string {
+	panic("not implemented")
+}
