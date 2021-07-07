e1ee2741dca26ce2318cc956a949836ab196637d
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-07 13:25:30 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible.

--- exercises/practice/hello-world/hello_world.go
+++ exercises/practice/hello-world/hello_world.go
@@ -1,15 +1,6 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package greeting should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package greeting
 
-// HelloWorld should have a comment documenting it.
+// HelloWorld greets the world.
 func HelloWorld() string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	return "Goodbye, Mars!"
 }
