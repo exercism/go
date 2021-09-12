537e2d3ef1021534eac9f8b152f502f83403a42a
Jason Runkle <jmrunkle@google.com>
2021-09-12 07:21:48 -0700

    Add missing parameters to stub (#1538)
    * Add missing parameters to stub
    
    Fixes #1537
    
    * Update instructions to improve clarity
    
    Also match the tests and stub configuration.
    
    * Fix gocritic complaint about combining units

--- exercises/concept/gross-store/gross_store.go
+++ exercises/concept/gross-store/gross_store.go
@@ -11,12 +11,12 @@ func NewBill() map[string]int {
 }
 
 // AddItem add item to customer bill
-func AddItem(bill map[string]int, item string, unit string) bool {
+func AddItem(bill, units map[string]int, item, unit string) bool {
 	panic("Please implement the AddItem() function")
 }
 
 // RemoveItem remove item from customer bill
-func RemoveItem(bill map[string]int, item string, unit string) bool {
+func RemoveItem(bill, units map[string]int, item, unit string) bool {
 	panic("Please implement the RemoveItem() function")
 }
 
