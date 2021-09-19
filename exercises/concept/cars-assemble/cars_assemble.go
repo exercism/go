60775142635543fe4935b34c9be08b7b10a3c344
Vishnu Sreekumar <vishnu.srkmr@gmail.com>
2021-09-20 07:55:49 +0800

    Mention the function names in the panic output to facilitate TDD

--- exercises/concept/cars-assemble/cars_assemble.go
+++ exercises/concept/cars-assemble/cars_assemble.go
@@ -3,13 +3,13 @@ package cars
 // CalculateProductionRatePerHour for the assembly line, taking into account
 // its success rate
 func CalculateProductionRatePerHour(speed int) float64 {
-	panic("not implemented")
+	panic("CalculateProductionRatePerHour not implemented")
 }
 
 // CalculateProductionRatePerMinute describes how many working items are
 // produced by the assembly line every minute
 func CalculateProductionRatePerMinute(speed int) int {
-	panic("not implemented")
+	panic("CalculateProductionRatePerMinute not implemented")
 }
 
 // successRate is used to calculate the ratio of an item being created without
