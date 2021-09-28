ad08be56f1d51d78a4f3cc6c4aae49a224f573f3
Jason Runkle <jmrunkle@gmail.com>
2021-09-28 17:34:32 +0000

    gofmt the test file

--- exercises/concept/sorting-room/sorting_room_test.go
+++ exercises/concept/sorting-room/sorting_room_test.go
@@ -9,24 +9,24 @@ func TestDescribeNumber(t *testing.T) {
 		want        string
 	}{
 		{
-			description:  "Describe 4.1",
-			input: 4.1,
-			want:  "This is the number 4.1",
+			description: "Describe 4.1",
+			input:       4.1,
+			want:        "This is the number 4.1",
 		},
 		{
-			description:  "Describe -3.2",
-			input: -3.2,
-			want:  "This is the number -3.2",
+			description: "Describe -3.2",
+			input:       -3.2,
+			want:        "This is the number -3.2",
 		},
 		{
-			description:  "Pads to single decimal place",
-			input: 4.0,
-			want:  "This is the number 4.0",
+			description: "Pads to single decimal place",
+			input:       4.0,
+			want:        "This is the number 4.0",
 		},
 		{
-			description:  "Truncates to single decimal place",
-			input: 7.11,
-			want:  "This is the number 7.1",
+			description: "Truncates to single decimal place",
+			input:       7.11,
+			want:        "This is the number 7.1",
 		},
 	}
 	for _, test := range tests {
@@ -45,14 +45,14 @@ func TestDescribeBoolean(t *testing.T) {
 		want        string
 	}{
 		{
-			description:  "Describe true",
-			input: true,
-			want:  "This is true",
+			description: "Describe true",
+			input:       true,
+			want:        "This is true",
 		},
 		{
-			description:  "Describe false",
-			input: false,
-			want:  "This is false",
+			description: "Describe false",
+			input:       false,
+			want:        "This is false",
 		},
 	}
 	for _, test := range tests {
@@ -79,14 +79,14 @@ func TestDescribeNumberBox(t *testing.T) {
 		want        string
 	}{
 		{
-			description:  "Describe NumberBox with 4",
-			input: testNumberBox{4},
-			want:  "This is a box containing the number 4.0",
+			description: "Describe NumberBox with 4",
+			input:       testNumberBox{4},
+			want:        "This is a box containing the number 4.0",
 		},
 		{
-			description:  "Describe NumberBox with -3",
-			input: testNumberBox{-3},
-			want:  "This is a box containing the number -3.0",
+			description: "Describe NumberBox with -3",
+			input:       testNumberBox{-3},
+			want:        "This is a box containing the number -3.0",
 		},
 	}
 	for _, test := range tests {
@@ -113,14 +113,14 @@ func TestDescribeBooleanBox(t *testing.T) {
 		want        string
 	}{
 		{
-			description:  "Describe BooleanBox with true",
-			input: testBooleanBox{true},
-			want:  "This box contains true",
+			description: "Describe BooleanBox with true",
+			input:       testBooleanBox{true},
+			want:        "This box contains true",
 		},
 		{
-			description:  "Describe false",
-			input: testBooleanBox{false},
-			want:  "This box contains false",
+			description: "Describe false",
+			input:       testBooleanBox{false},
+			want:        "This box contains false",
 		},
 	}
 	for _, test := range tests {
@@ -139,29 +139,29 @@ func TestDescribeAnything(t *testing.T) {
 		want        string
 	}{
 		{
-			description:  "Describe 7.2",
-			input: 7.2,
-			want:  "This is the number 7.2",
+			description: "Describe 7.2",
+			input:       7.2,
+			want:        "This is the number 7.2",
 		},
 		{
-			description:  "Describe false",
-			input: false,
-			want:  "This is false",
+			description: "Describe false",
+			input:       false,
+			want:        "This is false",
 		},
 		{
-			description:  "Describe NumberBox with 16",
-			input: testNumberBox{16},
-			want:  "This is a box containing the number 16.0",
+			description: "Describe NumberBox with 16",
+			input:       testNumberBox{16},
+			want:        "This is a box containing the number 16.0",
 		},
 		{
-			description:  "BooleanBox with true",
-			input: testBooleanBox{true},
-			want:  "This box contains true",
+			description: "BooleanBox with true",
+			input:       testBooleanBox{true},
+			want:        "This box contains true",
 		},
 		{
-			description:  "Something unknown is labelled return to sender",
-			input: "something we did not anticipate",
-			want:  "Return to sender",
+			description: "Something unknown is labelled return to sender",
+			input:       "something we did not anticipate",
+			want:        "Return to sender",
 		},
 	}
 	for _, test := range tests {
