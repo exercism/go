e73176fa318ef4b0b4182f6fe2b643a963ff945c
André Santos <andrerfcsantos@gmail.com>
2021-10-01 20:53:43 +0100

    Add pointer concept
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    Co-authored-by: June <12543047+junedev@users.noreply.github.com>

---
+++ exercises/concept/election-day/election_day.go
@@ -0,0 +1,32 @@
+package electionday
+
+// NewVoteCounter returns a new vote counter with
+// a given number of inital votes.
+func NewVoteCounter(initialVotes int) *int {
+	panic("Please implement the NewVoteCounter() function")
+}
+
+// VoteCount extracts the number of votes from a counter.
+func VoteCount(counter *int) int {
+	panic("Please implement the VoteCount() function")
+}
+
+// IncrementVoteCount increments the value in a vote counter
+func IncrementVoteCount(counter *int, increment int) {
+	panic("Please implement the IncrementVoteCount() function")
+}
+
+// NewElectionResult creates a new election result
+func NewElectionResult(candidateName string, votes int) *ElectionResult {
+	panic("Please implement the NewElectionResult() function")
+}
+
+// DisplayResult creates a message with the result to be displayed
+func DisplayResult(result *ElectionResult) string {
+	panic("Please implement the DisplayResult() function")
+}
+
+// DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map
+func DecrementVotesOfCandidate(results map[string]int, candidate string) {
+	panic("Please implement the DecrementVotesOfCandidate() function")
+}
