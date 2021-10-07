47a21e3c520461e34f873f7304450996bdee0bbb
Peter Turner <syntax.valid@gmail.com>
2021-10-07 21:07:12 +0100

    Fix typo in Lasagna Master test name (#1821)

--- exercises/concept/lasagna-master/lasagna-master_test.go
+++ exercises/concept/lasagna-master/lasagna-master_test.go
@@ -174,7 +174,7 @@ type scaleRecipeTest struct {
 	expected []float64
 }
 
-func TestSscaleRecipeTest(t *testing.T) {
+func TestScaleRecipe(t *testing.T) {
 	tests := []scaleRecipeTest{
 		scaleRecipeTest{
 			name:     "scales up correctly",
