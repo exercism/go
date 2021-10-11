620e0659c73f5cbdf612d15638b4d8320f7d2110
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-11 16:41:28 +0200

    interest-is-interesting: add exercise (#1740)
    * interest-is-interesting: add exercise
    
    * interest-is-interesting: add floating-point-numbers concept
    
    * interest-is-interesting: fix json
    
    * Apply suggestions from code review
    
    Co-authored-by: Jason Runkle <jmrunkle@gmail.com>
    
    * Update concepts/floating-point-numbers/about.md
    
    Co-authored-by: Jason Runkle <jmrunkle@gmail.com>
    
    * Update concepts/floating-point-numbers/about.md
    
    Co-authored-by: Jason Runkle <jmrunkle@gmail.com>
    
    * Update concepts/floating-point-numbers/about.md
    
    Co-authored-by: Jason Runkle <jmrunkle@gmail.com>
    
    * Update concepts/floating-point-numbers/about.md
    
    Co-authored-by: Jason Runkle <jmrunkle@gmail.com>
    
    * Update exercises/concept/interest-is-interesting/.meta/design.md
    
    Co-authored-by: Jason Runkle <jmrunkle@gmail.com>
    
    * Remove name parameters
    
    * Simplify int comparison
    
    * Make floatEqualityThreshold private
    
    * Add function for floating-point comparison
    
    * Add type-conversion prerequisite
    
    * Remove unused floating-point concept
    
    * Simplify return type text
    
    * Update exercises/concept/interest-is-interesting/.docs/hints.md
    
    Co-authored-by: Jason Runkle <jmrunkle@gmail.com>
    
    * Update interest instructions
    
    * interest-is-interesting: remove prerequisites
    
    Co-authored-by: Jason Runkle <jmrunkle@gmail.com>

---
+++ exercises/concept/interest-is-interesting/.meta/exemplar.go
@@ -0,0 +1,43 @@
+package savings
+
+import "math"
+
+// InterestRate calculates the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	if balance < 0.0 {
+		return -3.213
+	}
+
+	if balance < 1000.0 {
+		return 0.5
+	}
+
+	if balance < 5000.0 {
+		return 1.621
+	}
+
+	return 2.475
+}
+
+// InterestRate calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return math.Abs(balance) * (float64(InterestRate(balance)) / 100.0)
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return balance + Interest(balance)
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance:
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	var accumulatingBalance = balance
+	years := 0
+
+	for accumulatingBalance < targetBalance {
+		accumulatingBalance = AnnualBalanceUpdate(accumulatingBalance)
+		years++
+	}
+
+	return years
+}
