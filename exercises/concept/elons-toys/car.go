c3c664653fd5023d5b0cf6c1a77512aeef4b69ce
PakkuDon <5287128+PakkuDon@users.noreply.github.com>
2021-10-21 23:55:21 +1100

    elons-toys: Move Car and NewCar to separate file (#1878)

---
+++ exercises/concept/elons-toys/car.go
@@ -0,0 +1,19 @@
+package elon
+
+// Car implements a remote controlled car.
+type Car struct {
+	speed        int
+	batteryDrain int
+
+	battery  int
+	distance int
+}
+
+// NewCar creates a new car with given specifications.
+func NewCar(speed, batteryDrain int) *Car {
+	return &Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
+	}
+}
