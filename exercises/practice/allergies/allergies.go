c9bf8be4dfeafbaf58b91031492c32268a3e8e5e
PakkuDon <5287128+PakkuDon@users.noreply.github.com>
2021-10-25 01:28:03 +1100

    allergies: add stub functions (#1896)

--- exercises/practice/allergies/allergies.go
+++ exercises/practice/allergies/allergies.go
@@ -1,1 +1,9 @@
 package allergies
+
+func Allergies(allergies uint) []string {
+	panic("Please implement the Allergies function")
+}
+
+func AllergicTo(allergies uint, allergen string) bool {
+	panic("Please implement the AllergicTo function")
+}
