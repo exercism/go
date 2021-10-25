a064106d93ba21f485abb6b190390877d4817ee9
PakkuDon <pakku.pd@gmail.com>
2021-10-25 21:24:52 +1100

    circular-buffer: add stub functions and struct

--- exercises/practice/circular-buffer/circular_buffer.go
+++ exercises/practice/circular-buffer/circular_buffer.go
@@ -1,1 +1,27 @@
 package circular
+
+import "container/ring"
+
+type Buffer struct {
+	start, end *ring.Ring
+}
+
+func NewBuffer(size int) *Buffer {
+	panic("Please implement the NewBuffer function")
+}
+
+func (b *Buffer) ReadByte() (byte, error) {
+	panic("Please implement the ReadByte function")
+}
+
+func (b *Buffer) WriteByte(c byte) error {
+	panic("Please implement the WriteByte function")
+}
+
+func (b *Buffer) Overwrite(c byte) {
+	panic("Please implement the Overwrite function")
+}
+
+func (b *Buffer) Reset() {
+	panic("Please implement the Reset function")
+}
