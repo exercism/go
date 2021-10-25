6d83360630944cb56ba4a9c89ced27db4a29206a
PakkuDon <pakku.pd@gmail.com>
2021-10-25 21:58:54 +1100

    difference-of-squares: add stub functions

--- exercises/practice/difference-of-squares/difference_of_squares.go
+++ exercises/practice/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,13 @@
 package diffsquares
+
+func SquareOfSum(n int) int {
+	panic("Please implement the SquareOfSum function")
+}
+
+func SumOfSquares(n int) (s int) {
+	panic("Please implement the SumOfSquares function")
+}
+
+func Difference(n int) int {
+	panic("Please implement the Difference function")
+}
