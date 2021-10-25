0fe4a36b9ed4aae0cea31f2e4e955a82b7ce901c
PakkuDon <pakku.pd@gmail.com>
2021-10-25 21:27:00 +1100

    clock: add stub functions and type definition

--- exercises/practice/clock/clock.go
+++ exercises/practice/clock/clock.go
@@ -1,1 +1,19 @@
 package clock
+
+type Clock int
+
+func New(h, m int) Clock {
+	panic("Please implement the New function")
+}
+
+func (c Clock) Add(m int) Clock {
+	panic("Please implement the Add function")
+}
+
+func (c Clock) Subtract(m int) Clock {
+	panic("Please implement the Subtract function")
+}
+
+func (c Clock) String() string {
+	panic("Please implement the String function")
+}
