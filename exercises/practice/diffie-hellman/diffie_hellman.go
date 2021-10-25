01b79f0cc367b53121193d6e48430854a1a87576
PakkuDon <pakku.pd@gmail.com>
2021-10-25 22:03:16 +1100

    diffie-hellman: add stub functions

--- exercises/practice/diffie-hellman/diffie_hellman.go
+++ exercises/practice/diffie-hellman/diffie_hellman.go
@@ -1,1 +1,19 @@
 package diffiehellman
+
+import "math/big"
+
+func PrivateKey(p *big.Int) *big.Int {
+	panic("Please implement the PrivateKey function")
+}
+
+func PublicKey(private, p *big.Int, g int64) *big.Int {
+	panic("Please implement the PublicKey function")
+}
+
+func NewPair(p *big.Int, g int64) (private, public *big.Int) {
+	panic("Please implement the NewPair function")
+}
+
+func SecretKey(private1, public2, p *big.Int) *big.Int {
+	panic("Please implement the SecretKey function")
+}
