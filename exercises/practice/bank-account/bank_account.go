42b9035ab742d7bd0d2d356dde61abbd841401e2
PakkuDon <pakku.pd@gmail.com>
2021-10-25 21:03:13 +1100

    bank-account: add stub functions and struct

--- exercises/practice/bank-account/bank_account.go
+++ exercises/practice/bank-account/bank_account.go
@@ -1,1 +1,25 @@
 package account
+
+import "sync"
+
+type Account struct {
+	sync.RWMutex
+	open    bool
+	balance int64
+}
+
+func Open(amt int64) *Account {
+	panic("Please implement the Open function")
+}
+
+func (account *Account) Balance() (bal int64, ok bool) {
+	panic("Please implement the Balance function")
+}
+
+func (account *Account) Deposit(amt int64) (newBal int64, ok bool) {
+	panic("Please implement the Deposit function")
+}
+
+func (account *Account) Close() (pay int64, ok bool) {
+	panic("Please implement the Close function")
+}
