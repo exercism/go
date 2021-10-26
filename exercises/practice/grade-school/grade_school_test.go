ccc91ba96892b08193ccf8fcc15e5b10807b6018
PakkuDon <pakku.pd@gmail.com>
2021-10-27 00:41:46 +1100

    grade-school: add stub functions

--- exercises/practice/grade-school/grade_school_test.go
+++ exercises/practice/grade-school/grade_school_test.go
@@ -1,15 +1,3 @@
-// API:
-//
-// type Grade struct {
-// 	int, []string
-// }
-//
-// type School
-// func New() *School
-// func (s *School) Add(string, int)
-// func (s *School) Grade(int) []string
-// func (s *School) Enrollment() []Grade
-
 package school
 
 import (
