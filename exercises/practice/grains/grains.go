fa7fc82557ccbbe38b87c1d6af29de1aec9d5f24
denistsoi <denistsoi@gmail.com>
2021-10-26 14:44:17 +0800

    Update grains.go
    Adds function stubs according to grains_test.go

--- exercises/practice/grains/grains.go
+++ exercises/practice/grains/grains.go
@@ -1,1 +1,9 @@
 package grains
+
+func Square(number int) (uint64 error) {
+	panic("Please implement the Square function")
+}
+
+func Total() uint64 {
+	panic("Please implement the Total function")
+}
