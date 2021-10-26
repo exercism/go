49242be7b9cf5b4b6bac3dce77be44a379611af2
PakkuDon <pakku.pd@gmail.com>
2021-10-27 00:45:13 +1100

    hexadecimal: add missing stub function

--- exercises/practice/hexadecimal/hexadecimal.go
+++ exercises/practice/hexadecimal/hexadecimal.go
@@ -3,3 +3,11 @@ package hexadecimal
 func ParseHex(in string, out int64, errCase string) {
 	panic("Please implement the ParseHex function")
 }
+
+// HandleErrors takes a list of inputs for ParseHex and returns a matching list
+// of error cases.  It must call ParseHex on each input, handle the error result,
+// and put one of three strings, "none", "syntax", or "range" in the result list
+// according to the error.
+func HandleErrors(inputs []string) []string {
+	panic("Please implement the HandleErrors function")
+}
