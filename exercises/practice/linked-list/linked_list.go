3cfe42db46b6336df79e7e258d1a62844495ddab
PakkuDon <5287128+PakkuDon@users.noreply.github.com>
2021-10-29 01:10:18 +1100

    Use one-letter name for receiver variables
    Co-authored-by: André Santos <andrerfcsantos@gmail.com>

--- exercises/practice/linked-list/linked_list.go
+++ exercises/practice/linked-list/linked_list.go
@@ -6,38 +6,38 @@ func NewList(args ...interface{}) *List {
 	panic("Please implement the NewList function")
 }
 
-func (elem *Node) Next() *Node {
+func (n *Node) Next() *Node {
 	panic("Please implement the Next function")
 }
 
-func (elem *Node) Prev() *Node {
+func (n *Node) Prev() *Node {
 	panic("Please implement the Prev function")
 }
 
-func (list *List) PushFront(v interface{}) {
+func (l *List) PushFront(v interface{}) {
 	panic("Please implement the PushFront function")
 }
 
-func (list *List) PushBack(v interface{}) {
+func (l *List) PushBack(v interface{}) {
 	panic("Please implement the PushBack function")
 }
 
-func (list *List) PopFront() (interface{}, error) {
+func (l *List) PopFront() (interface{}, error) {
 	panic("Please implement the PopFront function")
 }
 
-func (list *List) PopBack() (interface{}, error) {
+func (l *List) PopBack() (interface{}, error) {
 	panic("Please implement the PopBack function")
 }
 
-func (list *List) Reverse() {
+func (l *List) Reverse() {
 	panic("Please implement the Reverse function")
 }
 
-func (list *List) First() *Node {
+func (l *List) First() *Node {
 	panic("Please implement the First function")
 }
 
-func (list *List) Last() *Node {
+func (l *List) Last() *Node {
 	panic("Please implement the Last function")
 }
