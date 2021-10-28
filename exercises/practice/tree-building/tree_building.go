a9efc4f664e78ec41fa23f5afa273c64c07e8de4
denistsoi <denistsoi@gmail.com>
2021-10-28 18:37:10 +0800

    Added function and struct stub for tree_building.go

--- exercises/practice/tree-building/tree_building.go
+++ exercises/practice/tree-building/tree_building.go
@@ -1,1 +1,14 @@
 package tree
+
+type Record struct {
+  ID, Parent int
+}
+
+type Node struct {
+  ID int
+  Children []*Node
+}
+
+func Build(records []Record) (*Node, error) {
+  panic("Please implement the Build function")
+}
