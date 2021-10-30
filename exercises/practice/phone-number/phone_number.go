24b026ada8b97440729158ed1ab891c53e2174bf
PakkuDon <pakku.pd@gmail.com>
2021-10-31 00:25:36 +1100

    phone-number: add stub functions

--- exercises/practice/phone-number/phone_number.go
+++ exercises/practice/phone-number/phone_number.go
@@ -1,1 +1,13 @@
 package phonenumber
+
+func Number(phoneNumber string) (string, error) {
+	panic("Please implement the Number function")
+}
+
+func AreaCode(phoneNumber string) (string, error) {
+	panic("Please implement the AreaCode function")
+}
+
+func Format(phoneNumber string) (string, error) {
+	panic("Please implement the Format function")
+}
