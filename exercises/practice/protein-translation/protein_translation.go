cb295b0fcc23f5d5a4547009e34e568eb347a5c2
PakkuDon <pakku.pd@gmail.com>
2021-10-31 02:06:38 +1100

    protein-translation: add stub functions

--- exercises/practice/protein-translation/protein_translation.go
+++ exercises/practice/protein-translation/protein_translation.go
@@ -1,1 +1,9 @@
 package protein
+
+func FromRNA(rna string) ([]string, error) {
+	panic("Please implement the FromRNA function")
+}
+
+func FromCodon(codon string) (string, error) {
+	panic("Please implement the FromCodon function")
+}
