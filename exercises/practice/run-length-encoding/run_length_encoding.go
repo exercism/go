b793e3dcb5d5d629e0aa4f6226ed16edf07a4f81
PakkuDon <pakku.pd@gmail.com>
2021-11-01 00:55:19 +1100

    run-length-encoding: add stub functions

--- exercises/practice/run-length-encoding/run_length_encoding.go
+++ exercises/practice/run-length-encoding/run_length_encoding.go
@@ -1,1 +1,9 @@
 package encode
+
+func RunLengthEncode(input string) string {
+	panic("Please implement the RunLengthEncode function")
+}
+
+func RunLengthDecode(input string) string {
+	panic("Please implement the RunLengthDecode function")
+}
