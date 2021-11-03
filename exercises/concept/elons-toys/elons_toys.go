82b3329954dc3666beac1a71636b8442f8dbce9f
Eren <f.erenkara@outlook.com>
2021-11-03 22:48:26 +0300

    comments have sorted according to exercise order

--- exercises/concept/elons-toys/elons_toys.go
+++ exercises/concept/elons-toys/elons_toys.go
@@ -2,8 +2,8 @@ package elon
 
 // TODO: define the 'Drive()' method
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
-
 // TODO: define the 'DisplayDistance() string' method
 
 // TODO: define the 'DisplayBattery() string' method
+
+// TODO: define the 'CanFinish(trackDistance int) bool' method
