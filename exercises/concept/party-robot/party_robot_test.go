e21454d3b14edda498bc57f8967eac66246a03f4
Alex <13932330+TheArchDev@users.noreply.github.com>
2021-11-11 18:01:03 +0000

    party-robot: update tests (#1952)

--- exercises/concept/party-robot/party_robot_test.go
+++ exercises/concept/party-robot/party_robot_test.go
@@ -81,11 +81,20 @@ func TestAssignTable(t *testing.T) {
 		{
 			description: "Greet Xuân Jing and give them directions to their seat",
 			name:        "Xuân Jing",
-			direction:   "on the right",
+			direction:   "by the façade",
 			tableNumber: 4,
 			distance:    23.470103,
-			seatmate:    "Eike",
-			want:        "Welcome to my party, Xuân Jing!\nYou have been assigned to table 004. Your table is on the right, exactly 23.5 meters from here.\nYou will be sitting next to Eike.",
+			seatmate:    "Renée",
+			want:        "Welcome to my party, Xuân Jing!\nYou have been assigned to table 004. Your table is by the façade, exactly 23.5 meters from here.\nYou will be sitting next to Renée.",
+		},
+		{
+			description: "Greet Paula and give them directions to their seat",
+			name:        "Paula",
+			direction:   "on the right",
+			tableNumber: 101,
+			distance:    100.0,
+			seatmate:    "Chioma",
+			want:        "Welcome to my party, Paula!\nYou have been assigned to table 101. Your table is on the right, exactly 100.0 meters from here.\nYou will be sitting next to Chioma.",
 		},
 	}
 	for _, tt := range tests {
