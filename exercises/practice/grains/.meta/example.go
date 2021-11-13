b5e526144f8a5a9370173e548bae3eb4da0132c2
Katarzyna Kawala <kat.kawala@gmail.com>
2021-11-13 13:32:04 +0100

    grains: change the argument variable name to number (#1957)

--- exercises/practice/grains/.meta/example.go
+++ exercises/practice/grains/.meta/example.go
@@ -6,13 +6,13 @@ import (
 
 // Square returns the number of grains on a square on a chess board where the
 // first square has 1 and every subsequent square doubles the number.
-func Square(num int) (uint64, error) {
-	if num < 1 || num > 64 {
-		return 0, fmt.Errorf("Input[%d] invalid. Input should be between 1 and 64 (inclusive)", num)
+func Square(number int) (uint64, error) {
+	if number < 1 || number > 64 {
+		return 0, fmt.Errorf("Input[%d] invalid. Input should be between 1 and 64 (inclusive)", number)
 	}
 	// a left shift is equivalent to multiplying by 2.  So we need to left
 	// shift by num-1 times to get the number of grains on that square
-	return 1 << (uint16(num) - 1), nil
+	return 1 << (uint16(number) - 1), nil
 }
 
 // Total returns the total number of grains on the chess board
