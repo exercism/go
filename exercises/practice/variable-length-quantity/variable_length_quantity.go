3127d58b4ef2966c2867ffcf703a5162724e9e3a
PakkuDon <pakku.pd@gmail.com>
2021-11-14 21:03:52 +1100

    variable-length-quantity: add stub functions

--- exercises/practice/variable-length-quantity/variable_length_quantity.go
+++ exercises/practice/variable-length-quantity/variable_length_quantity.go
@@ -1,1 +1,9 @@
 package variablelengthquantity
+
+func EncodeVarint(input []uint32) []byte {
+	panic("Please implement the EncodeVarint function")
+}
+
+func DecodeVarint(input []byte) ([]uint32, error) {
+	panic("Please implement the EncodeVarint function")
+}
