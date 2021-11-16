ae2f5e785587d070eb530c14ba30dd598096ff08
Kah Goh <villastar@yahoo.com.au>
2021-11-17 07:06:12 +0800

    Add concept exercise "vehicle-purchase" (#1934) (#1960)

---
+++ exercises/concept/vehicle-purchase/.meta/design.md
@@ -0,0 +1,25 @@
+# Design
+
+## Learning objectives
+
+- How to compare numbers and strings with relational operators (`<`, `>`, `>=`, `<=`)
+- Equality checks with equals (`==`) and not equals (`!=`)
+- How to write if-statements (`if`, `else-if`, `else`)
+
+## Out of Scope
+
+- `switch`, `for`
+
+## Prerequisites
+
+- `arithmetic-operators` because they are used to practice if-conditions
+- `booleans` because they are the result of the comparison and used in the conditions
+- `numbers` because they are used to practice comparison
+- `strings` also because they are used to practice comparison
+
+## Notes
+
+The exercise is based on [Vehicle Purchase Exercise in the Javascript track][javascript-vehicle-purchase] (which is, in turn, inspired by [Vehicle Purchase Exercise in the Julia track][julia-vehicle-purchase])
+
+[javascript-vehicle-purchase]: https://github.com/exercism/javascript/blob/main/exercises/concept/vehicle-purchase/.docs/instructions.md
+[julia-vehicle-purchase]: https://github.com/exercism/julia/blob/main/exercises/concept/vehicle-purchase/.docs/instructions.md
---
+++ exercises/concept/vehicle-purchase/.meta/exemplar.go
@@ -0,0 +1,33 @@
+package purchase
+
+// NeedsLicence determines whether a licence is need to drive a type of vehicle. Only "car" and "truck" require a licence.
+func NeedsLicence(kind string) bool {
+	return kind == "car" || kind == "truck"
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in dictionary order.
+func ChooseVehicle(option1, option2 string) string {
+	var choice string
+	if option1 < option2 {
+		choice = option1
+	} else {
+		choice = option2
+	}
+	return choice + " is clearly the better choice."
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	var percentage float64
+	// Disables ifElseChain rule of gocritic, which suggests convert the following code to a switch,
+	// however the point of this exercise is to teach if-else-if-else, not switch.
+	//nolint:gocritic
+	if age < 3 {
+		percentage = 0.8
+	} else if age < 10 {
+		percentage = 0.7
+	} else {
+		percentage = 0.5
+	}
+	return percentage * originalPrice
+}
