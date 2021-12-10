ba124d3e8a2b1d550967feff67e12015203e9b95
ppmasa8 <kiyokiyopip56@gmail.com>
2021-12-10 15:06:03 +0900

    Moved additional documentation for range and sum func to append.md.

---
+++ exercises/practice/pythagorean-triplet/.docs/instructions.append.md
@@ -0,0 +1,7 @@
+# Instructions append
+
+Range returns a list of all Pythagorean triplets with sides in the range min to max inclusive.
+
+Sum returns a list of all Pythagorean triplets where the sum a+b+c (the perimeter) is equal to p.
+The three elements of each returned triplet must be in order, t[0] <= t[1] <= t[2], 
+and the list of triplets must be in lexicographic order.
--- exercises/practice/pythagorean-triplet/pythagorean_triplet.go
+++ exercises/practice/pythagorean-triplet/pythagorean_triplet.go
@@ -2,17 +2,10 @@ package pythagorean
 
 type Triplet [3]int
 
-// Range returns a list of all Pythagorean triplets with sides in the
-// range min to max inclusive.
 func Range(min, max int) []Triplet {
 	panic("Please implement the Range function")
 }
 
-// Sum returns a list of all Pythagorean triplets where the sum a+b+c
-// (the perimeter) is equal to p.
-// The three elements of each returned triplet must be in order,
-// t[0] <= t[1] <= t[2], and the list of triplets must be in lexicographic
-// order.
 func Sum(p int) []Triplet {
 	panic("Please implement the Sum function")
 }
