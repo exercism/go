c5ab85f211ed381cf50d4af76642a6769e8dc234
ppmasa8 <kiyokiyopip56@gmail.com>
2021-12-13 00:52:55 +0900

    Pythagorean Triplet: Add instructions append file (#1981)

--- exercises/practice/pythagorean-triplet/pythagorean_triplet.go
+++ exercises/practice/pythagorean-triplet/pythagorean_triplet.go
@@ -2,17 +2,13 @@ package pythagorean
 
 type Triplet [3]int
 
-// Range returns a list of all Pythagorean triplets with sides in the
-// range min to max inclusive.
+// Range generates list of all Pythagorean triplets with side lengths
+// in the provided range.
 func Range(min, max int) []Triplet {
 	panic("Please implement the Range function")
 }
 
-// Sum returns a list of all Pythagorean triplets where the sum a+b+c
-// (the perimeter) is equal to p.
-// The three elements of each returned triplet must be in order,
-// t[0] <= t[1] <= t[2], and the list of triplets must be in lexicographic
-// order.
+// Sum returns a list of all Pythagorean triplets with a certain perimeter.
 func Sum(p int) []Triplet {
 	panic("Please implement the Sum function")
 }
