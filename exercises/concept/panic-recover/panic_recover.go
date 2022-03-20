d0270192d69a227d01dce8ce6027ca07e45008d1
myworkcircle <pallavagarwal1997.pa75@gmail.com>
2022-03-20 12:36:37 +0530

    basic methods first iteration

---
+++ exercises/concept/panic-recover/panic_recover.go
@@ -0,0 +1,16 @@
+package panic_recover
+
+// Add condition to panic
+func AddPanic(names []string, index int) {
+	panic("Add panic with message :- `Index Out Of Bounds`")
+}
+
+// Resolve error causing panic
+func ResoveError() {
+	panic("Please resolve the error first")
+}
+
+// Recover from panic
+func RecoverPanic() {
+	panic("Please add the recovering logic for the panic caused by Index Out of Bounds Error")
+}
---
+++ exercises/concept/panic-recover/panic_recover_test.go
@@ -0,0 +1,43 @@
+package panic_recover
+
+import (
+	"testing"
+)
+
+func AddPanicTest(t *testing.T) {
+
+	tests := []struct {
+		name []string
+	}{
+		{
+			name: []string{"firstName", "secondName", "thirdName"},
+		},
+	}
+
+	for index, tt := range tests {
+		t.Run(tt.name[index], func(*testing.T) {
+			defer func() {
+				if err := recover(); err != "Index Out Of Bounds" {
+					t.Errorf("Panic Recovered, Error: %s", err)
+				}
+			}()
+			AddPanic(tt.name, len(tt.name))
+		})
+	}
+}
+
+func RecoverPanicTest(t *testing.T) {
+	tests := []struct {
+		name []string
+	}{
+		{
+			name: []string{"firstName", "secondName", "thirdName"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("Recover", func(*testing.T) {
+			RecoverPanic()
+		})
+		val := CreatePanic(tt.name, len(tt.name))
+	}
+}
