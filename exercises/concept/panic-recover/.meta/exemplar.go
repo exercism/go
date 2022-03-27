da1f289cdc3f090500259edc604faa41bcc9e783
myworkcircle <pallavagarwal1997.pa75@gmail.com>
2022-03-27 20:13:27 +0530

    update exemplar.go

--- exercises/concept/panic-recover/.meta/exemplar.go
+++ exercises/concept/panic-recover/.meta/exemplar.go
@@ -5,3 +5,15 @@ func CreatePanic(names []string, index int) string {
 	val := names[index]
 	return val
 }
+
+// Add condition to panic
+func AddPanic(names []string, index int) {
+}
+
+// Resolve error causing panic
+func ResoveError() {
+}
+
+// Recover from panic
+func RecoverPanic() {
+}
