4682558b73136fce95f9a40b7982118e18ef0361
Norbs <norvaer@gmail.com>
2022-03-29 09:43:26 +0100

    Implement new concept with exercise: randomness (#2139)

---
+++ exercises/concept/animal-magic/animal_magic.go
@@ -0,0 +1,21 @@
+package chance
+
+// SeedWithTime seeds math/rand with the current computer time
+func SeedWithTime() {
+	panic("Please implement the SeedWithTime function")
+}
+
+// RollADie returns a random int d with 1 <= d <= 20
+func RollADie() int {
+	panic("Please implement the RollADie function")
+}
+
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0
+func GenerateWandEnergy() float64 {
+	panic("Please implement the GenerateWandEnergy function")
+}
+
+// ShuffleAnimals returns a slice with all eight animal strings in random order
+func ShuffleAnimals() []string {
+	panic("Please implement the ShuffleAnimals function")
+}
