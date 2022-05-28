8822a8f6a91cdd09fb75413786c80be928c4c778
Mikaeel Mayeli <mikaeelmayeli@gmail.com>
2022-05-29 02:16:22 +0430

    Update exercises/practice/nth-prime/nth_prime.go
    Co-authored-by: André Santos <andrerfcsantos@gmail.com>

--- exercises/practice/nth-prime/nth_prime.go
+++ exercises/practice/nth-prime/nth_prime.go
@@ -1,5 +1,6 @@
 package prime
 
+// Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
 func Nth(n int) (int, error) {
-	panic("Implement the func for it to return Nth prime number")
+	panic("Please implement the Nth function")
 }
