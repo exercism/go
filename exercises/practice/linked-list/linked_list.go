d631baf6724946b6682fbcbad06bdeda0a09bf14
Marcel Behrmann <mail@marcelbehrmann.de>
2022-06-03 10:21:01 +0200

    Improve Linked List (#2229)

--- exercises/practice/linked-list/linked_list.go
+++ exercises/practice/linked-list/linked_list.go
@@ -15,20 +15,20 @@ func (n *Node) Prev() *Node {
 	panic("Please implement the Prev function")
 }
 
-func (l *List) PushFront(v interface{}) {
-	panic("Please implement the PushFront function")
+func (l *List) Unshift(v interface{}) {
+	panic("Please implement the Unshift function")
 }
 
-func (l *List) PushBack(v interface{}) {
-	panic("Please implement the PushBack function")
+func (l *List) Push(v interface{}) {
+	panic("Please implement the Push function")
 }
 
-func (l *List) PopFront() (interface{}, error) {
-	panic("Please implement the PopFront function")
+func (l *List) Shift() (interface{}, error) {
+	panic("Please implement the Shift function")
 }
 
-func (l *List) PopBack() (interface{}, error) {
-	panic("Please implement the PopBack function")
+func (l *List) Pop() (interface{}, error) {
+	panic("Please implement the Pop function")
 }
 
 func (l *List) Reverse() {
