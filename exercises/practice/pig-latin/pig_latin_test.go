b048fb5b594390bc70580656eeda63710cc190b5
Satho-M <74777619+Satho-M@users.noreply.github.com>
2022-06-05 14:25:24 +0100

    pig-latin: add subtests (#2231)

--- exercises/practice/pig-latin/pig_latin_test.go
+++ exercises/practice/pig-latin/pig_latin_test.go
@@ -1,15 +1,15 @@
 package piglatin
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestPigLatin(t *testing.T) {
 	for _, test := range testCases {
-		if pl := Sentence(test.input); pl != test.expected {
-			t.Fatalf("FAIL: Sentence(%q) = %q, want %q.", test.input, pl, test.expected)
-		}
-		fmt.Printf("PASS: %s\n", test.description)
+		t.Run(test.description, func(t *testing.T) {
+			if pl := Sentence(test.input); pl != test.expected {
+				t.Fatalf("Sentence(%q) = %q, want %q.", test.input, pl, test.expected)
+			}
+		})
 	}
 }
