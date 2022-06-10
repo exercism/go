6f5eef2934b2485f90d9c81c249e2a4c8ff666a6
Anton Ptashnik <iavtomator@gmail.com>
2022-06-10 19:24:32 +0300

    isbn_verifier task: run each test dataset in a separate TC

--- exercises/practice/isbn-verifier/isbn_verifier_test.go
+++ exercises/practice/isbn-verifier/isbn_verifier_test.go
@@ -6,13 +6,13 @@ import (
 
 func TestIsValidISBN(t *testing.T) {
 	for _, test := range testCases {
-		observed := IsValidISBN(test.isbn)
-		if observed == test.expected {
-			t.Logf("PASS: %s", test.description)
-		} else {
-			t.Errorf("FAIL: %s\nIsValidISBN(%q)\nExpected: %t, Actual: %t",
-				test.description, test.isbn, test.expected, observed)
-		}
+		t.Run(test.description, func(t *testing.T) {
+			observed := IsValidISBN(test.isbn)
+			if observed != test.expected {
+				t.Errorf("IsValidISBN(%q)\nExpected: %t, Actual: %t",
+					test.isbn, test.expected, observed)
+			}
+		})
 	}
 }
 
