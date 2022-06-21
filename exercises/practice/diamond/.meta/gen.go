8d2361b5442d7c3fc823aa59307366dbe8a5a8d9
Eric Klatzer <eric@klatzer.at>
2022-06-22 01:52:52 +0200

    Added the test-cases from canonical-data.json to exercise diamond (#2255)

---
+++ exercises/practice/diamond/.meta/gen.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"text/template"
+
+	"../../../../gen"
+)
+
+func main() {
+	t, err := template.New("").Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var j js
+	if err := gen.Gen("diamond", &j, t); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// The JSON structure we expect to be able to unmarshal into
+type js struct {
+	Cases []struct {
+		Description string `json:"description"`
+		Input       struct {
+			Letter string `json:"letter"`
+		} `json:"input"`
+		Expected []string `json:"expected"`
+	} `json:"cases"`
+}
+
+// template applied to above data structure generates the Go test cases
+var tmpl = `package diamond
+
+{{.Header}}
+
+var testCases = []struct {
+	description string
+	input       string
+	expected    []string
+	expectedError error
+}{
+{{range .J.Cases}}{
+		description: {{printf "%q"   .Description}},
+	input: {{printf "%q"   .Input.Letter}},
+	expectedError: nil,
+	expected: []string { {{range $line := .Expected}}{{printf "\n%q," $line}}{{end}}
+},
+},
+{{end}}
+}`
