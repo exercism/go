685364dc4b877ec3ca85b2650239973af48f9b77
Eric Klatzer <eric@klatzer.at>
2022-07-08 09:35:01 +0200

    added comment

--- gen/gen.go
+++ gen/gen.go
@@ -19,6 +19,7 @@ const (
 	canonicalDataURL = "https://raw.githubusercontent.com/exercism/problem-specifications/master/exercises/%s/canonical-data.json"
 	// commitsURL is the GitHub api endpoint for the commit history of canonical-data.json and requires the exercise name.
 	commitsURL    = "https://api.github.com/repos/exercism/problem-specifications/commits?path=exercises/%s/canonical-data.json"
+	// defaultOrigin is the origin used in the header of cases_test.go
 	defaultOrigin = "exercism/problem-specifications"
 )
 
