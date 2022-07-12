c198750505c32f55997b1a29831093399ee2c1b0
Eric Klatzer <eric.klatzer.1@gmx.at>
2022-07-12 09:52:13 +0200

    Update gen/gen.go
    Co-authored-by: June <12543047+junedev@users.noreply.github.com>

--- gen/gen.go
+++ gen/gen.go
@@ -34,7 +34,7 @@ const (
 //
 var problemSpecificationsDir string
 
-// exerciseDir is the location of the exercise and also the cases_test.go
+// exerciseDir is the location of the exercise and also the cases_test.go file.
 // This assumes that the generator script lives in the .meta directory within
 // the exercise directory. Falls back to the present working directory.
 var exerciseDir string
