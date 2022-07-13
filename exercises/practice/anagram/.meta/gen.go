192c004a73f60f85634ab006ccde5937cf239d32
Eric Klatzer <eric@klatzer.at>
2022-07-13 23:52:08 +0200

    anagram: updated exercise generator to new test generator

--- exercises/practice/anagram/.meta/gen.go
+++ exercises/practice/anagram/.meta/gen.go
@@ -12,28 +12,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var j js
-	if err := gen.Gen("anagram", &j, t); err != nil {
+	var j = map[string]interface{}{
+		"findAnagrams": &[]testCase{},
+	}
+	if err := gen.Gen("anagram", j, t); err != nil {
 		log.Fatal(err)
 	}
 }
 
-// The JSON structure we expect to be able to unmarshal into
-type js struct {
-	Exercise string
-	Version  string
-	Comments []string
-	Cases    []OneCase
-}
-
-// The JSON structure we expect to be able to unmarshal into
-type OneCase struct {
-	Description string
+type testCase struct {
+	Description string `json:"description"`
 	Input       struct {
-		Subject    string
-		Candidates []string
-	}
-	Expected []string
+		Subject    string   `json:"subject"`
+		Candidates []string `json:"candidates"`
+	} `json:"input"`
+	Expected []string `json:"expected"`
 }
 
 // template applied to above data structure generates the Go test cases
@@ -46,12 +39,12 @@ var testCases = []struct {
 	subject     string
 	candidates  []string
 	expected    []string
-}{ {{range .J.Cases}}
+}{ {{range .J.findAnagrams}}
 {
 	description: {{printf "%q"   .Description}},
-	subject: {{printf "%q"   .Input.Subject}},
-	candidates: []string { {{range $line := .Input.Candidates}}{{printf "\n%q," $line}}{{end}}},
-	expected: []string { {{range $line := .Expected}}{{printf "\n%q," $line}}{{end}}},
+	subject: {{printf "%q"       .Input.Subject}},
+	candidates: {{printf "%#v"   .Input.Candidates}},
+	expected: {{printf "%#v"     .Expected}},
 	},{{end}}
 }
 `
