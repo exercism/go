2311ab4af00994c5e9e90b80a27ab00f0c61dfd0
Eric Klatzer <eric@klatzer.at>
2022-07-14 08:59:09 +0200

    binary-search: updated exercise generator to new test generator

--- exercises/practice/binary-search/binary_search_test.go
+++ exercises/practice/binary-search/binary_search_test.go
@@ -8,16 +8,15 @@ import (
 
 func TestSearchInts(t *testing.T) {
 	for _, test := range testCases {
-		if x := SearchInts(test.slice, test.key); x != test.x {
-			t.Fatalf("FAIL: %s\nSearchInts(%#v, %d) = %d, want %d",
-				test.description, test.slice, test.key, x, test.x)
-		}
-		t.Logf("SUCCESS: %s", test.description)
+		t.Run(test.description, func(t *testing.T) {
+			if actual := SearchInts(test.inputList, test.inputKey); actual != test.expectedKey {
+				t.Errorf("SearchInts(%#v, %d) = %d, want %d", test.inputList, test.inputKey, actual, test.expectedKey)
+			}
+		})
 	}
 }
 
 // Benchmarks also test searching larger random slices
-
 type query struct {
 	slice []int
 	x     int
@@ -30,7 +29,7 @@ func newQuery(n int) (query, error) {
 	}
 	q.x = rand.Intn(n)
 	if res := SearchInts(q.slice, q.x); res != q.x {
-		return q, fmt.Errorf("Search of %d values gave different answer", n)
+		return q, fmt.Errorf("search of %d values gave different answer", n)
 	}
 	return q, nil
 }
@@ -41,7 +40,7 @@ func runBenchmark(n int, b *testing.B) {
 	}
 	q, err := newQuery(n)
 	if err != nil {
-		b.Fatal(err)
+		b.Error(err)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
