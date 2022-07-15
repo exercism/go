d136610aa30a74c76f401e22ef1258e1318b5fad
Katrina Owen <katrina.owen@gmail.com>
2022-07-15 18:37:18 +0200

    Tweak doc comments in concept exercises
    We encourage people to use proper punctuation in the doc comments for
    the Weather Forecast exercise.
    
    As such we should probably use punctuation in doc comments that we
    provide in other concept exercises as well.

--- exercises/concept/chessboard/chessboard.go
+++ exercises/concept/chessboard/chessboard.go
@@ -5,23 +5,23 @@ package chessboard
 // Declare a type named Chessboard which contains a map of eight Ranks, accessed with keys from "A" to "H"
 
 // CountInRank returns how many squares are occupied in the chessboard,
-// within the given rank
+// within the given rank.
 func CountInRank(cb Chessboard, rank string) int {
 	panic("Please implement CountInRank()")
 }
 
 // CountInFile returns how many squares are occupied in the chessboard,
-// within the given file
+// within the given file.
 func CountInFile(cb Chessboard, file int) int {
 	panic("Please implement CountInFile()")
 }
 
-// CountAll should count how many squares are present in the chessboard
+// CountAll should count how many squares are present in the chessboard.
 func CountAll(cb Chessboard) int {
 	panic("Please implement CountAll()")
 }
 
-// CountOccupied returns how many squares are occupied in the chessboard
+// CountOccupied returns how many squares are occupied in the chessboard.
 func CountOccupied(cb Chessboard) int {
 	panic("Please implement CountOccupied()")
 }
