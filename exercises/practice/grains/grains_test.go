6d2200aeacc85a695a6e8b7999537ff210b058c7
Eric Klatzer <eric@klatzer.at>
2022-07-15 10:47:00 +0200

    grains: updated exercise generator to new test generator

--- exercises/practice/grains/grains_test.go
+++ exercises/practice/grains/grains_test.go
@@ -5,31 +5,28 @@ import (
 )
 
 func TestSquare(t *testing.T) {
-	for _, test := range squareTests {
-		actualVal, actualErr := Square(test.input)
-
-		// check actualVal only if no error expected
-		if !test.expectError && actualVal != test.expectedVal {
-			t.Fatalf("FAIL: %s\nSquare(%d) expected %d, Actual %d", test.description, test.input, test.expectedVal, actualVal)
-		}
-
-		// if we expect an error and there isn't one
-		if test.expectError && actualErr == nil {
-			t.Fatalf("FAIL: %s\nSquare(%d) expected an error, but error is nil", test.description, test.input)
-		}
-		// if we don't expect an error and there is one
-		if !test.expectError && actualErr != nil {
-			var _ error = actualErr
-			t.Fatalf("FAIL: %s\nSquare(%d) expected no error, but error is: %s", test.description, test.input, actualErr)
-		}
-		t.Logf("PASS: %s", test.description)
+	for _, tc := range squareTests {
+		t.Run(tc.description, func(t *testing.T) {
+			actualVal, actualErr := Square(tc.input)
+			if tc.expectError {
+				if actualErr == nil {
+					t.Errorf("Square(%d) expected an error, got: %d", tc.input, actualVal)
+				}
+			} else {
+				if actualErr != nil {
+					t.Errorf("Square(%d) expected %d, but got error: %v", tc.input, actualVal, actualErr)
+				} else if actualVal != tc.expectedVal {
+					t.Errorf("Square(%d) = %d, want %d", tc.input, actualVal, tc.expectedVal)
+				}
+			}
+		})
 	}
 }
 
 func TestTotal(t *testing.T) {
 	var expected uint64 = 18446744073709551615
 	if actual := Total(); actual != expected {
-		t.Errorf("Total() expected %d, Actual %d", expected, actual)
+		t.Errorf("Total() = %d, want:%d", actual, expected)
 	}
 }
 
@@ -39,11 +36,9 @@ func BenchmarkSquare(b *testing.B) {
 	}
 
 	for i := 0; i < b.N; i++ {
-
 		for _, test := range squareTests {
 			Square(test.input)
 		}
-
 	}
 }
 
