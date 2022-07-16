f637d62b71472d7d8d532ae7769ea0e0c75bce8a
Eric Klatzer <eric.klatzer.1@gmx.at>
2022-07-16 16:08:29 +0200

    anagram: updated exercise generator to new test generator (#2311)

--- exercises/practice/anagram/cases_test.go
+++ exercises/practice/anagram/cases_test.go
@@ -1,8 +1,7 @@
 package anagram
 
 // Source: exercism/problem-specifications
-// Commit: baaf092 anagram: words are not anagrams of themselves
-// Problem Specifications Version: 1.4.0
+// Commit: 6373ab6 anagram: reimplement cases where `candidates` isn't a set (#1943)
 
 var testCases = []struct {
 	description string
@@ -13,116 +12,103 @@ var testCases = []struct {
 	{
 		description: "no matches",
 		subject:     "diaper",
-		candidates: []string{
-			"hello",
-			"world",
-			"zombies",
-			"pants"},
-		expected: []string{},
+		candidates:  []string{"hello", "world", "zombies", "pants"},
+		expected:    []string{},
 	},
 	{
 		description: "detects two anagrams",
 		subject:     "master",
-		candidates: []string{
-			"stream",
-			"pigeon",
-			"maters"},
-		expected: []string{
-			"stream",
-			"maters"},
+		candidates:  []string{"stream", "pigeon", "maters"},
+		expected:    []string{"stream", "maters"},
+	},
+	{
+		description: "detects two anagrams",
+		subject:     "solemn",
+		candidates:  []string{"lemons", "cherry", "melons"},
+		expected:    []string{"lemons", "melons"},
 	},
 	{
 		description: "does not detect anagram subsets",
 		subject:     "good",
-		candidates: []string{
-			"dog",
-			"goody"},
-		expected: []string{},
+		candidates:  []string{"dog", "goody"},
+		expected:    []string{},
 	},
 	{
 		description: "detects anagram",
 		subject:     "listen",
-		candidates: []string{
-			"enlists",
-			"google",
-			"inlets",
-			"banana"},
-		expected: []string{
-			"inlets"},
+		candidates:  []string{"enlists", "google", "inlets", "banana"},
+		expected:    []string{"inlets"},
 	},
 	{
 		description: "detects three anagrams",
 		subject:     "allergy",
-		candidates: []string{
-			"gallery",
-			"ballerina",
-			"regally",
-			"clergy",
-			"largely",
-			"leading"},
-		expected: []string{
-			"gallery",
-			"regally",
-			"largely"},
+		candidates:  []string{"gallery", "ballerina", "regally", "clergy", "largely", "leading"},
+		expected:    []string{"gallery", "regally", "largely"},
+	},
+	{
+		description: "detects multiple anagrams with different case",
+		subject:     "nose",
+		candidates:  []string{"Eons", "ONES"},
+		expected:    []string{"Eons", "ONES"},
 	},
 	{
 		description: "does not detect non-anagrams with identical checksum",
 		subject:     "mass",
-		candidates: []string{
-			"last"},
-		expected: []string{},
+		candidates:  []string{"last"},
+		expected:    []string{},
 	},
 	{
 		description: "detects anagrams case-insensitively",
 		subject:     "Orchestra",
-		candidates: []string{
-			"cashregister",
-			"Carthorse",
-			"radishes"},
-		expected: []string{
-			"Carthorse"},
+		candidates:  []string{"cashregister", "Carthorse", "radishes"},
+		expected:    []string{"Carthorse"},
 	},
 	{
 		description: "detects anagrams using case-insensitive subject",
 		subject:     "Orchestra",
-		candidates: []string{
-			"cashregister",
-			"carthorse",
-			"radishes"},
-		expected: []string{
-			"carthorse"},
+		candidates:  []string{"cashregister", "carthorse", "radishes"},
+		expected:    []string{"carthorse"},
 	},
 	{
 		description: "detects anagrams using case-insensitive possible matches",
 		subject:     "orchestra",
-		candidates: []string{
-			"cashregister",
-			"Carthorse",
-			"radishes"},
-		expected: []string{
-			"Carthorse"},
+		candidates:  []string{"cashregister", "Carthorse", "radishes"},
+		expected:    []string{"Carthorse"},
 	},
 	{
-		description: "does not detect a anagram if the original word is repeated",
+		description: "does not detect an anagram if the original word is repeated",
 		subject:     "go",
-		candidates: []string{
-			"go Go GO"},
-		expected: []string{},
+		candidates:  []string{"go Go GO"},
+		expected:    []string{},
 	},
 	{
 		description: "anagrams must use all letters exactly once",
 		subject:     "tapper",
-		candidates: []string{
-			"patter"},
-		expected: []string{},
+		candidates:  []string{"patter"},
+		expected:    []string{},
 	},
 	{
-		description: "words are not anagrams of themselves (case-insensitive)",
+		description: "words are not anagrams of themselves",
 		subject:     "BANANA",
-		candidates: []string{
-			"BANANA",
-			"Banana",
-			"banana"},
-		expected: []string{},
+		candidates:  []string{"BANANA"},
+		expected:    []string{},
+	},
+	{
+		description: "words are not anagrams of themselves even if letter case is partially different",
+		subject:     "BANANA",
+		candidates:  []string{"Banana"},
+		expected:    []string{},
+	},
+	{
+		description: "words are not anagrams of themselves even if letter case is completely different",
+		subject:     "BANANA",
+		candidates:  []string{"banana"},
+		expected:    []string{},
+	},
+	{
+		description: "words other than themselves can be anagrams",
+		subject:     "LISTEN",
+		candidates:  []string{"LISTEN", "Silent"},
+		expected:    []string{"Silent"},
 	},
 }
