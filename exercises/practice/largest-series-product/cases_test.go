e849f3a7d6b26c58b06e29a4737212ee10cbf5a2
Eric Klatzer <eric@klatzer.at>
2022-07-16 13:07:29 +0200

    largest-series-product: updated exercise generator to new test generator (#2338)

--- exercises/practice/largest-series-product/cases_test.go
+++ exercises/practice/largest-series-product/cases_test.go
@@ -1,29 +1,118 @@
 package lsproduct
 
 // Source: exercism/problem-specifications
-// Commit: 85da7a5 largest-series-product: testdata to have standard error indicators (#1331)
-// Problem Specifications Version: 1.2.0
+// Commit: 7a8722a Reorder keys (#1960)
 
-var tests = []struct {
-	digits  string
-	span    int
-	product int64
-	ok      bool
-	error   string
+var testCases = []struct {
+	description string
+	digits      string
+	span        int
+	expected    int64
+	error       string
 }{
-	{"29", 2, 18, true, ""},
-	{"0123456789", 2, 72, true, ""},
-	{"576802143", 2, 48, true, ""},
-	{"0123456789", 3, 504, true, ""},
-	{"1027839564", 3, 270, true, ""},
-	{"0123456789", 5, 15120, true, ""},
-	{"73167176531330624919225119674426574742355349194934", 6, 23520, true, ""},
-	{"0000", 2, 0, true, ""},
-	{"99099", 3, 0, true, ""},
-	{"123", 4, -1, false, "span must be smaller than string length"},
-	{"", 0, 1, true, ""},
-	{"123", 0, 1, true, ""},
-	{"", 1, -1, false, "span must be smaller than string length"},
-	{"1234a5", 2, -1, false, "digits input must only contain digits"},
-	{"12345", -1, -1, false, "span must not be negative"},
+	{
+		description: "finds the largest product if span equals length",
+		digits:      "29",
+		span:        2,
+		expected:    18,
+		error:       "",
+	},
+	{
+		description: "can find the largest product of 2 with numbers in order",
+		digits:      "0123456789",
+		span:        2,
+		expected:    72,
+		error:       "",
+	},
+	{
+		description: "can find the largest product of 2",
+		digits:      "576802143",
+		span:        2,
+		expected:    48,
+		error:       "",
+	},
+	{
+		description: "can find the largest product of 3 with numbers in order",
+		digits:      "0123456789",
+		span:        3,
+		expected:    504,
+		error:       "",
+	},
+	{
+		description: "can find the largest product of 3",
+		digits:      "1027839564",
+		span:        3,
+		expected:    270,
+		error:       "",
+	},
+	{
+		description: "can find the largest product of 5 with numbers in order",
+		digits:      "0123456789",
+		span:        5,
+		expected:    15120,
+		error:       "",
+	},
+	{
+		description: "can get the largest product of a big number",
+		digits:      "73167176531330624919225119674426574742355349194934",
+		span:        6,
+		expected:    23520,
+		error:       "",
+	},
+	{
+		description: "reports zero if the only digits are zero",
+		digits:      "0000",
+		span:        2,
+		expected:    0,
+		error:       "",
+	},
+	{
+		description: "reports zero if all spans include zero",
+		digits:      "99099",
+		span:        3,
+		expected:    0,
+		error:       "",
+	},
+	{
+		description: "rejects span longer than string length",
+		digits:      "123",
+		span:        4,
+		expected:    0,
+		error:       "span must be smaller than string length",
+	},
+	{
+		description: "reports 1 for empty string and empty product (0 span)",
+		digits:      "",
+		span:        0,
+		expected:    1,
+		error:       "",
+	},
+	{
+		description: "reports 1 for nonempty string and empty product (0 span)",
+		digits:      "123",
+		span:        0,
+		expected:    1,
+		error:       "",
+	},
+	{
+		description: "rejects empty string and nonzero span",
+		digits:      "",
+		span:        1,
+		expected:    0,
+		error:       "span must be smaller than string length",
+	},
+	{
+		description: "rejects invalid character in digits",
+		digits:      "1234a5",
+		span:        2,
+		expected:    0,
+		error:       "digits input must only contain digits",
+	},
+	{
+		description: "rejects negative span",
+		digits:      "12345",
+		span:        -1,
+		expected:    0,
+		error:       "span must not be negative",
+	},
 }
