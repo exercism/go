b8e3a3760db78280117d97d8afad5718d37ec137
Eric Klatzer <eric@klatzer.at>
2022-07-17 18:04:35 +0200

    rectangles: updated exercise generator to new test generator

--- exercises/practice/rectangles/cases_test.go
+++ exercises/practice/rectangles/cases_test.go
@@ -1,8 +1,7 @@
 package rectangles
 
 // Source: exercism/problem-specifications
-// Commit: 61bccfa rectangles: apply "input" policy
-// Problem Specifications Version: 1.1.0
+// Commit: 55b098b rectangles: Add test for missing sides (#2050)
 
 var testCases = []struct {
 	description string
@@ -16,119 +15,67 @@ var testCases = []struct {
 	},
 	{
 		description: "no columns",
-		input: []string{
-			"",
-		},
-		expected: 0,
+		input:       []string{""},
+		expected:    0,
 	},
 	{
 		description: "no rectangles",
-		input: []string{
-			" ",
-		},
-		expected: 0,
+		input:       []string{" "},
+		expected:    0,
 	},
 	{
 		description: "one rectangle",
-		input: []string{
-			"+-+",
-			"| |",
-			"+-+",
-		},
-		expected: 1,
+		input:       []string{"+-+", "| |", "+-+"},
+		expected:    1,
 	},
 	{
 		description: "two rectangles without shared parts",
-		input: []string{
-			"  +-+",
-			"  | |",
-			"+-+-+",
-			"| |  ",
-			"+-+  ",
-		},
-		expected: 2,
+		input:       []string{"  +-+", "  | |", "+-+-+", "| |  ", "+-+  "},
+		expected:    2,
 	},
 	{
 		description: "five rectangles with shared parts",
-		input: []string{
-			"  +-+",
-			"  | |",
-			"+-+-+",
-			"| | |",
-			"+-+-+",
-		},
-		expected: 5,
+		input:       []string{"  +-+", "  | |", "+-+-+", "| | |", "+-+-+"},
+		expected:    5,
 	},
 	{
 		description: "rectangle of height 1 is counted",
-		input: []string{
-			"+--+",
-			"+--+",
-		},
-		expected: 1,
+		input:       []string{"+--+", "+--+"},
+		expected:    1,
 	},
 	{
 		description: "rectangle of width 1 is counted",
-		input: []string{
-			"++",
-			"||",
-			"++",
-		},
-		expected: 1,
+		input:       []string{"++", "||", "++"},
+		expected:    1,
 	},
 	{
 		description: "1x1 square is counted",
-		input: []string{
-			"++",
-			"++",
-		},
-		expected: 1,
+		input:       []string{"++", "++"},
+		expected:    1,
 	},
 	{
 		description: "only complete rectangles are counted",
-		input: []string{
-			"  +-+",
-			"    |",
-			"+-+-+",
-			"| | -",
-			"+-+-+",
-		},
-		expected: 1,
+		input:       []string{"  +-+", "    |", "+-+-+", "| | -", "+-+-+"},
+		expected:    1,
 	},
 	{
 		description: "rectangles can be of different sizes",
-		input: []string{
-			"+------+----+",
-			"|      |    |",
-			"+---+--+    |",
-			"|   |       |",
-			"+---+-------+",
-		},
-		expected: 3,
+		input:       []string{"+------+----+", "|      |    |", "+---+--+    |", "|   |       |", "+---+-------+"},
+		expected:    3,
 	},
 	{
 		description: "corner is required for a rectangle to be complete",
-		input: []string{
-			"+------+----+",
-			"|      |    |",
-			"+------+    |",
-			"|   |       |",
-			"+---+-------+",
-		},
-		expected: 2,
+		input:       []string{"+------+----+", "|      |    |", "+------+    |", "|   |       |", "+---+-------+"},
+		expected:    2,
 	},
 	{
 		description: "large input with many rectangles",
-		input: []string{
-			"+---+--+----+",
-			"|   +--+----+",
-			"+---+--+    |",
-			"|   +--+----+",
-			"+---+--+--+-+",
-			"+---+--+--+-+",
-			"+------+  | |",
-			"          +-+",
-		},
-		expected: 60,
+		input:       []string{"+---+--+----+", "|   +--+----+", "+---+--+    |", "|   +--+----+", "+---+--+--+-+", "+---+--+--+-+", "+------+  | |", "          +-+"},
+		expected:    60,
+	},
+	{
+		description: "rectangles must have four sides",
+		input:       []string{"+-+ +-+", "| | | |", "+-+-+-+", "  | |  ", "+-+-+-+", "| | | |", "+-+ +-+"},
+		expected:    5,
 	},
 }
