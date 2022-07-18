2f991ab1672d8b5ed49df44c71d357dda7646846
Eric Klatzer <eric@klatzer.at>
2022-07-18 20:21:02 +0200

    two-bucket: updated exercise generator to new test generator

--- exercises/practice/two-bucket/cases_test.go
+++ exercises/practice/two-bucket/cases_test.go
@@ -1,8 +1,7 @@
 package twobucket
 
 // Source: exercism/problem-specifications
-// Commit: edbc86b two-bucket: apply lowerCamelCase rule to variables
-// Problem Specifications Version: 1.4.0
+// Commit: 4482b50 two-bucket: test inability to reach the goal (#1580)
 
 type bucketTestCase struct {
 	description   string
@@ -13,32 +12,107 @@ type bucketTestCase struct {
 	goalBucket    string
 	moves         int
 	otherBucket   int
-	errorExpected bool // always false for generated test cases.
+	expectedError string
 }
 
 var testCases = []bucketTestCase{
 	{
-		"Measure using bucket one of size 3 and bucket two of size 5 - start with bucket one",
-		3, 5, 1, "one", "one", 4, 5, false,
+		description:   "Measure using bucket one of size 3 and bucket two of size 5 - start with bucket one",
+		bucketOne:     3,
+		bucketTwo:     5,
+		goal:          1,
+		startBucket:   "one",
+		goalBucket:    "one",
+		moves:         '\x04',
+		otherBucket:   '\x05',
+		expectedError: "",
 	},
 	{
-		"Measure using bucket one of size 3 and bucket two of size 5 - start with bucket two",
-		3, 5, 1, "two", "two", 8, 3, false,
+		description:   "Measure using bucket one of size 3 and bucket two of size 5 - start with bucket two",
+		bucketOne:     3,
+		bucketTwo:     5,
+		goal:          1,
+		startBucket:   "two",
+		goalBucket:    "two",
+		moves:         '\b',
+		otherBucket:   '\x03',
+		expectedError: "",
 	},
 	{
-		"Measure using bucket one of size 7 and bucket two of size 11 - start with bucket one",
-		7, 11, 2, "one", "one", 14, 11, false,
+		description:   "Measure using bucket one of size 7 and bucket two of size 11 - start with bucket one",
+		bucketOne:     7,
+		bucketTwo:     11,
+		goal:          2,
+		startBucket:   "one",
+		goalBucket:    "one",
+		moves:         '\x0e',
+		otherBucket:   '\v',
+		expectedError: "",
 	},
 	{
-		"Measure using bucket one of size 7 and bucket two of size 11 - start with bucket two",
-		7, 11, 2, "two", "two", 18, 7, false,
+		description:   "Measure using bucket one of size 7 and bucket two of size 11 - start with bucket two",
+		bucketOne:     7,
+		bucketTwo:     11,
+		goal:          2,
+		startBucket:   "two",
+		goalBucket:    "two",
+		moves:         '\x12',
+		otherBucket:   '\a',
+		expectedError: "",
 	},
 	{
-		"Measure one step using bucket one of size 1 and bucket two of size 3 - start with bucket two",
-		1, 3, 3, "two", "two", 1, 0, false,
+		description:   "Measure one step using bucket one of size 1 and bucket two of size 3 - start with bucket two",
+		bucketOne:     1,
+		bucketTwo:     3,
+		goal:          3,
+		startBucket:   "two",
+		goalBucket:    "two",
+		moves:         '\x01',
+		otherBucket:   '\x00',
+		expectedError: "",
 	},
 	{
-		"Measure using bucket one of size 2 and bucket two of size 3 - start with bucket one and end with bucket two",
-		2, 3, 3, "one", "two", 2, 2, false,
+		description:   "Measure using bucket one of size 2 and bucket two of size 3 - start with bucket one and end with bucket two",
+		bucketOne:     2,
+		bucketTwo:     3,
+		goal:          3,
+		startBucket:   "one",
+		goalBucket:    "two",
+		moves:         '\x02',
+		otherBucket:   '\x02',
+		expectedError: "",
+	},
+	{
+		description:   "Not possible to reach the goal",
+		bucketOne:     6,
+		bucketTwo:     15,
+		goal:          5,
+		startBucket:   "one",
+		goalBucket:    "",
+		moves:         '\x00',
+		otherBucket:   '\x00',
+		expectedError: "impossible",
+	},
+	{
+		description:   "With the same buckets but a different goal, then it is possible",
+		bucketOne:     6,
+		bucketTwo:     15,
+		goal:          9,
+		startBucket:   "one",
+		goalBucket:    "two",
+		moves:         '\n',
+		otherBucket:   '\x00',
+		expectedError: "",
+	},
+	{
+		description:   "Goal larger than both buckets is impossible",
+		bucketOne:     5,
+		bucketTwo:     7,
+		goal:          8,
+		startBucket:   "one",
+		goalBucket:    "",
+		moves:         '\x00',
+		otherBucket:   '\x00',
+		expectedError: "impossible",
 	},
 }
--- exercises/practice/two-bucket/two_bucket_test.go
+++ exercises/practice/two-bucket/two_bucket_test.go
@@ -2,36 +2,21 @@ package twobucket
 
 import "testing"
 
-func runTestCase(t *testing.T, tc bucketTestCase) {
-	g, m, other, e := Solve(tc.bucketOne, tc.bucketTwo, tc.goal, tc.startBucket)
-	var _ error = e
-	if e == nil {
-		if tc.errorExpected {
-			t.Fatalf("FAIL: %s\nSolve(%d, %d, %d, %q)\nExpected error\nActual: %q, %d, %d",
-				tc.description,
-				tc.bucketOne, tc.bucketTwo, tc.goal, tc.startBucket,
-				g, m, other)
-		}
-		if g != tc.goalBucket || m != tc.moves || other != tc.otherBucket {
-			t.Fatalf("FAIL: %s\nSolve(%d, %d, %d, %q)\nExpected: %q, %d, %d\nActual: %q, %d, %d",
-				tc.description,
-				tc.bucketOne, tc.bucketTwo, tc.goal, tc.startBucket,
-				tc.goalBucket, tc.moves, tc.otherBucket,
-				g, m, other)
-		}
-	} else if !tc.errorExpected {
-		t.Fatalf("FAIL: %s\nSolve(%d, %d, %d, %q)\nExpected: %q, %d, %d\nGot Error %q",
-			tc.description,
-			tc.bucketOne, tc.bucketTwo, tc.goal, tc.startBucket,
-			tc.goalBucket, tc.moves, tc.otherBucket,
-			e)
-	}
-	t.Logf("PASS: %s", tc.description)
-}
-
 func TestSolve(t *testing.T) {
 	for _, tc := range append(testCases, errorTestCases...) {
-		runTestCase(t, tc)
+		t.Run(tc.description, func(t *testing.T) {
+			g, m, other, err := Solve(tc.bucketOne, tc.bucketTwo, tc.goal, tc.startBucket)
+			switch {
+			case tc.expectedError != "":
+				if err == nil {
+					t.Fatalf("Solve(%d,%d,%d,%q) expected error, got:%q,%d,%d", tc.bucketOne, tc.bucketTwo, tc.goal, tc.startBucket, g, m, other)
+				}
+			case err != nil:
+				t.Fatalf("Solve(%d,%d,%d,%q) returned error: %v, want:%q,%d,%d", tc.bucketOne, tc.bucketTwo, tc.goal, tc.startBucket, err, tc.goalBucket, tc.moves, tc.otherBucket)
+			case g != tc.goalBucket || m != tc.moves || other != tc.otherBucket:
+				t.Fatalf("Solve(%d,%d,%d,%q) = %q,%d,%d, want:%q,%d,%d", tc.bucketOne, tc.bucketTwo, tc.goal, tc.startBucket, g, m, other, tc.goalBucket, tc.moves, tc.otherBucket)
+			}
+		})
 	}
 }
 
@@ -49,18 +34,18 @@ func BenchmarkSolve(b *testing.B) {
 var errorTestCases = []bucketTestCase{
 	{
 		"Invalid first bucket size",
-		0, 5, 5, "one", "one", 1, 0, true,
+		0, 5, 5, "one", "one", 1, 0, "invalid first bucket size",
 	},
 	{
 		"Invalid second bucket size",
-		3, 0, 3, "one", "one", 1, 0, true,
+		3, 0, 3, "one", "one", 1, 0, "invalid second bucket size",
 	},
 	{
 		"Invalid goal amount",
-		1, 1, 0, "one", "one", 0, 1, true,
+		1, 1, 0, "one", "one", 0, 1, "invalid goal amount",
 	},
 	{
 		"Invalid start bucket name",
-		3, 5, 1, "three", "one", 4, 5, true,
+		3, 5, 1, "three", "one", 4, 5, "invalid start bucket name",
 	},
 }
