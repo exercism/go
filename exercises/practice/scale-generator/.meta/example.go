62d1b81039aef8bda4de0f465927f36a716c89d9
André Santos <andrerfcsantos@gmail.com>
2022-07-18 09:55:50 +0100

    Copy strings.Title in place to bypass linter checks

--- exercises/practice/scale-generator/.meta/example.go
+++ exercises/practice/scale-generator/.meta/example.go
@@ -3,7 +3,54 @@ package scale
 
 import (
 	"strings"
+	"unicode"
 )
+
+// Title is a copy of strings.Title function of the stdlib.
+// The copy is here because strings.Title is deprecated but we still
+// want to use this function as the alternative would require us to support
+// external dependencies which we don't yet (tracking issue https://github.com/exercism/go/issues/2379).
+// Students should still be able to use strings.Title if they want.
+func Title(s string) string {
+	// Use a closure here to remember state.
+	// Hackish but effective. Depends on Map scanning in order and calling
+	// the closure once per rune.
+	prev := ' '
+	return strings.Map(
+		func(r rune) rune {
+			if isSeparator(prev) {
+				prev = r
+				return unicode.ToTitle(r)
+			}
+			prev = r
+			return r
+		},
+		s)
+}
+
+// Copy of strings.isSeparator function of the stdlib.
+func isSeparator(r rune) bool {
+	// ASCII alphanumerics and underscore are not separators
+	if r <= 0x7F {
+		switch {
+		case '0' <= r && r <= '9':
+			return false
+		case 'a' <= r && r <= 'z':
+			return false
+		case 'A' <= r && r <= 'Z':
+			return false
+		case r == '_':
+			return false
+		}
+		return true
+	}
+	// Letters and digits are not separators
+	if unicode.IsLetter(r) || unicode.IsDigit(r) {
+		return false
+	}
+	// Otherwise, all we can do for now is treat spaces as separators.
+	return unicode.IsSpace(r)
+}
 
 var chromaticScale = []string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A",
 	"A#", "B"}
@@ -59,5 +106,5 @@ func formatTonic(tonic string) string {
 	if len(tonic) == 1 {
 		return strings.ToUpper(tonic)
 	}
-	return strings.Title(tonic)
+	return Title(tonic)
 }
