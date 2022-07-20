c25ecc5ce3884e645acb6ebccfd0cccaa3f9b72d
André Santos <andrerfcsantos@gmail.com>
2022-07-20 14:01:41 +0100

    meetup: revert stub to not include the solution

--- exercises/practice/meetup/meetup.go
+++ exercises/practice/meetup/meetup.go
@@ -2,21 +2,8 @@ package meetup
 
 import "time"
 
-type WeekSchedule int
-
-const (
-	First  WeekSchedule = 1
-	Second WeekSchedule = 8
-	Third  WeekSchedule = 15
-	Fourth WeekSchedule = 22
-	Teenth WeekSchedule = 13
-	Last   WeekSchedule = -6
-)
+// Define the WeekSchedule type here.
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	if wSched == Last {
-		month++
-	}
-	t := time.Date(year, month, int(wSched), 12, 0, 0, 0, time.UTC)
-	return t.Day() + int(wDay-t.Weekday()+7)%7
+	panic("Please implement the Day function")
 }
