2e78555f9e11d4c0e4e25e49b3ec6c126bed7852
Jasstkn <mariia.kotliarevskaia@gmail.com>
2022-07-22 08:57:54 +0200

    markdown: adopt code to pass newly generated tests
    Signed-off-by: Jasstkn <mariia.kotliarevskaia@gmail.com>

--- exercises/practice/markdown/markdown.go
+++ exercises/practice/markdown/markdown.go
@@ -16,7 +16,9 @@ func Render(markdown string) string {
 	markdown = strings.Replace(markdown, "_", "</em>", 1)
 	pos := 0
 	list := 0
+	listOpened := false
 	html := ""
+	he := false
 	for {
 		char := markdown[pos]
 		if char == '#' {
@@ -25,22 +27,41 @@ func Render(markdown string) string {
 				pos++
 				char = markdown[pos]
 			}
-			html += fmt.Sprintf("<h%d>", header)
+			if header == 7 {
+				html += fmt.Sprintf("<p>%s ", strings.Repeat("#", header))
+			} else if he {
+				html += "# "
+				header--
+			} else {
+				html += fmt.Sprintf("<h%d>", header)
+			}
 			pos++
 			continue
 		}
-		if char == '*' {
+		he = true
+		if char == '*' && header == 0 && strings.Contains(markdown, "\n") {
 			if list == 0 {
 				html += "<ul>"
 			}
-			html += "<li>"
 			list++
+			if listOpened == false {
+				html += "<li>"
+				listOpened = true
+			} else {
+				html += string(char) + " "
+			}
 			pos += 2
 			continue
 		}
 		if char == '\n' {
-			if list > 0 {
+			if listOpened == true && strings.LastIndex(markdown, "\n") == pos && strings.LastIndex(markdown, "\n") > strings.LastIndex(markdown, "*")  {
+				html += "</li></ul><p>"
+				listOpened = false
+				list = 0
+			}
+			if list > 0 && listOpened {
 				html += "</li>"
+				listOpened = false
 			}
 			if header > 0 {
 				html += fmt.Sprintf("</h%d>", header)
@@ -55,11 +76,16 @@ func Render(markdown string) string {
 			break
 		}
 	}
-	if header > 0 {
+	if header == 7 {
+		return html + fmt.Sprint("</p>")
+	} else if header > 0 {
 		return html + fmt.Sprintf("</h%d>", header)
 	}
 	if list > 0 {
 		return html + "</li></ul>"
+	}
+	if strings.Contains(html, "<p>") {
+		return html + "</p>"
 	}
 	return "<p>" + html + "</p>"
 
--- exercises/practice/markdown/markdown_test.go
+++ exercises/practice/markdown/markdown_test.go
@@ -3,11 +3,12 @@ package markdown
 import "testing"
 
 func TestMarkdown(t *testing.T) {
-	for _, test := range testCases {
-		if html := Render(test.input); html != test.expected {
-			t.Fatalf("FAIL: Render(%q) = %q, want %q.", test.input, html, test.expected)
-		}
-		t.Logf("PASS: %s\n", test.description)
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			if actual := Render(tc.input); actual != tc.expected {
+				t.Fatalf("Render(%q)\n got:%q\nwant:%q", tc.input, actual, tc.expected)
+			}
+		})
 	}
 }
 
@@ -15,7 +16,7 @@ func BenchmarkMarkdown(b *testing.B) {
 	if testing.Short() {
 		b.Skip("skipping benchmark in short mode.")
 	}
-	// Benchmark time to parse all the test cases
+
 	for i := 0; i < b.N; i++ {
 		for _, test := range testCases {
 			Render(test.input)
