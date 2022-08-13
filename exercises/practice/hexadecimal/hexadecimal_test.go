38e8879f0154f2b0651f3187136e76dce0d12404
Eric Klatzer <eric@klatzer.at>
2022-08-13 09:02:22 +0200

    added usage of subtests

--- exercises/practice/hexadecimal/hexadecimal_test.go
+++ exercises/practice/hexadecimal/hexadecimal_test.go
@@ -16,54 +16,42 @@ import (
 )
 
 var testCases = []struct {
-	in      string
-	out     int64
-	errCase string
+	in       string
+	expected int64
+	errCase  string
 }{
-	{"1", 1, "none"},
-	{"10", 0x10, "none"},
-	{"2d", 0x2d, "none"},
-	{"012", 0x12, "none"},
-	{"cfcfcf", 0xcfcfcf, "none"},
-	{"CFCFCF", 0xcfcfcf, "none"},
-	{"", 0, "syntax"},
-	{"peanut", 0, "syntax"},
-	{"2cg134", 0, "syntax"},
-	{"8000000000000000", 0, "range"},
-	{"9223372036854775809", 0, "range"},
+	{in: "1", expected: 1, errCase: "none"},
+	{in: "10", expected: 0x10, errCase: "none"},
+	{in: "2d", expected: 0x2d, errCase: "none"},
+	{in: "012", expected: 0x12, errCase: "none"},
+	{in: "cfcfcf", expected: 0xcfcfcf, errCase: "none"},
+	{in: "CFCFCF", expected: 0xcfcfcf, errCase: "none"},
+	{in: "", expected: 0, errCase: "syntax"},
+	{in: "peanut", expected: 0, errCase: "syntax"},
+	{in: "2cg134", expected: 0, errCase: "syntax"},
+	{in: "8000000000000000", expected: 0, errCase: "range"},
+	{in: "9223372036854775809", expected: 0, errCase: "range"},
 }
 
 func TestParseHex(t *testing.T) {
 	for _, test := range testCases {
-		out, err := ParseHex(test.in)
-		if test.errCase != "none" {
-			// check if err is of error type
-			var _ error = err
+		t.Run(test.in, func(t *testing.T) {
+			actual, err := ParseHex(test.in)
+			switch {
+			case test.errCase != "none":
+				if err == nil {
+					t.Fatalf("ParseHex(%q) expected an error, got: %d", test.in, actual)
+				}
 
-			// we expect error
-			if err == nil {
-				t.Errorf("ParseHex(%q): expected an error, but error is nil",
-					test.in)
-				continue
+				if !strings.Contains(strings.ToLower(err.Error()), test.errCase) {
+					t.Fatalf("ParseHex(%q) returned error %q but expected error containing %q", test.in, err, test.errCase)
+				}
+			case err != nil:
+				t.Fatalf("ParseHex(%q) returned error %q, want: %d", test.in, err, test.expected)
+			case actual != test.expected:
+				t.Errorf("ParseHex(%q) = %d, want: %d", test.in, actual, test.expected)
 			}
-
-			if !strings.Contains(strings.ToLower(err.Error()), test.errCase) {
-				t.Errorf(
-					"ParseHex(%q) returned error %q. Expected error containing '%s'.",
-					test.in, err, test.errCase)
-			}
-		} else {
-			if out != test.out {
-				t.Errorf("ParseHex(%q) = %d. Expected %d.",
-					test.in, out, test.out)
-			}
-
-			// we dont expect error
-			if err != nil {
-				t.Errorf("ParseHex(%q) returned error %q.  Error not expected.",
-					test.in, err)
-			}
-		}
+		})
 	}
 }
 
@@ -72,15 +60,13 @@ func TestHandleErrors(t *testing.T) {
 	for i, test := range testCases {
 		tests[i] = test.in
 	}
-	er := HandleErrors(tests)
-	if len(er) != len(tests) {
-		t.Fatalf("For %d tests, HandleErrors returned %d results, want %d",
-			len(tests), len(er), len(tests))
+	got := HandleErrors(tests)
+	if len(got) != len(tests) {
+		t.Fatalf("HandleErrors() returned %d results for %d tests, want: %d", len(got), len(tests), len(tests))
 	}
-	for i, e := range er {
-		if e != testCases[i].errCase {
-			t.Errorf("For ParseHex(%q), HandleErrors reports %q, want %q",
-				tests[i], e, testCases[i].errCase)
+	for i, err := range got {
+		if err != testCases[i].errCase {
+			t.Errorf("For ParseHex(%q), HandleErrors reports %q, want %q", tests[i], err, testCases[i].errCase)
 		}
 	}
 }
