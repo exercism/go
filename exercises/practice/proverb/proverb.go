f44a7abc0b520fcd92039397b7c40c62211bc027
Eric Klatzer <eric@klatzer.at>
2022-08-24 21:48:12 +0200

    adjusted stub

--- exercises/practice/proverb/proverb.go
+++ exercises/practice/proverb/proverb.go
@@ -11,5 +11,5 @@ func Proverb(rhyme []string) []string {
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-	return []string{}
+	panic("Please implement the Proverb function")
 }
