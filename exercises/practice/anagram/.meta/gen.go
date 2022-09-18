dd8b2162a9cfb423e1f597f5904f25a4ce84bf29
Yihao <48015192+CoderYihaoWang@users.noreply.github.com>
2022-09-18 18:15:21 +1200

    Add test case for non-ascii anagrams (#2445)
    * Add test case for non-ascii anagrams
    
    * Make test case structure un-exported; remove init func
    
    * reorder expressions to be more idiomatic
    
    Co-authored-by: junedev <12543047+junedev@users.noreply.github.com>

--- exercises/practice/anagram/.meta/gen.go
+++ exercises/practice/anagram/.meta/gen.go
@@ -34,17 +34,20 @@ var tmpl = `package anagram
 
 {{.Header}}
 
-var testCases = []struct {
+type anagramTest struct {
 	description string
 	subject     string
 	candidates  []string
 	expected    []string
-}{ {{range .J.findAnagrams}}
-{
-	description: {{printf "%q"   .Description}},
-	subject: {{printf "%q"       .Input.Subject}},
-	candidates: {{printf "%#v"   .Input.Candidates}},
-	expected: {{printf "%#v"     .Expected}},
-	},{{end}}
+}
+
+var testCases = []anagramTest{ 
+	{{range .J.findAnagrams}}{
+		description: {{printf "%q"   .Description}},
+		subject: {{printf "%q"       .Input.Subject}},
+		candidates: {{printf "%#v"   .Input.Candidates}},
+		expected: {{printf "%#v"     .Expected}},
+	},
+	{{end}}
 }
 `
--- exercises/practice/anagram/anagram_test.go
+++ exercises/practice/anagram/anagram_test.go
@@ -6,6 +6,27 @@ import (
 	"testing"
 )
 
+var nonAsciiTestCases = []anagramTest{
+	{
+		description: "detects non-ascii anagrams",
+		subject:     "你好，世界",
+		candidates:  []string{"世界，你好", "hello, 世界", "世界, 你好"},
+		expected:    []string{"世界，你好"},
+	},
+}
+
+func TestDetectAnagrams(t *testing.T) {
+	var allCases = append(testCases, nonAsciiTestCases...)
+	for _, tc := range allCases {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := Detect(tc.subject, tc.candidates)
+			if !equal(tc.expected, actual) {
+				t.Errorf("Detect(%q, %#v) = %#v, want: %#v", tc.subject, tc.candidates, actual, tc.expected)
+			}
+		})
+	}
+}
+
 func equal(a, b []string) bool {
 	if len(b) != len(a) {
 		return false
@@ -16,23 +37,13 @@ func equal(a, b []string) bool {
 	return fmt.Sprintf("%v", a) == fmt.Sprintf("%v", b)
 }
 
-func TestDetectAnagrams(t *testing.T) {
-	for _, tc := range testCases {
-		t.Run(tc.description, func(t *testing.T) {
-			actual := Detect(tc.subject, tc.candidates)
-			if !equal(tc.expected, actual) {
-				t.Errorf("Detect(%q, %#v) = %#v, want: %#v", tc.subject, tc.candidates, actual, tc.expected)
-			}
-		})
-	}
-}
-
 func BenchmarkDetectAnagrams(b *testing.B) {
 	if testing.Short() {
 		b.Skip("skipping benchmark in short mode.")
 	}
+	var allCases = append(testCases, nonAsciiTestCases...)
 	for i := 0; i < b.N; i++ {
-		for _, tt := range testCases {
+		for _, tt := range allCases {
 			Detect(tt.subject, tt.candidates)
 		}
 	}
