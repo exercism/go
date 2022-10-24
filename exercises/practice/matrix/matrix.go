eee4c5809890d1d123f31662f6817ba5dad9f6e1
garrettwgg <garrettwgg@gmail.com>
2022-10-24 19:13:35 -0400

    Update function signatures provided in matrix exercise (#2523)

--- exercises/practice/matrix/matrix.go
+++ exercises/practice/matrix/matrix.go
@@ -2,19 +2,19 @@ package matrix
 
 // Define the Matrix type here.
 
-func New(s string) (*Matrix, error) {
+func New(s string) (Matrix, error) {
 	panic("Please implement the New function")
 }
 
 // Cols and Rows must return the results without affecting the matrix.
-func (m *Matrix) Cols() [][]int {
+func (m Matrix) Cols() [][]int {
 	panic("Please implement the Cols function")
 }
 
-func (m *Matrix) Rows() [][]int {
+func (m Matrix) Rows() [][]int {
 	panic("Please implement the Rows function")
 }
 
-func (m *Matrix) Set(row, col, val int) bool {
+func (m Matrix) Set(row, col, val int) bool {
 	panic("Please implement the Set function")
 }
