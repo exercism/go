f7df305a69522aa534bdb8e9851475702816630c
easyyoung <74750121+easyyoungg@users.noreply.github.com>
2022-11-01 16:15:55 +0800

    Update tree_building.go

--- exercises/practice/tree-building/tree_building.go
+++ exercises/practice/tree-building/tree_building.go
@@ -1,5 +1,7 @@
 package tree
 
+import "errors"
+
 type Record struct {
 	ID     int
 	Parent int
@@ -13,5 +15,31 @@ type Node struct {
 }
 
 func Build(records []Record) (*Node, error) {
-	panic("Please implement the Build function")
+	if len(records)==0{
+        return nil,nil
+    }
+
+    positions := make([]int, len(records))
+    nodes := make([]Node,len(records))
+    for i, r := range records {
+        if r.ID < 0 || r.ID >= len(records) {
+			return nil, errors.New("records ID is wrong1.")
+		}
+		positions[r.ID] = i
+	}
+    for i := range positions{
+        d := records[positions[i]]
+        if d.ID != i || d.ID<d.Parent || (d.ID==d.Parent && d.ID!=0){
+            return nil,errors.New("records ID is wrong2.")
+        }
+
+        nodes[i].ID=d.ID
+        if i!=0{
+            p := &nodes[d.Parent]
+            p.Children = append(p.Children,&nodes[i])
+        }
+    
+    }
+
+    return &nodes[0],nil
 }
