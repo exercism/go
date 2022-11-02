e01baaf041a41e5de1062735ef3f8eb61e712669
easyyoung <74750121+easyyoungg@users.noreply.github.com>
2022-11-02 09:58:16 +0800

    Update welcome_to_tech_palace.go

--- exercises/concept/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ exercises/concept/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,50 @@
 package techpalace
 
+import "strings"
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return "Welcome to the Tech Palace, "+strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	var str string
+    for i:=0;i<numStarsPerLine;i++{
+        str+="*"
+        if i==numStarsPerLine-1{
+            str+="\n"
+        }
+    }
+
+    str+=welcomeMsg
+    str+="\n"
+    for i:=0;i<numStarsPerLine;i++{
+        str+="*"
+    }
+    return str
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+    var str string
+    var count int
+    var end int 
+    var start int
+	for i,word := range oldMsg{
+        if (string(word)>="A" && string(word)<="Z") || (string(word)>="a" && string(word)<="z") || string(word)=="%"{
+            if count ==0 {
+                start = i
+            }
+            end = i 
+            count++
+        }
+    }
+
+    for i,word := range oldMsg{
+        if i>=start && i<=end{
+            str += string(word)
+        }
+    }
+    return str
 }
