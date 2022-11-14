bca7657c3a4eceed6b973e4fd845446ad5d5e474
Mikaeil Mayeli <mikaeelmayeli@gmail.com>
2022-11-14 11:40:53 -0500

    temp

--- exercises/concept/weather-forecast-client/.meta/exemplar.go
+++ exercises/concept/weather-forecast-client/.meta/exemplar.go
@@ -5,25 +5,41 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
-func ForecastClient(serverAddr, method string) (string, error) {
-	client := &http.Client{}
-	req, err := http.NewRequest(method, serverAddr, http.NoBody)
-	if err != nil {
-		return "", err
+type Client struct {
+	httpClient *http.Client
+	serverAddr string
+}
+
+func NewForecastClient(serverAddr string) (*Client, error) {
+	return &Client{httpClient: &http.Client{Timeout: 5 * time.Second}, serverAddr: serverAddr}, nil
+}
+
+// SELF:Check what is the standard way to structure the Get and Post different functions
+func (c *Client) Query(city, method string, data ...string) (string, error) {
+	if method == http.MethodGet {
+		req, err := http.NewRequest(method, fmt.Sprintf("%s/weather?city=%s", c.serverAddr, city), http.NoBody)
+		if err != nil {
+			return "", err
+		}
+		resp, err := c.httpClient.Do(req)
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			return "", errors.New(fmt.Sprint(resp.StatusCode))
+		}
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		return string(b), nil
+	} else if method == http.MethodPost {
+		// SELF: complete this section
 	}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return "", errors.New(fmt.Sprint(resp.StatusCode))
-	}
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(b), nil
+	// SELF: find a better way to raise this error
+	return "", errors.New(fmt.Sprint(http.StatusMethodNotAllowed))
 }
