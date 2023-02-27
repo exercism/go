25be82126aaeb9647d777a91a2c0b3bca9b10e39
André Santos <andrerfcsantos@gmail.com>
2023-02-27 22:00:07 +0000

    Add resistor-color practice exercise (#2614)

---
+++ exercises/practice/resistor-color/.meta/example.go
@@ -0,0 +1,35 @@
+package resistorcolor
+
+var colorMap = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
+// Colors should return the list of all colors.
+func Colors() []string {
+	return []string{
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white",
+	}
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	return colorMap[color]
+}
