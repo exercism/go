cb0cd4d02cf5a8a688e389d1e25fd863767cc391
André Santos <andrerfcsantos@gmail.com>
2023-02-28 23:55:20 +0000

    Use uint64 in exemplar

--- exercises/practice/resistor-color-trio/.meta/example.go
+++ exercises/practice/resistor-color-trio/.meta/example.go
@@ -18,14 +18,14 @@ var colorMap = map[string]int{
 	"white":  9,
 }
 
-func Exp(base, exp int) int {
+func Exp(base, exp int) uint64 {
 	if exp == 0 {
 		return 1
 	}
 
-	res := 1
+	res := uint64(1)
 	for i := 0; i < exp; i++ {
-		res *= base
+		res *= uint64(base)
 	}
 
 	return res
@@ -35,27 +35,27 @@ func Exp(base, exp int) int {
 // The label is a string with a resistance value with an unit appended
 // (e.g. "33 ohms", "470 kiloohms").
 func Label(colors []string) string {
-	value := 10*colorMap[colors[0]] + colorMap[colors[1]]
+	value := uint64(10*colorMap[colors[0]]) + uint64(colorMap[colors[1]])
 	value *= Exp(10, colorMap[colors[2]])
-	label := strconv.Itoa(value)
+	label := strconv.FormatUint(value, 10)
 
 	var unit string
 	switch {
 	case len(label) < 4:
 		unit = "ohms"
 	case len(label) < 7:
-		label = strconv.Itoa(value / 1000)
+		label = strconv.FormatUint(value/1000, 10)
 		unit = "kiloohms"
 	case len(label) <= 8:
-		label = strconv.Itoa(value / 1000_000)
+		label = strconv.FormatUint(value/1000_000, 10)
 		unit = "megaohms"
 	case len(label) >= 9:
-		label = strconv.Itoa(value / 1000_000_000)
+		label = strconv.FormatUint(value/1000_000_000, 10)
 		unit = "gigaohms"
 	}
 
 	if value < 1000 {
-		label = strconv.Itoa(value)
+		label = strconv.FormatUint(value, 10)
 	}
 
 	return fmt.Sprintf("%s %s", label, unit)
