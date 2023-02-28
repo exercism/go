429449f437430b8d64b6e5017a9fc309bb472bcc
André Santos <andrerfcsantos@gmail.com>
2023-02-28 22:30:03 +0000

    Add resistor-color-duo

---
+++ exercises/practice/resistor-color-duo/.meta/gen.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"text/template"
+
+	"../../../../gen"
+)
+
+type valuePropertyCase struct {
+	Description string `json:"description"`
+	Input       struct {
+		Colors []string `json:"colors"`
+	} `json:"input"`
+	Expected int `json:"expected"`
+}
+
+func (v valuePropertyCase) InputColorsString() string {
+
+	s := "[]string{"
+
+	for _, c := range v.Input.Colors {
+		s += `"` + c + `", `
+	}
+
+	s = s[:len(s)-2] + "}"
+	return s
+}
+
+func main() {
+	t, err := template.New("").Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	j := map[string]interface{}{
+		"value": &[]valuePropertyCase{},
+	}
+	if err := gen.Gen("resistor-color-duo", j, t); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Template to generate test cases.
+var tmpl = `package resistorcolorduo
+
+{{.Header}}
+
+type valueTestCase struct {
+	description	string
+	input		[]string
+	expected	int
+}
+
+var valueTestCases = []valueTestCase{
+	{{range .J.value}}
+{
+	description:	{{printf "%q"  .Description}},
+	input:			{{printf "%v" .InputColorsString}},
+	expected:		{{printf "%d"  .Expected}},
+},{{end}}
+}
+
+
+
+`
---
+++ exercises/practice/resistor-color-duo/resistor_color.go
@@ -0,0 +1,6 @@
+package resistorcolorduo
+
+// Value should return the resistance value of a resistor with a given colors.
+func Value(colors []string) int {
+	panic("Implement the Value function")
+}
