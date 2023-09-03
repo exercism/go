ecc5877ea91692bfb64240df0ae8e492b8c7c1b9
junedev <12543047+junedev@users.noreply.github.com>
2023-09-03 23:32:12 +0200

    change strain to use generics

--- exercises/practice/strain/strain.go
+++ exercises/practice/strain/strain.go
@@ -1,21 +1,7 @@
 package strain
 
-type Ints []int
-type Lists [][]int
-type Strings []string
+// Implement the "Keep" and "Discard" function in this file.
 
-func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
-}
-
-func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
-}
-
-func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
-}
-
-func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
-}
+// You will need typed parameters (aka "Generics") to solve this exercise.
+// They are not part of the Exercism syllabus yet but you can learn about
+// them here: https://go.dev/tour/generics/1
