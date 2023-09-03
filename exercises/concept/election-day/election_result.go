4ed931480246f75a9a36b4768f7d27f474861c06
Marvin Wendt <git@marvinjwendt.com>
2023-09-03 22:55:43 +0200

    Fix godoc comment variable name (#2698)
    * Fix godoc comment variable name
    * Fix second wrong comment
    
    ---------
    
    Co-authored-by: junedev <12543047+junedev@users.noreply.github.com>

--- exercises/concept/election-day/election_result.go
+++ exercises/concept/election-day/election_result.go
@@ -4,6 +4,6 @@ package electionday
 type ElectionResult struct {
 	// Name is the name of the candidate.
 	Name string
-	// Number is the total number of votes the candidate had.
+	// Votes is the total number of votes the candidate had.
 	Votes int
 }
