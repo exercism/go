657e1156c60a2aac0017a32487c9208fe1f3679e
Jesse Kroon <jessekroon2@gmail.com>
2024-01-25 16:53:03 +0100

    ran go mod init and added basic contents for dnd_character*.go files

--- exercises/practice/dnd-character/dnd_character.go
+++ exercises/practice/dnd-character/dnd_character.go
@@ -1,1 +1,23 @@
 package dndcharacter
+
+type CharacterSheet struct {
+	Strength     string
+	Dexterity    string
+	Constitution string
+	Intelligence string
+	Wisdom       string
+	Charisma     string
+	Hitpoints    string
+}
+
+func Modifier(score int) int {
+	panic("implement me")
+}
+
+func Ability() string {
+	panic("implement me")
+}
+
+func Character() CharacterSheet {
+	panic("implement me")
+}
--- exercises/practice/dnd-character/dnd_character_test.go
+++ exercises/practice/dnd-character/dnd_character_test.go
@@ -1,1 +1,36 @@
 package dndcharacter
+
+import "testing"
+
+func TestModifier(t *testing.T) {
+	for _, tc := range modifierTests {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := Modifier(tc.input.Score)
+			if actual != tc.expected {
+				t.Fatalf("Modifier(%#v) = %#v, want %#v", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestAbility(t *testing.T) {
+	for _, tc := range abilityTests {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := Ability()
+			if actual != tc.expected {
+				t.Fatalf("Ability(%#v) = %#v, want %#v", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestCharacter(t *testing.T) {
+	for _, tc := range characterTests {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := Character()
+			if actual != tc.expected {
+				t.Fatalf("Character(%#v) = %#v, want %#v", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
