1c6970cbfbdb875a0a2c44cd98318a87944730fc
Kasim Kaizer <155087257+KasimKaizer@users.noreply.github.com>
2024-05-01 14:22:31 +0400

    update(eliuds-eegs): add test and benchmark for this exercise

--- exercises/practice/eliuds-eggs/eliuds_eggs_test.go
+++ exercises/practice/eliuds-eggs/eliuds_eggs_test.go
@@ -1,1 +1,25 @@
 package eliudseggs
+
+import "testing"
+
+func TestEggCount(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			got := EggCount(tc.input)
+			if got != tc.expected {
+				t.Fatalf("EggCount(%d) = %d, want: %d", tc.input, got, tc.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkEggCount(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping benchmark in short mode.")
+	}
+	for i := 0; i < b.N; i++ {
+		for _, tc := range testCases {
+			EggCount(tc.input)
+		}
+	}
+}
