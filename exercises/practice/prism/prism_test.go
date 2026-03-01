5675d63751ed5e625a8fcc0d95fe428a850aa686
Isaac Good <IsaacG@users.noreply.github.com>
2026-02-28 21:49:29 -0800

    Use `for range` loop
    Co-authored-by: Kah Goh <villastar@yahoo.com.au>

--- exercises/practice/prism/prism_test.go
+++ exercises/practice/prism/prism_test.go
@@ -19,7 +19,7 @@ func BenchmarkFindSequence(b *testing.B) {
 	if testing.Short() {
 		b.Skip("skipping benchmark in short mode.")
 	}
-	for i := 0; i < b.N; i++ {
+	for range b.N {
 		for _, tc := range testCases {
 			FindSequence(tc.start, tc.prisms)
 		}
