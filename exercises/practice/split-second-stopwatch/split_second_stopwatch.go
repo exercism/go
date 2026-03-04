66855508525f459baf28457e1a33aeb26d78e1ba
Isaac Good <IsaacG@users.noreply.github.com>
2026-03-03 20:31:32 -0800

    Implement practice exercise Split Second Stopwatch (#2933)

---
+++ exercises/practice/split-second-stopwatch/split_second_stopwatch.go
@@ -0,0 +1,43 @@
+package splitsecondstopwatch
+
+type SplitSecondStopwatch struct {}
+
+func (sss *SplitSecondStopwatch) Start() error {
+	panic("Please implement the Start function")
+}
+
+func (sss *SplitSecondStopwatch) Stop() error {
+	panic("Please implement the Stop function")
+}
+
+func (sss *SplitSecondStopwatch) Reset() error {
+	panic("Please implement the Reset function")
+}
+
+func (sss *SplitSecondStopwatch) Lap() error {
+	panic("Please implement the Lap function")
+}
+
+func (sss *SplitSecondStopwatch) AdvanceTime(by string) {
+	panic("Please implement the AdvanceTime function")
+}
+
+func (sss *SplitSecondStopwatch) State() string {
+	panic("Please implement the State function")
+}
+
+func (sss *SplitSecondStopwatch) CurrentLap() string {
+	panic("Please implement the CurrentLap function")
+}
+
+func (sss *SplitSecondStopwatch) Total() string {
+	panic("Please implement the Total function")
+}
+
+func (sss *SplitSecondStopwatch) PreviousLaps() []string {
+	panic("Please implement the PreviousLaps function")
+}
+
+func NewSplitSecondStopwatch() *SplitSecondStopwatch {
+	panic("Please implement the NewSplitSecondStopwatch function")
+}
