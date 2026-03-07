1ff7b454f529e30ce981affb8dd68329ffab96e4
Kasim Suterwala <155087257+KasimKaizer@users.noreply.github.com>
2026-03-07 14:35:57 +0530

    Update exercises/practice/eliuds-eggs/eliuds_eggs_test.go
    use the range function
    
    Co-authored-by: Isaac Good <IsaacG@users.noreply.github.com>

--- exercises/practice/eliuds-eggs/eliuds_eggs_test.go
+++ exercises/practice/eliuds-eggs/eliuds_eggs_test.go
@@ -17,7 +17,7 @@ func BenchmarkEggCount(b *testing.B) {
 	if testing.Short() {
 		b.Skip("skipping benchmark in short mode.")
 	}
-	for i := 0; i < b.N; i++ {
+	for range b.N {
 		for _, tc := range testCases {
 			EggCount(tc.input)
 		}
