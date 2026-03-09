77b90ff0da5654711a42ae3b15640adc4e7bf13e
Isaac Good <IsaacG@users.noreply.github.com>
2026-03-08 20:51:08 -0700

    [camicia] Add new practice exercise (#2973)

---
+++ exercises/practice/camicia/.meta/gen.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"../../../../gen"
+	"log"
+	"text/template"
+)
+
+func main() {
+	t, err := template.New("").Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	j := map[string]any{
+		"simulateGame": &[]testCase{},
+	}
+	if err := gen.Gen("camicia", j, t); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type testCase struct {
+	Description string `json:"description"`
+	Input struct {
+		PlayerA []string `json:"playerA"`
+		PlayerB []string `json:"playerB"`
+	} `json:"input"`
+	Expected struct {
+		Status string `json:"status"`
+		Cards int `json:"cards"`
+		Tricks int `json:"tricks"`
+	}`json:"expected"`
+}
+
+var tmpl = `{{.Header}}
+
+var testCases = []struct {
+	description string
+	playerA     []string
+	playerB     []string
+	expected    Outcome
+}{ {{range .J.simulateGame}}
+	{
+		description: {{printf "%q" .Description}},
+		playerA:     {{printf "%#v" .Input.PlayerA}},
+		playerB:     {{printf "%#v" .Input.PlayerB}},
+		expected:    Outcome {
+			finishes: {{eq .Expected.Status "finished" | printf "%t"}},
+			cards:  {{.Expected.Cards}},
+			tricks: {{.Expected.Tricks}},
+		},
+	},{{end}}
+}`
---
+++ exercises/practice/camicia/camicia.go
@@ -0,0 +1,11 @@
+package camicia
+
+type Outcome struct {
+	finishes bool
+	cards    int
+	tricks   int
+}
+
+func SimulateGame(playerA, playerB []string) Outcome {
+	panic("Please implement the SimulateGame function")
+}
