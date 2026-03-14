76fec8d4735f7a9e0831a9a7181a617cc4610cb4
Isaac Good <IsaacG@users.noreply.github.com>
2026-03-14 11:19:24 -0700

    Apply suggestion from @IsaacG

--- exercises/concept/election-day/election_day_test.go
+++ exercises/concept/election-day/election_day_test.go
@@ -51,7 +51,7 @@ func TestVoteCount(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := VoteCount(tt.counter); got != tt.expected {
-				t.Errorf("VoteCount(%s) = %d, want %d", intPtrRepresentation(tt.counter), got, tt.expected)
+				t.Fatalf("VoteCount(%s) = %d, want %d", intPtrRepresentation(tt.counter), got, tt.expected)
 			}
 		})
 	}
