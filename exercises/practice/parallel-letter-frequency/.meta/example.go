8a5eb9fb8a66f79c7ded77310ffaabec9261d6a7
Isaac Good <github@isaacgood.com>
2026-03-17 19:56:52 -0700

    [parallel-letter-frequency] Add a test generator and sync from canonical data

--- exercises/practice/parallel-letter-frequency/.meta/example.go
+++ exercises/practice/parallel-letter-frequency/.meta/example.go
@@ -1,21 +1,29 @@
-package letter
+package parallelletterfrequency
 
-import "sync"
+import (
+	"slices"
+	"strings"
+	"sync"
+)
 
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
+
+var Ignore = []rune{' ', '\t', '\n', '\r', '!', '?', '.', ',', ':', '-', ';', '"', '\'', '(', ')'}
 
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
 func Frequency(text string) FreqMap {
 	frequencies := FreqMap{}
-	for _, r := range text {
+	for _, r := range strings.ToLower(text) {
+		if r >= '0' && r <= '9' || slices.Contains(Ignore, r) {
+			continue
+		}
 		frequencies[r]++
 	}
 	return frequencies
 }
 
-// ConcurrentFrequency is a (not necessarily ideal) example of how to call Frequency() concurrently.
 func ConcurrentFrequency(texts []string) FreqMap {
 	res := make(FreqMap)
 	ch := make(chan FreqMap, 10)
--- exercises/practice/parallel-letter-frequency/parallel_letter_frequency.go
+++ exercises/practice/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,4 +1,4 @@
-package letter
+package parallelletterfrequency
 
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
@@ -6,11 +6,7 @@ type FreqMap map[rune]int
 // Frequency counts the frequency of each rune in a given text and returns this
 // data as a FreqMap.
 func Frequency(text string) FreqMap {
-	frequencies := FreqMap{}
-	for _, r := range text {
-		frequencies[r]++
-	}
-	return frequencies
+	panic("Implement the Frequency function")
 }
 
 // ConcurrentFrequency counts the frequency of each rune in the given strings,
