5b37a44c515e467091429a2cec819f14ec8983be
Isaac Good <github@isaacgood.com>
2026-03-19 14:21:05 -0700

    [etl] Add a test generator and sync from canonical data

---
+++ exercises/practice/etl/.meta/gen.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"../../../../gen"
+	"log"
+	"strconv"
+	"text/template"
+)
+
+func main() {
+	t, err := template.New("").Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	j := map[string]any{
+		"transform": &[]testCase{},
+	}
+	if err := gen.Gen("etl", j, t); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type testCase struct {
+	Description string `json:"description"`
+	Input       struct {
+		Legacy map[string][]string `json:"Legacy"`
+	} `json:"input"`
+	Expected map[string]int `json:"expected"`
+}
+
+func (tc *testCase) Legacy() map[int][]string {
+	legacy := make(map[int][]string)
+	for key, values := range tc.Input.Legacy {
+		num, err := strconv.Atoi(key)
+		if err != nil {
+			panic("Could not parse the data")
+		}
+		legacy[num] = values
+	}
+	return legacy
+}
+
+var tmpl = `{{.Header}}
+
+var testCases = []struct {
+	description string
+	input       map[int][]string
+	expected    map[string]int
+}{ {{range .J.transform}}
+	{
+		description: {{printf "%q" .Description}},
+		input:       {{printf "%#v" .Legacy}},
+		expected:    {{printf "%#v" .Expected}},
+	},{{end}}
+}`
---
+++ exercises/practice/etl/cases_test.go
@@ -0,0 +1,33 @@
+package etl
+
+// This is an auto-generated file. Do not change it manually. Run the generator to update the file.
+// See https://github.com/exercism/go#synchronizing-tests-and-instructions
+// Source: exercism/problem-specifications
+// Commit: 65e86ee Rework etl documentation (#2250)
+
+var testCases = []struct {
+	description string
+	input       map[int][]string
+	expected    map[string]int
+}{
+	{
+		description: "single letter",
+		input:       map[int][]string{1: []string{"A"}},
+		expected:    map[string]int{"a": 1},
+	},
+	{
+		description: "single score with multiple letters",
+		input:       map[int][]string{1: []string{"A", "E", "I", "O", "U"}},
+		expected:    map[string]int{"a": 1, "e": 1, "i": 1, "o": 1, "u": 1},
+	},
+	{
+		description: "multiple scores with multiple letters",
+		input:       map[int][]string{1: []string{"A", "E"}, 2: []string{"D", "G"}},
+		expected:    map[string]int{"a": 1, "d": 2, "e": 1, "g": 2},
+	},
+	{
+		description: "multiple scores with differing numbers of letters",
+		input:       map[int][]string{1: []string{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}, 2: []string{"D", "G"}, 3: []string{"B", "C", "M", "P"}, 4: []string{"F", "H", "V", "W", "Y"}, 5: []string{"K"}, 8: []string{"J", "X"}, 10: []string{"Q", "Z"}},
+		expected:    map[string]int{"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4, "z": 10},
+	},
+}
