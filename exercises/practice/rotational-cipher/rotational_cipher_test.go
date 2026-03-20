99004365568b03ae0c9c2001ccd1249801a355d9
Isaac Good <IsaacG@users.noreply.github.com>
2026-03-19 17:57:31 -0700

    [rotational-cipher] Add a test generator and sync from canonical data (#3036)
    [no important files changed]

--- exercises/practice/rotational-cipher/rotational_cipher_test.go
+++ exercises/practice/rotational-cipher/rotational_cipher_test.go
@@ -4,74 +4,12 @@ import (
 	"testing"
 )
 
-var testCases = []struct {
-	description   string
-	inputPlain    string
-	inputShiftKey int
-	expected      string
-}{
-	{
-		description:   "rotate a by 0, same output as input",
-		inputPlain:    "a",
-		inputShiftKey: 0,
-		expected:      "a",
-	},
-	{
-		description:   "rotate a by 1",
-		inputPlain:    "a",
-		inputShiftKey: 1,
-		expected:      "b",
-	},
-	{
-		description:   "rotate a by 26, same output as input",
-		inputPlain:    "a",
-		inputShiftKey: 26,
-		expected:      "a",
-	},
-	{
-		description:   "rotate n by 13 with wrap around alphabet",
-		inputPlain:    "n",
-		inputShiftKey: 13,
-		expected:      "a",
-	},
-	{
-		description:   "rotate capital letters",
-		inputPlain:    "OMG",
-		inputShiftKey: 5,
-		expected:      "TRL",
-	},
-	{
-		description:   "rotate spaces",
-		inputPlain:    "O M G",
-		inputShiftKey: 5,
-		expected:      "T R L",
-	},
-	{
-		description:   "rotate numbers",
-		inputPlain:    "Testing 1 2 3 testing",
-		inputShiftKey: 4,
-		expected:      "Xiwxmrk 1 2 3 xiwxmrk",
-	},
-	{
-		description:   "rotate punctuation",
-		inputPlain:    "Let's eat, Grandma!",
-		inputShiftKey: 21,
-		expected:      "Gzo'n zvo, Bmviyhv!",
-	},
-	{
-		description:   "rotate all letters",
-		inputPlain:    "The quick brown fox jumps over the lazy dog.",
-		inputShiftKey: 13,
-		expected:      "Gur dhvpx oebja sbk whzcf bire gur ynml qbt.",
-	},
-}
-
 func TestRotationalCipher(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.description, func(t *testing.T) {
-			got := RotationalCipher(tc.inputPlain, tc.inputShiftKey)
+			got := RotationalCipher(tc.input, tc.shift)
 			if got != tc.expected {
-				t.Fatalf("RotationalCipher(%q, %d)\n got: %q\nwant: %q", tc.inputPlain, tc.inputShiftKey, got, tc.expected)
+				t.Fatalf("RotationalCipher(%q, %d)\n got: %q\nwant: %q", tc.input, tc.shift, got, tc.expected)
 			}
 		})
 	}
@@ -79,8 +17,8 @@ func TestRotationalCipher(t *testing.T) {
 
 func BenchmarkRotationalCipher(b *testing.B) {
 	for range b.N {
-		for _, testCase := range testCases {
-			RotationalCipher(testCase.inputPlain, testCase.inputShiftKey)
+		for _, tc := range testCases {
+			RotationalCipher(tc.input, tc.shift)
 		}
 	}
 }
