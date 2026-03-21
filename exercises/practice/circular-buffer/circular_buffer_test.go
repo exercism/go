ed102878a0d71e008a80c997a991ef8b17f9a3a0
Isaac Good <IsaacG@users.noreply.github.com>
2026-03-20 22:37:34 -0700

    [circular-buffer] Add a test generator and sync from canonical data (#3043)
    [no important files changed]

--- exercises/practice/circular-buffer/circular_buffer_test.go
+++ exercises/practice/circular-buffer/circular_buffer_test.go
@@ -1,7 +1,9 @@
 package circularbuffer
 
 import (
+	"fmt"
 	"io"
+	"strings"
 	"testing"
 )
 
@@ -13,166 +15,67 @@ var (
 	_ io.ByteWriter = new(Buffer)
 )
 
-// testBuffer and methods support the tests, providing log and fail messages.
+func (tc testCase) Run(t *testing.T) {
+	var calls []string
 
-type testBuffer struct {
-	*testing.T
-	b *Buffer
-}
-
-func nb(size int, t *testing.T) testBuffer {
-	t.Logf("NewBuffer(%d)", size)
-	return testBuffer{t, NewBuffer(size)}
-}
-
-func (tb testBuffer) read(want byte) {
-	switch c, err := tb.b.ReadByte(); {
-	case err != nil:
-		var _ error = err
-		tb.Fatalf("ReadByte() failed unexpectedly: %v", err)
-	case c != want:
-		tb.Fatalf("ReadByte() = %c, want %c.", c, want)
+	b := NewBuffer(tc.capacity)
+	calls = append(calls, fmt.Sprintf("b := NewBuffer(%d)", tc.capacity))
+	for _, op := range tc.operations {
+		var got byte
+		var err error
+		switch op.name {
+		case "ReadByte":
+			got, err = b.ReadByte()
+			calls = append(calls, "b.ReadByte()")
+		case "WriteByte":
+			err = b.WriteByte(op.value)
+			calls = append(calls, fmt.Sprintf("b.WriteByte(%q)", op.value))
+		case "Overwrite":
+			b.Overwrite(op.value)
+			calls = append(calls, fmt.Sprintf("b.Overwrite(%q)", op.value))
+		case "Reset":
+			b.Reset()
+			calls = append(calls, "b.Reset()")
+		}
+		if op.name == "ReadByte" || op.name == "WriteByte" {
+			if op.wantErr && err != nil {
+				continue
+			} else if op.wantErr {
+				t.Fatalf("calls: %s\nexpected error for the last call but got nil", strings.Join(calls, "; "))
+			} else if err != nil {
+				t.Fatalf("calls: %s\ngot unexpected error, %v", strings.Join(calls, "; "), err)
+			}
+		}
+		if op.name == "ReadByte" && got != op.value {
+			t.Fatalf("calls: %s\ngot %q, want %q", strings.Join(calls, "; "), got, op.value)
+		}
 	}
-	tb.Logf("ReadByte %c", want)
 }
 
-func (tb testBuffer) readFail() {
-	c, err := tb.b.ReadByte()
-	if err == nil {
-		tb.Fatalf("ReadByte() = %c, expected a failure", c)
+func TestRun(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.description, tc.Run)
 	}
-	var _ error = err
-	tb.Log("ReadByte() fails as expected")
 }
 
-func (tb testBuffer) write(c byte) {
-	if err := tb.b.WriteByte(c); err != nil {
-		var _ error = err
-		tb.Fatalf("WriteByte(%c) failed unexpectedly: %v", c, err)
+func BenchmarkRun(b *testing.B) {
+	for range b.N {
+		for _, tc := range testCases {
+			b := NewBuffer(tc.capacity)
+			for _, op := range tc.operations {
+				switch op.name {
+				case "ReadByte":
+					b.ReadByte()
+				case "WriteByte":
+					b.WriteByte(op.value)
+				case "Overwrite":
+					b.Overwrite(op.value)
+				case "Reset":
+					b.Reset()
+				}
+			}
+		}
 	}
-	tb.Logf("WriteByte(%c)", c)
-}
-
-func (tb testBuffer) writeFail(c byte) {
-	err := tb.b.WriteByte(c)
-	if err == nil {
-		tb.Fatalf("WriteByte(%c) succeeded, expected a failure", c)
-	}
-	var _ error = err
-	tb.Logf("WriteByte(%c) fails as expected", c)
-}
-
-func (tb testBuffer) reset() {
-	tb.b.Reset()
-	tb.Log("Reset()")
-}
-
-func (tb testBuffer) overwrite(c byte) {
-	tb.b.Overwrite(c)
-	tb.Logf("Overwrite(%c)", c)
-}
-
-// tests.  separate functions so log will have descriptive test name.
-
-func TestReadEmptyBuffer(t *testing.T) {
-	tb := nb(1, t)
-	tb.readFail()
-}
-
-func TestWriteAndReadOneItem(t *testing.T) {
-	tb := nb(1, t)
-	tb.write('1')
-	tb.read('1')
-	tb.readFail()
-}
-
-func TestWriteAndReadMultipleItems(t *testing.T) {
-	tb := nb(2, t)
-	tb.write('1')
-	tb.write('2')
-	tb.read('1')
-	tb.read('2')
-	tb.readFail()
-}
-
-func TestReset(t *testing.T) {
-	tb := nb(3, t)
-	tb.write('1')
-	tb.write('2')
-	tb.write('3')
-	tb.reset()
-	tb.write('1')
-	tb.write('3')
-	tb.read('1')
-	tb.write('4')
-	tb.read('3')
-}
-
-func TestAlternateWriteAndRead(t *testing.T) {
-	tb := nb(2, t)
-	tb.write('1')
-	tb.read('1')
-	tb.write('2')
-	tb.read('2')
-}
-
-func TestReadOldestItem(t *testing.T) {
-	tb := nb(3, t)
-	tb.write('1')
-	tb.write('2')
-	tb.read('1')
-	tb.write('3')
-	tb.read('2')
-	tb.read('3')
-}
-
-func TestWriteFullBuffer(t *testing.T) {
-	tb := nb(2, t)
-	tb.write('1')
-	tb.write('2')
-	tb.writeFail('A')
-}
-
-func TestOverwriteFull(t *testing.T) {
-	tb := nb(2, t)
-	tb.write('1')
-	tb.write('2')
-	tb.overwrite('A')
-	tb.read('2')
-	tb.read('A')
-	tb.readFail()
-}
-
-func TestOverwriteNonFull(t *testing.T) {
-	tb := nb(2, t)
-	tb.write('1')
-	tb.overwrite('2')
-	tb.read('1')
-	tb.read('2')
-	tb.readFail()
-}
-
-func TestAlternateReadAndOverwrite(t *testing.T) {
-	tb := nb(5, t)
-	tb.write('1')
-	tb.write('2')
-	tb.write('3')
-	tb.read('1')
-	tb.read('2')
-	tb.write('4')
-	tb.read('3')
-	tb.write('5')
-	tb.write('6')
-	tb.write('7')
-	tb.write('8')
-	tb.overwrite('A')
-	tb.overwrite('B')
-	tb.read('6')
-	tb.read('7')
-	tb.read('8')
-	tb.read('A')
-	tb.read('B')
-	tb.readFail()
 }
 
 func BenchmarkOverwrite(b *testing.B) {
