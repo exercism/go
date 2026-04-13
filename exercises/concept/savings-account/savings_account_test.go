307396c30588368968784587c7ec7aa72a12ff8c
Isaac Good <github@isaacgood.com>
2026-04-13 16:35:04 -0700

    [savings-account] Use slices of explicit types for test cases

--- exercises/concept/savings-account/savings_account_test.go
+++ exercises/concept/savings-account/savings_account_test.go
@@ -4,14 +4,33 @@ import (
 	"testing"
 )
 
+type interestRateTestCase struct {
+	description string
+	want        float32
+}
+
+type daysPerYearTestCase struct {
+	description string
+	want        int
+}
+
+type getMonthTestCase struct {
+	description string
+	month       int
+	want        int
+}
+
+type getAccountNumberTestCase struct {
+	description string
+	want        AccNo
+}
+
 func TestGetFixedInterestRate(t *testing.T) {
-	tests := map[string]struct {
-		want float32
-	}{
-		"GetFixedInterestRate 1": {want: 0.05},
+	tests := []interestRateTestCase{
+		{description: "GetFixedInterestRate 1", want: 0.05},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
 			if got := GetFixedInterestRate(); got != tc.want {
 				t.Errorf("GetFixedInterestRate() = %v, want %v", got, tc.want)
 			}
@@ -20,13 +39,11 @@ func TestGetFixedInterestRate(t *testing.T) {
 }
 
 func TestGetDaysPerYear(t *testing.T) {
-	tests := map[string]struct {
-		want int
-	}{
-		"GetDaysPerYear 1": {want: 365},
+	tests := []daysPerYearTestCase{
+		{description: "GetDaysPerYear 1", want: 365},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
 			if got := GetDaysPerYear(); got != tc.want {
 				t.Errorf("GetDaysPerYear() = %v, want %v", got, tc.want)
 			}
@@ -35,30 +52,26 @@ func TestGetDaysPerYear(t *testing.T) {
 }
 
 func TestGetMonth(t *testing.T) {
-	tests := map[string]struct {
-		arg, want int
-	}{
-		"GetMonth(Jan)": {arg: Jan, want: 1},
-		"GetMonth(Mar)": {arg: Mar, want: 3},
-		"GetMonth(Oct)": {arg: Oct, want: 10},
+	tests := []getMonthTestCase{
+		{description: "GetMonth(Jan)", month: Jan, want: 1},
+		{description: "GetMonth(Mar)", month: Mar, want: 3},
+		{description: "GetMonth(Oct)", month: Oct, want: 10},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
-			if got := GetMonth(tc.arg); got != tc.want {
-				t.Errorf("GetMonth(%v) = %v, want %v", tc.arg, got, tc.want)
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := GetMonth(tc.month); got != tc.want {
+				t.Errorf("GetMonth(%v) = %v, want %v", tc.month, got, tc.want)
 			}
 		})
 	}
 }
 
 func TestGetAccountNumber(t *testing.T) {
-	tests := map[string]struct {
-		want AccNo
-	}{
-		"GetAccountNumber 1": {want: "XF348IJ"},
+	tests := []getAccountNumberTestCase{
+		{description: "GetAccountNumber 1", want: "XF348IJ"},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
 			if got := GetAccountNumber(); got != tc.want {
 				t.Errorf("GetAccountNumber() = %v, want %v", got, tc.want)
 			}
