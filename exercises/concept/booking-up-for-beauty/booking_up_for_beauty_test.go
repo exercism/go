a61d4a3f9f44e3d467fd7f6039399c4378051aae
Isaac Good <github@isaacgood.com>
2026-04-13 16:05:49 -0700

    [booking-up-for-beauty] Use a slice and explicit types for test cases

--- exercises/concept/booking-up-for-beauty/booking_up_for_beauty_test.go
+++ exercises/concept/booking-up-for-beauty/booking_up_for_beauty_test.go
@@ -5,17 +5,20 @@ import (
 	"time"
 )
 
+type scheduleTestCase struct {
+	description string
+	in          string
+	want        time.Time
+}
+
 func TestSchedule(t *testing.T) {
-	tests := map[string]struct {
-		in   string
-		want time.Time
-	}{
-		"Schedule 1": {in: "7/13/2020 20:32:00", want: time.Date(2020, time.July, 13, 20, 32, 0, 0, time.UTC)},
-		"Schedule 2": {in: "11/28/1984 2:02:02", want: time.Date(1984, time.November, 28, 2, 2, 2, 0, time.UTC)},
-		"Schedule 3": {in: "2/29/2112 11:59:59", want: time.Date(2112, time.February, 29, 11, 59, 59, 0, time.UTC)},
+	tests := []scheduleTestCase{
+		{description: "Schedule 1", in: "7/13/2020 20:32:00", want: time.Date(2020, time.July, 13, 20, 32, 0, 0, time.UTC)},
+		{description: "Schedule 2", in: "11/28/1984 2:02:02", want: time.Date(1984, time.November, 28, 2, 2, 2, 0, time.UTC)},
+		{description: "Schedule 3", in: "2/29/2112 11:59:59", want: time.Date(2112, time.February, 29, 11, 59, 59, 0, time.UTC)},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
 			if got := Schedule(tc.in); !got.Equal(tc.want) {
 				t.Errorf("Schedule(%s) = '%v', want '%v'", tc.in, got, tc.want)
 			}
@@ -23,17 +26,20 @@ func TestSchedule(t *testing.T) {
 	}
 }
 
+type hasPassedTestCase struct {
+	description string
+	in          string
+	want        bool
+}
+
 func TestHasPassed(t *testing.T) {
-	tests := map[string]struct {
-		in   string
-		want bool
-	}{
-		"HasPassed 1": {in: "October 3, 2019 20:32:00", want: true},
-		"HasPassed 2": {in: "January 28, 1974 2:02:02", want: true},
-		"HasPassed 3": {in: "December 9, 2112 11:59:59", want: false},
+	tests := []hasPassedTestCase{
+		{description: "HasPassed 1", in: "October 3, 2019 20:32:00", want: true},
+		{description: "HasPassed 2", in: "January 28, 1974 2:02:02", want: true},
+		{description: "HasPassed 3", in: "December 9, 2112 11:59:59", want: false},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
 			if got := HasPassed(tc.in); got != tc.want {
 				t.Errorf("HasPassed(%s) = '%v', want '%v'", tc.in, got, tc.want)
 			}
@@ -41,17 +47,20 @@ func TestHasPassed(t *testing.T) {
 	}
 }
 
+type isAfternoonAppointmentTestCase struct {
+	description string
+	in          string
+	want        bool
+}
+
 func TestIsAfternoonAppointment(t *testing.T) {
-	tests := map[string]struct {
-		in   string
-		want bool
-	}{
-		"IsAfternoonAppointment 1": {in: "Thursday, May 13, 2010 20:32:00", want: false},
-		"IsAfternoonAppointment 2": {in: "Friday, March 8, 1974 12:02:02", want: true},
-		"IsAfternoonAppointment 3": {in: "Friday, September 9, 2112 11:59:59", want: false},
+	tests := []isAfternoonAppointmentTestCase{
+		{description: "IsAfternoonAppointment 1", in: "Thursday, May 13, 2010 20:32:00", want: false},
+		{description: "IsAfternoonAppointment 2", in: "Friday, March 8, 1974 12:02:02", want: true},
+		{description: "IsAfternoonAppointment 3", in: "Friday, September 9, 2112 11:59:59", want: false},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
 			if got := IsAfternoonAppointment(tc.in); got != tc.want {
 				t.Errorf("IsAfternoonAppointment(%s) = '%v', want '%v'", tc.in, got, tc.want)
 			}
@@ -59,16 +68,20 @@ func TestIsAfternoonAppointment(t *testing.T) {
 	}
 }
 
+type descriptionTestCase struct {
+	description string
+	in          string
+	want        string
+}
+
 func TestDescription(t *testing.T) {
-	tests := map[string]struct {
-		in, want string
-	}{
-		"Description 1": {in: "6/6/2005 10:30:00", want: "You have an appointment on Monday, June 6, 2005, at 10:30."},
-		"Description 2": {in: "9/19/1994 12:15:00", want: "You have an appointment on Monday, September 19, 1994, at 12:15."},
-		"Description 3": {in: "4/4/2012 16:45:00", want: "You have an appointment on Wednesday, April 4, 2012, at 16:45."},
+	tests := []descriptionTestCase{
+		{description: "Description 1", in: "6/6/2005 10:30:00", want: "You have an appointment on Monday, June 6, 2005, at 10:30."},
+		{description: "Description 2", in: "9/19/1994 12:15:00", want: "You have an appointment on Monday, September 19, 1994, at 12:15."},
+		{description: "Description 3", in: "4/4/2012 16:45:00", want: "You have an appointment on Wednesday, April 4, 2012, at 16:45."},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
 			if got := Description(tc.in); got != tc.want {
 				t.Errorf("Description(%s) = '%v', want '%v'", tc.in, got, tc.want)
 			}
@@ -76,14 +89,17 @@ func TestDescription(t *testing.T) {
 	}
 }
 
+type anniversaryDateTestCase struct {
+	description string
+	want        time.Time
+}
+
 func TestAnniversaryDate(t *testing.T) {
-	tests := map[string]struct {
-		want time.Time
-	}{
-		"AnniversaryDate 1": {want: time.Date(time.Now().Year(), time.September, 15, 0, 0, 0, 0, time.UTC)},
+	tests := []anniversaryDateTestCase{
+		{description: "AnniversaryDate 1", want: time.Date(time.Now().Year(), time.September, 15, 0, 0, 0, 0, time.UTC)},
 	}
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
 			if got := AnniversaryDate(); !got.Equal(tc.want) {
 				t.Errorf("AnniversaryDate() = '%v', want '%v'", got, tc.want)
 			}
