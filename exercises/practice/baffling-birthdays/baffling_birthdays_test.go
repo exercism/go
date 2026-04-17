7680c8d5b3048113774d6eef59d146ae68d8c9e9
Isaac Good <github@isaacgood.com>
2026-04-17 09:54:32 -0700

    [baffling-birthdays] Improve runner "TEST CODE" by following the expected test function pattern

--- exercises/practice/baffling-birthdays/baffling_birthdays_test.go
+++ exercises/practice/baffling-birthdays/baffling_birthdays_test.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+const randomSampleSize = 1024 * 32
+
 func TestSharedBirthday(t *testing.T) {
 	for _, tc := range sharedTestCase {
 		t.Run(tc.description, func(t *testing.T) {
@@ -42,53 +44,61 @@ func isLeapYear(year int) bool {
 	return year%4 == 0 && year%100 != 0 || year%400 == 0
 }
 
-func TestRandomBirthdays(t *testing.T) {
-	size := 1024 * 32
-	birthdates := RandomBirthdates(size)
+func TestRandomBirthdays_GeneratesExpectedNumer(t *testing.T) {
 	// "uuid": "70b38cea-d234-4697-b146-7d130cd4ee12"
-	t.Run("generate requested number of birthdates", func(t *testing.T) {
-		if len(birthdates) != size {
-			t.Fatalf("RandomBirthdates(%d) gave %d birthdates; want %d birthdates", size, len(birthdates), size)
-		}
-	})
+	// generate requested number of birthdates
+	birthdates := RandomBirthdates(randomSampleSize)
+	if len(birthdates) != randomSampleSize {
+		t.Fatalf("RandomBirthdates(%d) gave %d birthdates; want %d birthdates", randomSampleSize, len(birthdates), randomSampleSize)
+	}
+}
+
+func TestRandomBirthdays_NonLeapYears(t *testing.T) {
 	// "uuid": "d9d5b7d3-5fea-4752-b9c1-3fcd176d1b03"
-	t.Run("years are not leap years", func(t *testing.T) {
-		for _, date := range birthdates {
-			if isLeapYear(date.Year()) {
-				t.Fatalf("RandomBirthdates(%d) gave a leap year, %v; want non-leap years old", size, date)
-			}
+	// years are not leap years
+	birthdates := RandomBirthdates(randomSampleSize)
+	for _, date := range birthdates {
+		if isLeapYear(date.Year()) {
+			t.Fatalf("RandomBirthdates(%d) gave a leap year, %v; want non-leap years old", randomSampleSize, date)
 		}
-	})
+	}
+}
+
+func TestRandomBirthdays_RandomMonths(t *testing.T) {
 	// "uuid": "d1074327-f68c-4c8a-b0ff-e3730d0f0521"
-	t.Run("months are random", func(t *testing.T) {
-		avg := float64(size / 12)
-		errBand := 0.2
-		months := make(map[time.Month]int)
-		for _, date := range birthdates {
-			months[date.Month()]++
+	// months are random
+	birthdates := RandomBirthdates(randomSampleSize)
+	avg := float64(randomSampleSize / 12)
+	errBand := 0.2
+	months := make(map[time.Month]int)
+	for _, date := range birthdates {
+		months[date.Month()]++
+	}
+	for month, count := range months {
+		if float64(count) < (1-errBand)*avg || float64(count) > (1+errBand)*avg {
+			t.Fatalf("RandomBirthdates(%d) has %d dates in %v; expected a uniform distribution (about %v per month)", randomSampleSize, count, month, avg)
 		}
-		for month, count := range months {
-			if float64(count) < (1-errBand)*avg || float64(count) > (1+errBand)*avg {
-				t.Fatalf("RandomBirthdates(%d) has %d dates in %v; expected a uniform distribution (about %v per month)", size, count, month, avg)
-			}
-		}
-	})
+	}
+}
+
+func TestRandomBirthdays_RandomDays(t *testing.T) {
 	// "uuid": "7df706b3-c3f5-471d-9563-23a4d0577940"
-	t.Run("days are random", func(t *testing.T) {
-		days := make(map[int]int)
-		for _, date := range birthdates {
-			days[date.Day()]++
+	// days are random
+	birthdates := RandomBirthdates(randomSampleSize)
+	days := make(map[int]int)
+	for _, date := range birthdates {
+		days[date.Day()]++
+	}
+	for day, count := range days {
+		wantFloat := 0.80 * randomSampleSize / 31
+		want := int(wantFloat)
+		if day == 31 {
+			want /= 2
 		}
-		for day, count := range days {
-			want := int(0.80 * float64(size) / 31)
-			if day == 31 {
-				want /= 2
-			}
-			if count < want {
-				t.Fatalf("RandomBirthdates(%d), day-of-month %d appears %d times; expected a uniform distribution (about %d per day)", size, day, count, want)
-			}
+		if count < want {
+			t.Fatalf("RandomBirthdates(%d), day-of-month %d appears %d times; expected a uniform distribution (about %d per day)", randomSampleSize, day, count, want)
 		}
-	})
+	}
 }
 
 func BenchmarkSharedBirthday(b *testing.B) {
@@ -105,8 +115,7 @@ func BenchmarkSharedBirthday(b *testing.B) {
 }
 
 func BenchmarkRandomBirthdates(b *testing.B) {
-	size := 1024 * 16
 	for range b.N {
-		RandomBirthdates(size)
+		RandomBirthdates(randomSampleSize)
 	}
 }
